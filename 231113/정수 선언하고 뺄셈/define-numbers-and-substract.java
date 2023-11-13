3ee9339838c358144bf14020819fae9a6728c668
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-13 09:05:36 +0900

    Title: 정수 선언하고 뺄셈, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231113/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int v1 = 97, v2 = 13;
+        int output = v1 - v2;
+        System.out.print("97 - 13 = " + output); 
+    }
+}
