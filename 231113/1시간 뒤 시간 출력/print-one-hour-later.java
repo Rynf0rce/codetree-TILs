e1ee69b05f18204fdb69c9e77cb082bcf9104de2
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-13 15:37:32 +0900

    Title: 1시간 뒤 시간 출력, Time: 184ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231113/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter(":");
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.printf("%d:%d", h + 1, m);
+    }
+}
