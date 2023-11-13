1c1a6775fe586e4703cc5a4eef696fa5ec428f29
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-13 22:22:05 +0900

    Title: 살 수 있는 물건, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231113/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int money = sc.nextInt();
+        String output = "no";
+        if(money >= 3000){
+            output = "book";
+        }
+        else if(money >=1000){
+            output = "mask";
+        }
+        System.out.print(output);
+    }
+}
