540ea67fa7beabc0f08c2723af5c763650ba9ece
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-13 09:29:03 +0900

    Title: 변수 출력하기 3, Time: 111ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231113/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        char c = 'C';
+        String arrow = "->";
+        
+        System.out.printf("%d%s%d%s%c", a, arrow, b, arrow, c);
+
+        // 여기에 코드를 작성해주세요.
+    }
+}
