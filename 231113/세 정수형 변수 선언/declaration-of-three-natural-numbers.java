a2fe843223a03bef788247e47dedae283f4169e6
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-13 09:18:34 +0900

    Title: 세 정수형 변수 선언, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231113/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int v1 = 7, v2 = 23;
+        int v3 = v1 + v2;
+        System.out.printf("%d + %d = %d", v1, v2, v3);
+        // 여기에 코드를 작성해주세요.
+    }
+}
