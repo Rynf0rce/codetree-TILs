9dbc932d7ac97eb507fabbb034f5c028a4690f20
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-13 09:32:57 +0900

    Title: 달에서 무게 구하기, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231113/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 13;
+        double gravityRate = 0.165;
+        double output = weight * gravityRate;
+        System.out.printf("%d * %.6f = %.6f", weight, gravityRate, output);
+        // 여기에 코드를 작성해주세요.
+    }
+}
