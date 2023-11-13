c8cca3523426254f9541790f1e7c5475cf3bb73e
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-13 22:16:48 +0900

    Title: 물의 상태, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231113/물의 상태/state-of-water.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int temperture = sc.nextInt();
+        String output = "water";
+        if(temperture < 0){
+            output = "ice";
+        }
+        else if(temperture >=0){
+            output = "vapor";
+        }
+        System.out.print(output);
+    }
+}
