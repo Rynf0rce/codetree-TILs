77ba5fa2bf22732ddca8e1fa20e0fd0cdd565972
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-14 09:00:49 +0900

    Title: 최대 2번의 연산, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 231114/최대 2번의 연산/up-to-2-calculations.java
+++ 231114/최대 2번의 연산/up-to-2-calculations.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        a = a % 2 == 0 ? a / 2 : (a + 1) / 2;
-        a = a % 2 == 0 ? a / 2 : (a + 1) / 2;
+        if (a%2 == 0){
+            a = a / 2;
+        }
+        if(a%2 == 1){
+            a = (a + 1) / 2;
+        }
         System.out.print(a);
     }
 }
