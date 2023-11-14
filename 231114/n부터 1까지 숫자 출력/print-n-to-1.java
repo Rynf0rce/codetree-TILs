56d66f921def59ebffa5ba0e3add42cd8267d5d6
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-14 13:32:38 +0900

    Title: n부터 1까지 숫자 출력, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231114/n부터 1까지 숫자 출력/print-n-to-1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = n;
+        while(num >= 1){
+            System.out.print(num + " ");
+            num--;
+        }
+    }
+}
