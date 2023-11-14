d42ede8e9a4f18907af5bf2e8c9cbc4d630b9f54
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-14 13:31:40 +0900

    Title: 감소시키며 출력하기 2, Time: 80ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231114/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 26;
+        while(num >= 10){
+            System.out.print(num + " ");
+            num--;
+        }
+    }
+}
