5625153f45099112e8c3c77792d122b0c7a0b0a9
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-14 15:46:26 +0900

    Title: 청소당번 정하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231114/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt(), cleanClass = 0, cleancorridor = 0, cleanToilet = 0;
+        for(int i = 1 ; i < day ; i++){
+            if(i % 12 == 0){
+                cleanToilet++;
+            }
+            else if(i % 3 == 0){
+                cleancorridor++;
+            }
+            else if(i % 2 == 0){
+                cleanClass++;
+            }
+        }
+        System.out.printf("%d %d %d", cleanClass, cleancorridor, cleanToilet);
+    }
+}
