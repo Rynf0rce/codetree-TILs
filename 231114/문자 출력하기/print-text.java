2227141e8e348bb43af4fa00e0ff5197686a8dc6
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-14 13:39:51 +0900

    Title: 문자 출력하기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 231114/문자 출력하기/print-text.java
+++ 231114/문자 출력하기/print-text.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
+        char num = sc.next().charAt(0);
         for(int i = 0 ; i < 8 ; i++){
-            System.out.print(input);
+            System.out.print(num);
         }
     }
 }
