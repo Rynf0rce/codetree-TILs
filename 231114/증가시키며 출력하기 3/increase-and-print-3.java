6af8e23da83ceaeb3a265ed8431f8668c77891d1
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-14 13:26:20 +0900

    Title: 증가시키며 출력하기 3, Time: 118ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231114/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 10;
+        while(num <= 26){
+            System.out.print(num + " ");
+            num += 2;
+        }
+    }
+}
