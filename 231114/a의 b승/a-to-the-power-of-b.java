e5e2f7c3da499d1d8424ca7297d0de225c043451
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-14 18:08:20 +0900

    Title: a의 b승, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231114/a의 b승/a-to-the-power-of-b.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int productVal = 1;
+        for(int i = 0 ; i < b ; i++){
+            productVal *= a;
+        }
+        System.out.print(productVal);
+
+    }
+}
