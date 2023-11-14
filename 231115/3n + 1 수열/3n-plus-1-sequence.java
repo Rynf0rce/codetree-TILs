bfb573e9ea18a27abc45de678c83554600e10874
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-15 08:13:39 +0900

    Title: 3n + 1 수열, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231115/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int conVal = input, cnt = 0;
+        while(true){
+            if(conVal % 2 == 0){
+                 conVal /= 2;
+            }
+            else{
+                conVal = conVal * 3 + 1;
+            }
+            cnt++;
+            if(conVal ==1){
+                break;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
