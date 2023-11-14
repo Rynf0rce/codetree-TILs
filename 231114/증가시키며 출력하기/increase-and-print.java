7c81cdbc5cad6582df58793f3ada5d10d5f704dc
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-14 13:05:50 +0900

    Title: 증가시키며 출력하기, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231114/증가시키며 출력하기/increase-and-print.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int counter = 5 ; counter < 18 ; counter += 2){
+            System.out.print(counter + " ");
+        }
+    }
+}
