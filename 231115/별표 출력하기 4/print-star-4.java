d3689ff12fe61090ce6d90d7f3cb622983f95696
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-15 17:12:12 +0900

    Title: 별표 출력하기 4, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231115/별표 출력하기 4/print-star-4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0 ; i < 2*n -1 ; i++){
+            if(i < n){
+                for(int j = n - i ; j > 0 ; j--){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            else{
+                for(int j = -1 ; j <= i-n ; j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
