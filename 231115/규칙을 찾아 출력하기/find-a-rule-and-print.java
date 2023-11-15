8c38cd3cd1e6763ed5055117371cd67788b76ae7
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-15 22:17:49 +0900

    Title: 규칙을 찾아 출력하기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 231115/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 231115/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
         for(int i = 1 ; i <= n  ; i++){
             for(int j = 1 ; j <= n ; j++){
-                if(i == 1 || i == 5 || j == 1 || j == 5 || i > j){
+                if(i == 1 || i == n || j == 1 || j == n || i > j){
                     System.out.print("* ");
                 }
                 else{
