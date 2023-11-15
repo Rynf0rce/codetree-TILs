e87c999dbe9e0590d49e164048cc8847032396c0
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-15 20:00:51 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 231115/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
+++ 231115/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -1,5 +1,40 @@
 import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), blankCnt = n, starCnt = 0;
+        boolean triger = true;
+        for(int i = 0 ; i < 2*n - 1 ; i++){
+            if(i < n){
+                blankCnt--;
+            }
+            else{
+                blankCnt++;
+                triger = false;
+            }
+            starCnt = n-blankCnt;
+            if(triger){
+                for(int j = 0 ; j < blankCnt  ; j++){
+                System.out.print("  ");
+                }
+                for(int j = 0 ; j < starCnt ; j++ ){
+                    System.out.print("@ ");
+                }
+            }
+            else{
+                for(int j = 0 ; j < starCnt  ; j++){
+                    System.out.print("@ ");
+                }
+                for(int j = 0 ; j < blankCnt ; j++ ){
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
 /* n = int(input())
 for i in range(1,n+1):
     print('  '*(n-i), end='')
@@ -9,9 +44,7 @@ for j in range(n-1, 0, -1):
     print('@ '*j, end=' ')
     print() */
 
-
-
-public class Main {
+/* public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -34,4 +67,4 @@ public class Main {
             System.out.println();
         }
     }
-}
+} */
