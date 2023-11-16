ccf1d2150ae5600e773a00db7f4684f6ef9147eb
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-16 18:52:47 +0900

    Title: 숫자들의 배수, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 231116/숫자들의 배수/multiple-of-numbers.java
+++ 231116/숫자들의 배수/multiple-of-numbers.java
@@ -5,10 +5,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), flag = 0;
         int[] arr = new int[10];
+        
         for(int i = 0 ; i < arr.length ; i++){
             arr[i] = n * (i+1);
         }
-
 
         for(int i = 0 ; i < arr.length ; i++){
             System.out.print(arr[i] + " ");
