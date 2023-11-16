52b4146364512c83527d778fad01eeb93a14b76d
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-16 17:13:09 +0900

    Title: 특정 위치의 숫자, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231116/특정 위치의 숫자/numbers-in-specific-location.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print(arr[2] + arr[4] + arr[9]);
+        
+    }
+}
