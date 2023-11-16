67b8cd0185a28ad5424b3241f22a82fd577e0330
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-16 19:52:11 +0900

    Title: 십의 자리 숫자, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231116/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        int[] count = new int[10];
+        for(int i = 0 ; i < arr.length ; i++){
+            try{
+                arr[i] = sc.nextInt();
+                count[arr[i] / 10]++;
+
+            }catch(NoSuchElementException e){
+                break;
+            }         
+        }
+
+        for(int i = 1; i < count.length ; i++){
+            System.out.printf("%d - %d\n", i, count[i]);
+        }
+
+
+    }
+}
