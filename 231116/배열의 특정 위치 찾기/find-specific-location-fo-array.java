8c39b6a515c9e28c64f90af4cf716ef5ef84b8cc
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-16 17:26:07 +0900

    Title: 배열의 특정 위치 찾기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 231116/배열의 특정 위치 찾기/find-specific-location-fo-array.java
+++ 231116/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -4,14 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[10];
-        int sum = 0, cnt = 0;
+        int evenSum = 0, thirdSum = 0, cnt = 0;
         for(int i = 0 ; i < arr.length ; i++){
             arr[i] = sc.nextInt();
         }
-        for(int i = 1 ; i < arr.length ; i += 2){
-            sum += arr[i];
-            cnt++;
+        for(int i = 0 ; i < arr.length ; i++){
+            if((i + 1) % 2 == 0){
+                evenSum += arr[i];
+            }
+            if((i + 1) % 3 == 0){
+                thirdSum += arr[i];
+                cnt++;
+            }
         }
-        System.out.printf("%d %.1f", sum, (double)sum / cnt);
+        System.out.printf("%d %.1f", evenSum, (double)thirdSum / cnt);
     }
 }
