ac7876fdf751352b48b6c438c103e90c6d104cff
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-17 15:05:20 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 216ms, Memory: 13MB, Status: Passed - CodeTree

--- 231117/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 231117/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -4,18 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n], arrCnt = new int[1001];
+        int[] arr = new int[n];
         int maxVal = 0, pointer = 0, idx = arr.length;
-        // arr = new int[]{3,1,2,3,4};
         for(int i = 0; i < arr.length ; i++){
             arr[i] = sc.nextInt();
-            arrCnt[arr[i]]++;
         }
 
-
-
-
-        while(idx > 1){
+        while(true){
             maxVal = 0;
             for(int i = 0 ; i < idx ; i++){
                 if(arr[i] > maxVal){
@@ -25,14 +20,12 @@ public class Main {
             }
 
             System.out.print(pointer + 1 + " ");
+            idx = pointer;
 
-            if(arrCnt[maxVal] > 1){
-                idx = pointer;
+            if(idx > 0){
                 continue;
             }
             else{
-                idx = 0;
-                System.out.print(idx + 1);
                 break;
             }
         }   
