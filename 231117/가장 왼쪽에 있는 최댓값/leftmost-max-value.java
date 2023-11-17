22b67df7973ce437e85f86901fdeea9972b57005
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-17 14:30:41 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231117/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 231117/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -6,12 +6,16 @@ public class Main {
         int n = sc.nextInt();
         int[] arr = new int[n], arrCnt = new int[1001];
         int maxVal = 0, pointer = 0, idx = arr.length;
+        // arr = new int[]{3,1,2,3,4};
         for(int i = 0; i < arr.length ; i++){
             arr[i] = sc.nextInt();
             arrCnt[arr[i]]++;
         }
 
-        while(true){
+
+
+
+        while(idx > 1){
             maxVal = 0;
             for(int i = 0 ; i < idx ; i++){
                 if(arr[i] > maxVal){
@@ -19,12 +23,16 @@ public class Main {
                     pointer = i;
                 }
             }
+
+            System.out.print(pointer + 1 + " ");
+
             if(arrCnt[maxVal] > 1){
-                System.out.print(pointer + 1 + " ");
                 idx = pointer;
                 continue;
             }
             else{
+                idx = 0;
+                System.out.print(idx + 1);
                 break;
             }
         }   
