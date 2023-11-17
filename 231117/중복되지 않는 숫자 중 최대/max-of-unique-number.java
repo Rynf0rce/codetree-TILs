7f0f7bf51cb2015c06ee21042288595e7575ef9e
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-17 13:43:25 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231117/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inputVal = sc.nextInt();
+        int[] arr = new int[inputVal];
+        int pointer = 0,  temp = 0;
+        int[] cntArr = new int[1001];
+        boolean overlap = false;
+        for(int i = 0; i < arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        final int INT_MIN = Integer.MIN_VALUE;
+        int maxVal = INT_MIN;
+        
+        for(int i = 0 ; i < arr.length ; i++ ){
+            pointer = i;
+            maxVal = INT_MIN;
+            for(int j = i ; j < arr.length ; j++){
+                if(maxVal < arr[j]){
+                    maxVal = arr[j];
+                    pointer = j;
+                }
+            }
+
+            if(pointer != i){
+                arr[pointer] = arr[i];
+                arr[i] = maxVal;
+                cntArr[maxVal]++;
+            }
+        }
+        // System.out.printf("%d %d", arr[0], arr[1]);
+        for(int i = 0 ; i < arr.length ; i++){
+            overlap = true;
+            if(cntArr[arr[i]] == 1){
+                overlap = false;
+                pointer = arr[i];
+                break;
+            }
+        }
+        System.out.print(overlap ? -1 : pointer);
+    }
+}
