7172896e51897cdf0a4ee906cdea5f9fd34c165f
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-18 11:16:59 +0900

    Title: 동전이 있는 위치, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231118/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), row = 0, column = 0;
+        int[][] arr2d = new int[n][n];
+        for(int i = 0; i < m ; i++){
+            row = sc.nextInt();
+            column = sc.nextInt();
+            arr2d[row - 1][column - 1]++;
+        }
+        for(int i = 0 ; i < arr2d.length ; i++){
+            for(int j = 0 ; j < arr2d.length ; j++){
+                System.out.print(arr2d[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
