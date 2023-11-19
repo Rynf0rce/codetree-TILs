1f5fdc9dda1fab1aaa5d08fc27ff0ad857e401ae
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-19 17:17:41 +0900

    Title: e 제거하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231119/e 제거하기/e-to-remove.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+        boolean triger = true;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] = 'e' && triger){
+                triger = false;
+            }
+            else{
+                System.out.print(arr[i]);
+            }
+        }
+    }
+}
