db629404ed07812bdf7af92c20b5c80bbaefd791
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-19 15:07:54 +0900

    Title: 부분문자열 위치 구하기, Time: 109ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 231119/부분문자열 위치 구하기/find-location-of-substring.java
+++ 231119/부분문자열 위치 구하기/find-location-of-substring.java
@@ -5,28 +5,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
         String target = sc.next();
-        int idx = 0, first_idx = -1;
-        boolean judge = false;
-        input = "aababaa";
-        target = "aba"
+        boolean triger = false;
 
         for(int i = 0 ; i < input.length() ; i++){
-            if(input.charAt(i) == target.charAt(idx)){
-                first_idx = idx == 0 ? i : first_idx;
-                if(first_idx + target.length() > input.length()){
-                    first_idx = -1;
-                    break;
+            if(input.charAt(i) == target.charAt(0) && i + target.length() <= input.length()){
+                for(int j = 1 ; j < target.length() ; j++){
+                    if(input.charAt(i+j) != target.charAt(j)){
+                        break;
+                    }
+                    else if(j == target.length() - 1){
+                        triger = true;
+                    }
                 }
-                else if(idx == target.length() - 1){
-                    break;
-                }
-                idx++;
             }
-            else{
-                idx = 0;
-                first_idx = -1;
+            if(triger){
+                System.out.print(i);
+                break;
             }
         }
-        System.out.print(first_idx);
     }
 }
