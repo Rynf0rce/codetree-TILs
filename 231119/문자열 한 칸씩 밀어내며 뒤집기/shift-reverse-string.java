18904d3387fb9fdbf0efc1930cc49eb382756b6c
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-19 21:06:08 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231119/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
+++ 231119/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
@@ -5,28 +5,29 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
 
-        //str = "ba";
-
         char[] arr = str.toCharArray();
         char temp = ' ';
         int num = sc.nextInt(), task = 0;
         for(int i = 0 ; i < num ; i++){
             task = sc.nextInt();
+            char[] workArr = new char[str.length()];
             switch(task){
                 case 1 :
                     temp = arr[0];
                     for(int j = 0 ;  j < arr.length-1 ; j++){
-                        arr[j] = arr[j+1];
+                        workArr[j] = arr[j+1];
                     }
-                    arr[arr.length - 1] = temp;
+                    workArr[arr.length - 1] = temp;
+                    arr = workArr.clone();
                     str = String.valueOf(arr);
                     break;
                 case 2 :
                     temp = arr[arr.length -1];
                     for(int j = arr.length - 1 ;  j > 0 ; j--){
-                        arr[j] = arr[j-1];
+                        workArr[j] = arr[j-1];
                     }
-                    arr[0] = temp;
+                    workArr[0] = temp;
+                    arr = workArr.clone();
                     str = String.valueOf(arr);
                     break;
                 case 3 :
