d0c257e835bc0672bbe99b0bdf56975707da1a25
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-19 17:08:57 +0900

    Title: 문자열 계속 지우기, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 231119/문자열 계속 지우기/keep-removing-string.java
+++ 231119/문자열 계속 지우기/keep-removing-string.java
@@ -7,15 +7,22 @@ public class Main {
         String target = sc.next();
         boolean triger = true;
         int idx = 0;
+
+        //input = "aaabbb";
+        //target = "ab";
+
+
         while(triger){
-            //System.out.println(input);
+            //System.out.println(input + " current idx : " + idx);
             if(target.length() + idx > input.length()){
+                //System.out.println("break : " +  target.length() + idx + " " + input.length());
                 break;
             }
 
             if(input.substring(0 + idx, target.length() + idx).equals(target)){
                 input = input.substring(0, idx) + input.substring(idx + target.length());
                 idx = 0;
+                continue;
             }
             idx++;
         }
