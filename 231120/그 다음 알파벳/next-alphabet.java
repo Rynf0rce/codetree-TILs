f1da81ec202d3ecc790ca8cf46bb5a45ab5d754f
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-20 10:53:47 +0900

    Title: 그 다음 알파벳, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 231120/그 다음 알파벳/next-alphabet.java
+++ 231120/그 다음 알파벳/next-alphabet.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char input = sc.next().charAt(0);
-        System.out.printf("%c", input + 1);
+        if(input == 'z'){
+            input = 'a';
+        }
+        else{
+            input = (char)(input + 1);
+        }
+        System.out.printf("%c", input);
     }
 }
