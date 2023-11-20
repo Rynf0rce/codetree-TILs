d620bc336e2a361e27bf3d758a2ac4483d813ac4
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-20 10:26:34 +0900

    Title: 규칙에 따라 밀기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231120/규칙에 따라 밀기/push-by-the-rules.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.nextLine(), B = sc.nextLine();
+        char[] a_Arr = A.toCharArray(), b_Arr = B.toCharArray();
+        int push = 0;
+        for(int i = 0 ; i < b_Arr.length ; i++){
+            if(b_Arr[i] == 'L'){
+                push--;
+            }
+            else{
+                push++;
+            }
+        }
+
+        char[] taskArr = new char[A.length()];
+        // R
+        if(push > 0){
+            for(int i = 0 ; i < taskArr.length - push ; i++){
+                taskArr[i + push] = a_Arr[i];
+                if(i < push){
+                    taskArr[i] = a_Arr[taskArr.length - push + i];
+                }
+            }
+        }
+        // L
+        else{
+            push = Math.abs(push);
+            for(int i = 0 ; i < taskArr.length ; i++){
+                if(i >= push){
+                    taskArr[i - push] = a_Arr[i];
+                }
+                else{
+                    taskArr[taskArr.length - push + i] = a_Arr[i];
+                } 
+            }
+        }
+        A = String.valueOf(taskArr);
+        System.out.print(A);
+    }
+}
