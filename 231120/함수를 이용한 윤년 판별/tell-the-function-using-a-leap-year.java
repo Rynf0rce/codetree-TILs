98fe78ddea0e357d2f196e4c0af7588b78cdf343
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-20 17:11:43 +0900

    Title: 함수를 이용한 윤년 판별, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231120/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static boolean fucLeap(int year){
+        if(year % 400 == 0){
+            return true;
+        }
+        else if(year % 100 == 0){
+            return false;
+        }
+        else if(year % 4 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.print(fucLeap(year));
+    }
+}
