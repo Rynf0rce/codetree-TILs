2440fc4f483691982d116efcc38b5c0bd59cf2fe
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-20 10:48:34 +0900

    Title: 아스키코드 변환, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231120/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char input1 = sc.next().charAt(0);
+        int input2 = sc.nextInt();
+        System.out.printf("%d %c", (int)input1, (char)input2);
+    }
+}
