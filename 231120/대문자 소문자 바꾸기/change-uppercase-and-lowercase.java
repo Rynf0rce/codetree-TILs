52c11f6d536f6125b0da097075273c3091a6796e
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-20 11:10:08 +0900

    Title: 대문자 소문자 바꾸기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231120/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] arr = str.toCharArray();
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] >= 'A' && arr[i] <= 'Z'){
+                System.out.print((char)(arr[i] - 'A' + 'a'));
+            }
+            else if(  (arr[i] >= 'a' && arr[i] <= 'z')){
+                System.out.print((char)(arr[i] - 'a' + 'A'));
+            }
+        }
+    }
+}
