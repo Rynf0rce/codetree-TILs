518ed6a68fdad12f6c7022b5fd6370fbb8dfccde
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-21 08:52:44 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231121/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static boolean conFuc(int[] arrA, int[] arrB){
+        if(arrB.length > arrA.length){
+            return false;
+        }
+
+        int cnt = 0;
+        for(int i = 0 ; i <= arrA.length - arrB.length; i++){
+            cnt = 0;
+            for(int j = 0 ; j < arrB.length ; j++){
+                if(arrA[i+j] == arrB[j]){
+                    cnt++;
+                }
+                else{
+                    break;
+                }
+            }
+            if(cnt == arrB.length){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        int[] arrA = new int[a], arrB = new int[b];
+        System.out.print(conFuc(arrA, arrB) ? "Yes" : "No");
+    }
+}
