e9ca9d1718b43d8c08e60fe0b1585bdf59d70480
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-21 14:18:42 +0900

    Title: 반복 출력하기 2, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231121/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void printHW(int i){
+        if(i == 0){
+            return;
+        }
+        printHW(i-1);
+        System.out.println("HelloWorld");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        printHW(4);
+    }
+}
