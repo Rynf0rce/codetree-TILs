91fb6cb2dde8bf5479696680b777fcc8f0d72bf9
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-21 11:41:32 +0900

    Title: 2개 이상의 알파벳, Time: 115ms, Memory: 10MB, Status: Passed - CodeTree

--- 231121/2개 이상의 알파벳/more-than-one-alphabet.java
+++ 231121/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -2,11 +2,16 @@ import java.util.*;
 
 public class Main {
     public static boolean mulAlpha(String str){
+        int cnt = 0;
         for(int i = 0 ; i < str.length() ; i++){
             for(int j = i + 1 ; j < str.length(); j++){
-                if(str.charAt(i) == str.charAt(j)){
-                    return true;
+                if(str.charAt(i) != str.charAt(j)){
+                    cnt++;
+                    break;
                 }
+            }
+            if(cnt >= 2){
+                return true;
             }
         }
         return false;
