6d22b1ba22d561a14e285554d4205a94ff62c562
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-22 16:12:20 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231122/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+    public static int conFnc(int[] arr, int n){
+        boolean triger = false;
+        boolean proTriger = false;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] > n){
+                triger = true;
+                break;
+            }
+        }
+
+        if(triger){
+            for(int i = 0 ; i < arr.length ; i++){
+                if(arr[i] % n == 0){
+                    arr[i] /= n;
+                    proTriger = true;
+                }
+            }
+
+            if(proTriger){
+                return conFnc(arr, n+1) * n;
+            }
+            else{
+                return conFnc(arr, n+1);
+            }
+            
+        }
+        else{
+            int product = 1;
+            for(int i = 0; i < arr.length ; i++){
+                product *= arr[i];
+            }
+            return product;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print(conFnc(arr, 1));
+    }
+}
