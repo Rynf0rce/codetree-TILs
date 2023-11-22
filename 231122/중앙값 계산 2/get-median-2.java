a48d1705a71b056bb01998b55198e1f731c24e7e
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-22 20:04:33 +0900

    Title: 중앙값 계산 2, Time: 129ms, Memory: 11MB, Status: Passed - CodeTree

--- 231122/중앙값 계산 2/get-median-2.java
+++ 231122/중앙값 계산 2/get-median-2.java
@@ -19,3 +19,31 @@ public class Main {
         }
     }
 }
+
+/*
+public class Main {
+    public static final int MAX_N = 100;
+    
+    // 변수 선언
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        // 홀수 번째 수를 지날때마다 정렬을 진행한 후 가운데 값을 출력합니다.
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0) {
+                // 오름차순 정렬을 진행합니다.
+                Arrays.sort(arr, 0, i + 1);
+                // 가운데 값을 출력합니다.
+                System.out.print(arr[i / 2] + " ");
+            }
+        }
+    }
+}
+*/
