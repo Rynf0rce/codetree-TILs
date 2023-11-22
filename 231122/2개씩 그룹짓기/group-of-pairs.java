a9833dc36f5d51462cae1a0e93e7532ca58d1750
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-22 18:06:19 +0900

    Title: 2개씩 그룹짓기, Time: 207ms, Memory: 13MB, Status: Passed - CodeTree

--- 231122/2개씩 그룹짓기/group-of-pairs.java
+++ 231122/2개씩 그룹짓기/group-of-pairs.java
@@ -3,12 +3,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        int num = sc.nextInt(), maxVal = Integer.MIN_VALUE;
         int[] arr = new int[num*2];
         for(int i = 0; i < arr.length ; i++){
             arr[i] = sc.nextInt();
         }
         Arrays.sort(arr);
-        System.out.print(arr[arr.length/2] + arr[(arr.length/2) - 1]);
+        for(int i = 0 ; i < arr.length / 2 ; i++){
+            if(maxVal < arr[i] + arr[arr.length - 1 - i]){
+                maxVal = arr[i] + arr[arr.length - 1 - i];
+            }
+        }
+        System.out.print(maxVal);
     }
 }
