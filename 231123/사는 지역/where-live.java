27ac965b2636db5c28f327db2944f5cf0907314a
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-23 13:00:49 +0900

    Title: 사는 지역, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231123/사는 지역/where-live.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class info{
+    String name;
+    String address;
+    String city;
+
+    public info(String name, String address, String city){
+        this.name = name;
+        this.address = address;
+        this.city = city;
+    }
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        info[] arr = new info[num];
+        int idx = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+            String name = sc.next();
+            String address = sc.next();
+            String city = sc.next();
+            arr[i] = new info(name, address, city);
+        }
+        
+        for(int i = 1 ; i < arr.length ; i++){
+            for(int j = 0 ; j < arr[i].name.length() ; j++){
+                if(arr[idx].name.charAt(j) < arr[i].name.charAt(j)){
+                    idx = i;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("name " + arr[idx].name);
+        System.out.println("addr " + arr[idx].address);
+        System.out.println("city " + arr[idx].city);     
+    }
+}
