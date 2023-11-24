5822abf7e5b81f921a045655828fc1c7678caeef
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-24 12:11:44 +0900

    Title: 2진수로 변환하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 231124/2진수로 변환하기/convert-to-binary.java
+++ 231124/2진수로 변환하기/convert-to-binary.java
@@ -5,9 +5,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt(), cnt = 0;
         int[] arr = new int[100];
-        while(input > 0){
+        while(true){
             arr[cnt++] = input % 2;
             input /= 2;
+            if(input == 0){
+                break;
+            }
         }
 
         for(int i = cnt - 1; i >= 0 ; i--){
