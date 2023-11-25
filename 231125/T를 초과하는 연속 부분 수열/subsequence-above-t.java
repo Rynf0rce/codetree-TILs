7846e164e3b31deb2f1f347b852a3f4e5f6aec0b
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-25 17:01:46 +0900

    Title: T를 초과하는 연속 부분 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231125/T를 초과하는 연속 부분 수열/subsequence-above-t.java
+++ 231125/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -8,37 +8,47 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[MAX_INPUT];
-        int N = sc.nextInt(), T = sc.nextInt(), maxValue = 0, cnt = 0;
+        int[] judgeArr = new int[MAX_INPUT];
+        int N = sc.nextInt(), T = sc.nextInt(), idx = 0;   
         for(int i = 0 ; i < N ; i++){
             arr[i] = sc.nextInt();
         }
 
         for(int i = 0 ; i < N ; i++){
-            if(arr[i] > T){
-                if(cnt == 0){
-                    cnt = 1;
-                }
-                else{
-                    if(arr[i-1] < arr[i]){
-                        cnt++;
-                    }
-                    else{
-                        if(maxValue < cnt){
-                            maxValue = cnt + 1;
-                        }
-                        cnt = 1;
-                    }
-                }
-
+            if(arr[i] <= 3){
+                judgeArr[i] = 0;
             }
             else{
-                cnt = 0;
-            }
-
-            if(i == N-1 && maxValue < cnt){
-                maxValue = cnt;
+                if(judgeArr[i-1] == 0){
+                    idx++;
+                    judgeArr[i] = idx;
+                }
+                else if(arr[i] - arr[i-1] > 0){
+                    judgeArr[i] = idx;
+                    
+                }
+                else{
+                    System.out.println("check : " + i);
+                    idx++;
+                    judgeArr[i] = idx;
+                }
             }
         }
-        System.out.print(maxValue);
+
+        int cnt = 1, maxVal = Integer.MIN_VALUE;
+        for(int i = 1 ; i < N ; i++){
+            if(judgeArr[i] == judgeArr[i-1]){
+                cnt++;
+            }
+            else{
+                cnt = 1;
+            }
+
+            if(cnt > maxVal){
+                maxVal = cnt;
+            }
+        }
+    System.out.print(maxVal);
+        
     }
 }
