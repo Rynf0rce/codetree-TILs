1817a73559a606936807fe4ee8487e197ae14695
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-26 14:23:51 +0900

    Title: 좌우로 움직이는 로봇, Time: 1144ms, Memory: 53MB, Status: Passed - CodeTree

--- 231126/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 231126/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -16,24 +16,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(), M = sc.nextInt(), A_idx = 1, B_idx = 1;
-        // test
-        // N = 9;
-        // M = 6;
-        // int[] timeArr = new int[]{20,3,2,19,9,10,2,20,16,20,20,2,6,9,20};
-        // char[] directionArr = new char[]{'R', 'R', 'R', 'L','L','R','L','L','R','L','L','L','L','R','R'};
         double[] arrA = new double[distance], arrB = new double[distance];
         for(int i = 0 ; i < N ; i++){ //int i = 0 ; i < N ; i++
-            int time = /* timeArr[i]; */ sc.nextInt();
-            char direction = /* directionArr[i]; */ sc.next().charAt(0); 
+            int time = sc.nextInt();
+            char direction =  sc.next().charAt(0); 
             for(int j = 0 ; j < time ; j++){
                 arrA[A_idx] = arrA[A_idx-1] + goFnc(direction, 1);
                 A_idx++;
             }
         }
 
-        for(int i = 0 ; i < M ; i++){ // int i = 0 ; i < M ; i++
-            int time = /* timeArr[i]; */ sc.nextInt();
-            char direction = /* directionArr[i]; */ sc.next().charAt(0);
+        for(int i = 0 ; i < M ; i++){
+            int time = sc.nextInt();
+            char direction = sc.next().charAt(0);
             for(int j = 0 ; j < time ; j++){
                 arrB[B_idx] = arrB[B_idx-1] + goFnc(direction, 1);
                 B_idx++;
@@ -49,25 +44,14 @@ public class Main {
         else{
             Arrays.fill(arrA, minCnt, maxCnt, arrA[minCnt-1]);
         }
-
-        // for(int i = 1 ; i < maxCnt ; i++){
-        //     System.out.print(arrA[i] + " ");
-        // }
-        // System.out.println();
-        // for(int i = 1 ; i < maxCnt ; i++){
-        //     System.out.print(arrB[i] + " ");
-        // }
-        //System.out.print(arrA[A_idx - 1] + " ");
-
-        
-        boolean triger = false;
+    
+        boolean triger = false; // 0에서 만나고 이후에 곂치는건 카운팅 하지 않는다.
         int cnt = 0;
         for(int i = 1 ; i < maxCnt ; i++){
             if(arrA[i] == arrB[i]){
                 if(triger){
                     cnt++;
                     triger = false;
-                    // System.out.println("이하" + i);
                 }
             }
             else{
