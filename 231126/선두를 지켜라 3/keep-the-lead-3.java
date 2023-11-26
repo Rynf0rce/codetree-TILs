e5d3aedeb2dfa03dd0e87ebf26658e5b25e983f5
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-26 13:39:46 +0900

    Title: 선두를 지켜라 3, Time: 290ms, Memory: 167MB, Status: Wrong Answer - CodeTree

---
+++ 231126/선두를 지켜라 3/keep-the-lead-3.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_N = 1000 * 1000 * 10;
+    public static final int MAX_M = 1000 * 1000 * 10;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] A_Arr = new double[MAX_N], B_Arr = new double[MAX_M];
+        int N = sc.nextInt(), M = sc.nextInt(), A_idx = 1, B_idx = 1;
+        for(int i = 0 ; i < N ; i++){
+            int velocity = sc.nextInt();
+            int time = sc.nextInt();
+            for(int j = 0 ; j < time ; j++){
+                A_Arr[A_idx] = A_Arr[A_idx - 1] + velocity;
+                A_idx++;
+            }
+        }
+
+        for(int i = 0 ; i < M ; i++){
+            int velocity = sc.nextInt();
+            int time = sc.nextInt();
+            for(int j = 0 ; j < time ; j++){
+                B_Arr[B_idx] = B_Arr[B_idx - 1] + velocity;
+                B_idx++;
+            }
+        }
+
+        for(int i = 0 ; i < A_idx ; i++){
+            //System.out.println(A_Arr[i] + " " + B_Arr[i] + " " + (A_Arr[i] - B_Arr[i]));
+        }
+
+        int scope = Math.min(A_idx, B_idx);
+        double preVal = 0, currentVal = 0, cnt = 0;
+        for(int i = 1 ; i < scope ; i++){
+            currentVal = A_Arr[i] - B_Arr[i];
+            if(i == 1){
+                preVal = currentVal;
+            }
+            else{
+                if(currentVal * preVal < 0){
+                    cnt++;
+                }
+                else if(currentVal * preVal == 0){
+                    cnt++;
+                }
+                preVal = currentVal;
+            }
+            //System.out.println("currentVal" + " " + currentVal + " " + "preVal" + preVal);
+        }
+        System.out.print((int)cnt);
+    }
+}
