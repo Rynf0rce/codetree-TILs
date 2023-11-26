fc0ed940aa853e01beba8f6cc4dbc2af5b8c3b9a
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-26 22:30:40 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 240ms, Memory: 12MB, Status: Passed - CodeTree

--- 231126/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
+++ 231126/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -20,24 +20,35 @@ public class Main {
         arr2D[currentRow][currentColumn] = 1;
         
         for(int i = 1 ; i < arr2D.length * arr2D[0].length ; i++){
-            int tempRow = currentRow + arrRow[direction];
-            int tempColumn = currentColumn + arrColumn[direction];
-            // System.out.println("temp " + tempRow + " " + tempColumn);
-            if(inRange(arr2D, tempRow, tempColumn)){
-                currentRow = tempRow;
-                currentColumn = tempColumn;
+            int cnt = 0;
+            while(cnt < 4){
+                int tempRow = currentRow + arrRow[direction];
+                int tempColumn = currentColumn + arrColumn[direction];
+                // System.out.println("temp " + tempRow + " " + tempColumn);
+                if(inRange(arr2D, tempRow, tempColumn)){
+                    currentRow = tempRow;
+                    currentColumn = tempColumn;
+                    break;
+                }
+                else{
+                    direction = (direction + 1) % 4;
+                    cnt++;
+                }
+            }
+
+            if(cnt >= 4){
+                break;
             }
             else{
-                direction = (direction + 1) % 4;
-                currentRow = currentRow + arrRow[direction];
-                currentColumn = currentColumn + arrColumn[direction];
+                //System.out.println("current " + currentRow + " " + currentColumn);
+                arr2D[currentRow][currentColumn] = i + 1;
             }
+            // System.out.println("temp " + tempRow + " " + tempColumn);
             // System.out.println("current " + currentRow + " " + currentColumn);
-            arr2D[currentRow][currentColumn] = i + 1;
         }
 
         for(int i = 0 ; i < arr2D.length ; i++){
-            for(int j = 0 ; j < arr2D.length ; j++){
+            for(int j = 0 ; j < arr2D[i].length ; j++){
                 System.out.print(arr2D[i][j] + " ");
             }
             System.out.println();
@@ -48,6 +59,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int row = sc.nextInt(), column = sc.nextInt();
+        //row = 1;
+        //column = 6;
         int[][] arr2D = new int[row][column];
         conFnc(arr2D, 0, 0, 1);
     }
