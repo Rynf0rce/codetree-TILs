1c97805dfe926ccc6625dbd8d70d6103e9b567ee
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-26 20:58:43 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231126/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Main {
+    public static boolean inRange(int[][] arr2D, int row, int column){
+        if(row >= 0 && row < arr2D.length && column >= 0 && column < arr2D[0].length && arr2D[row][column] <= 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void conFnc(int[][] arr2D, int sRow, int sColumn, int direction){
+        int[] arrRow = new int[]{-1,0,1,0};
+        int[] arrColumn = new int[]{0,1,0,-1}; // 북동남서 0123
+
+        int currentRow = sRow;
+        int currentColumn = sColumn;
+
+        arr2D[currentRow][currentColumn] = 1;
+        
+        for(int i = 1 ; i < arr2D.length * arr2D[0].length ; i++){
+            int tempRow = currentRow + arrRow[direction];
+            int tempColumn = currentColumn + arrColumn[direction];
+            // System.out.println("temp " + tempRow + " " + tempColumn);
+            if(inRange(arr2D, tempRow, tempColumn)){
+                currentRow = tempRow;
+                currentColumn = tempColumn;
+            }
+            else{
+                direction = (direction + 1) % 4;
+                currentRow = currentRow + arrRow[direction];
+                currentColumn = currentColumn + arrColumn[direction];
+            }
+            // System.out.println("current " + currentRow + " " + currentColumn);
+            arr2D[currentRow][currentColumn] = i + 1;
+        }
+
+        for(int i = 0 ; i < arr2D.length ; i++){
+            for(int j = 0 ; j < arr2D.length ; j++){
+                System.out.print(arr2D[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt(), column = sc.nextInt();
+        int[][] arr2D = new int[row][column];
+        conFnc(arr2D, 0, 0, 1);
+    }
+}
