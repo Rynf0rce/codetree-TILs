0f429b5cb63271dda3e45b7c9a37a2da9eaedf61
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-27 20:34:52 +0900

    Title: 이상한 진수 2, Time: 105ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231127/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static int binaryToDecimal(char[] arr){
+        int sum = 0;
+        for(int i = arr.length -1 ; i >= 0 ; i--){
+            if(arr[i] == '1'){
+                sum += Math.pow(2, Math.abs(arr.length - 1 - i));
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        char[] arr = input.toCharArray();
+        int maxVal = binaryToDecimal(arr);
+        for(int i = arr.length -1 ; i >= 0 ; i--){
+            char[] tempArr = arr.clone();
+            if(tempArr[i] == '0'){
+                tempArr[i] = '1';
+                maxVal = Math.max(maxVal, binaryToDecimal(tempArr));
+            }
+        }
+        System.out.print(maxVal);
+    }
+}
