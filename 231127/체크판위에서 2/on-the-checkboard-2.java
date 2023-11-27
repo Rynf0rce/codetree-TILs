f1b2da49061f98c49ec0d95178591b4286df0ebc
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-27 20:01:42 +0900

    Title: 체크판위에서 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231127/체크판위에서 2/on-the-checkboard-2.java
+++ 231127/체크판위에서 2/on-the-checkboard-2.java
@@ -3,38 +3,36 @@ import java.util.*;
 public class Main {
     public static char[][] arr2D;
 
-    public static int jump(char currentWord, int row, int column){
-        int cnt = 0;
-        for(int i = row + 1 ; i < arr2D.length - 1 ;i++){
-            for(int j = column + 1 ; j < arr2D[i].length - 1 ; j++){
-                if(arr2D[i][j] == currentWord){
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int R = sc.nextInt(), C = sc.nextInt();
+        int R = sc.nextInt();
+        int C = sc.nextInt();
         arr2D = new char[R][C];
-        for(int i = 0 ; i < arr2D.length ; i++){
-            for(int j = 0 ; j < arr2D[i].length ; j++){
+        for(int i = 0 ; i < R ; i++){
+            for(int j = 0 ; j < C ; j++){
                 arr2D[i][j] = sc.next().charAt(0);
             }
         }
-        int num = 0;
-        char comText = arr2D[0][0];
-        // System.out.print(jump(comText, 0, 0));
-        for(int i = 1 ; i < arr2D.length -1 ; i++){
-            for(int j = 1 ; j < arr2D.length - 1 ; j++){
-                if(arr2D[i][j] != comText){
-                    // System.out.println("i and j : " + i + " " + j);
-                    num += jump(comText, i,j);
+
+        char inputWord = arr2D[0][0];
+        int cnt = 0;
+        if(arr2D[0][0] != arr2D[R-1][C-1]){
+            for(int i = 1 ; i < arr2D.length - 1 ; i++){
+                for(int j = 1 ; j < arr2D[i].length - 1 ; j++){
+                    for(int o = i + 1 ; o < arr2D.length - 1 ; o++){
+                        for(int p = j + 1 ; p < arr2D[i].length - 1; p++){
+                            if(inputWord != arr2D[i][j] && inputWord == arr2D[o][p]){
+                                cnt++;
+                            }
+                        }
+                    }
                 }
             }
         }
-        System.out.println(num);
+        else{
+            System.out.print(-1);
+        }
+        System.out.print(cnt);
+        
     }
 }
