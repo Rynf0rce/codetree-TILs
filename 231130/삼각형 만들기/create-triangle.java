fdf1f0dc2d9f49ca3092acdcdf875bac5effb0e9
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-11-30 09:21:53 +0900

    Title: 삼각형 만들기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 231130/삼각형 만들기/create-triangle.java
+++ 231130/삼각형 만들기/create-triangle.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class coordinate implements Comparable<coordinate>{
+class coordinate{
     int x;
     int y;
     int idx;
@@ -12,18 +12,13 @@ class coordinate implements Comparable<coordinate>{
         this.y = y;
         this.idx = idx;
     }
-
-    @Override
-    public int compareTo(coordinate input){
-        return this.x - input.x;
-    }
 }
 
 public class Main {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        int num = sc.nextInt(), area = 0;
         coordinate[] arr = new coordinate[num];
         for(int i = 0 ; i < arr.length ; i++){
             int x = sc.nextInt();
@@ -31,9 +26,6 @@ public class Main {
             arr[i] = new coordinate(x,y,i);
         }
 
-        // Arrays.sort(arr);
-        
-        int area = 0;
         for(int i = 0 ; i < arr.length ; i++){
             for(int j = 0 ; j < arr.length ; j++ ){
                 for(int k = 0 ; k < arr.length ; k++){
@@ -41,7 +33,9 @@ public class Main {
                         continue;
                     }
                     else if(arr[i].y == arr[j].y && ( arr[i].x == arr[k].x || arr[j].x == arr[k].x )){
-                        area = Math.abs ( (arr[i].x - arr[j].x ) * ( arr[i].y - arr[k].y ) );
+                        // System.out.println(arr[i].y + " " + arr[j].y + " " + arr[k].x);
+                        area = Math.max(area, Math.abs( (arr[i].x * arr[j].y + arr[j].x * arr[k].y + arr[k].x * arr[i].y) -
+                            ( arr[j].x * arr[i].y + arr[k].x * arr[j].y + arr[i].x * arr[k].y )  ));
                     }
                 }
             }
