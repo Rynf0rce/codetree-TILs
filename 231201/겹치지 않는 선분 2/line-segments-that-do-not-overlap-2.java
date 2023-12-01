9094ea965574e738adb7f01f1f2fcd6a29ef1467
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-01 10:04:41 +0900

    Title: 겹치지 않는 선분 2, Time: 167ms, Memory: 10MB, Status: Passed - CodeTree

--- 231201/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
+++ 231201/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -58,7 +58,6 @@ public class Main {
         int cnt = 0;
         for(int i = 0 ; i < arr.length ; i++){
             if(arr[i].isolated){
-                System.out.println(i);
                 cnt++;
             }
         }
