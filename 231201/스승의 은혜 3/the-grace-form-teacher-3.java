7d443dc615e8823e3cfabdaf29d3e735f993894f
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-01 13:49:43 +0900

    Title: 스승의 은혜 3, Time: 1319ms, Memory: 24MB, Status: Passed - CodeTree

--- 231201/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 231201/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -11,20 +11,20 @@ public class Main {
             }
         }
 
-        Arrays.sort(arr2D, (a,b) -> (a[0] + a[1]) - (b[0] + b[1]));
-
         int maxVal = Integer.MIN_VALUE;
         for(int i = 0 ; i < arr2D.length; i++){
+            int [][] tempArr2D = new int[arr2D.length][];
+            for(int j = 0; j < arr2D.length; j++){
+                tempArr2D[j] = arr2D[j].clone();
+            }             
+            tempArr2D[i][0] /= 2;
+            Arrays.sort(tempArr2D, (a,b) -> (a[0] + a[1]) - (b[0] + b[1]));
+            
             int tempBudget = B;
             int cnt = 0;
-            for(int j = 0 ; j < arr2D.length ; j++){
-                for(int k = 0 ; k < arr2D[j].length ; k++){
-                    if(i == j && k == 0){
-                        tempBudget -= (arr2D[j][0] / 2);
-                    }
-                    else{
-                        tempBudget -= arr2D[j][k];
-                    }
+            for(int j = 0 ; j < tempArr2D.length ; j++){
+                for(int k = 0 ; k < tempArr2D[j].length ; k++){
+                    tempBudget -= tempArr2D[j][k];
                 }
                 if(tempBudget < 0){
                     break;
