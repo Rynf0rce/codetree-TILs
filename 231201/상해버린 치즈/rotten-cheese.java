3d7b79bde139e0abf7b30d984870cfb4e0e21a2e
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-01 11:31:41 +0900

    Title: 상해버린 치즈, Time: 220ms, Memory: 15MB, Status: Passed - CodeTree

--- 231201/상해버린 치즈/rotten-cheese.java
+++ 231201/상해버린 치즈/rotten-cheese.java
@@ -51,27 +51,26 @@ public class Main {
         Arrays.sort(sickArr);
 
 
-        
-        int targetCheese = 0;
+        // 문제 치즈 하나씩 검사.
         for(int i = 1 ; i < cheeseArr.length ; i++){
+            // sickArr의 조건을 세는변수.
             int cnt = 0;
+            // 어떤 치즈를 먹었는지 모두 검사 의심 치즈를 먹었을 경우 조건에 부합한지 검사.
             for(int j = 0 ; j < timeArr.length ; j++){
                 if(timeArr[j].cheese == i){
                     for(int k = 0 ; k < sickArr.length ; k++){
                         if(timeArr[j].eater == sickArr[k].eater && sickArr[k].time >= timeArr[j].time + 1){
                             cnt++;
-                            // System.out.println("What cheese? : " + i);
-                            //cheeseArr[i] = true;
                         }
                     }
                     if(cnt == sickArr.length){
-                        //System.out.println("What cheese? : " + i);
                         cheeseArr[i] = true;
                     }
                 }
             }
         }
 
+        // 의심 치즈가 1개 일수도 있지만 여러개 일 수 있기에 의심 치즈를 먹은 사람을 체크.
         for(int i = 0 ; i < timeArr.length ; i++){
             for(int j = 1 ; j < cheeseArr.length ; j++){
                 if(timeArr[i].cheese == j && cheeseArr[j]){
@@ -80,6 +79,7 @@ public class Main {
             }
         }
 
+        // 의심 치즈 먹은 사람의 수 검사 (중복해서 먹은 경우가 있을 수 있기 때문에 boolean 배열로 마지막에 검사.)
         for(int i = 1 ; i < manArr.length ; i++){
             if(manArr[i]){
                 pill++;
