b9462c017fc3f36c8dd5dc3ee13b0e18df1bc6ae
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-02 14:23:42 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

--- 231202/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.java
+++ 231202/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static int[] arr;
@@ -6,29 +6,29 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         arr = new int[n];
-        for(int i = 0 ; i < arr.length ; i++){
+        for(int i = 0; i < arr.length ; i++){
             arr[i] = sc.nextInt();
         }
 
         int minVal = Integer.MAX_VALUE;
         for(int i = 0 ; i < arr.length ; i++){
             arr[i] *= 2;
-            for(int j = 0 ; j < arr.length ; i++){
-                int[] tempArr = new int[n-1];
-                int cnt = 0;
+            for(int j = 0 ; j < arr.length ; j++){
                 int sum = 0;
+                int cnt = 0;
+                int[] tempArr = new int[n-1];
                 for(int k = 0 ; k < arr.length ; k++){
-                    if(k==j){
+                    if(k == j){
                         continue;
                     }
                     tempArr[cnt++] = arr[k];
                 }
-
-                for(int k = 0 ; k < arr.length - 2 ; k++){
+                
+                for(int k = 0 ; k < tempArr.length - 1 ; k++){
                     sum += Math.abs(tempArr[k] - tempArr[k+1]);
                 }
                 minVal = Math.min(minVal, sum);
-            } 
+            }
             arr[i] /= 2;
         }
         System.out.println(minVal);
