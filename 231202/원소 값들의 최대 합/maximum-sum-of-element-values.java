6888562c7066dd1990bf0fce2226799e44aa65b8
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-02 21:55:01 +0900

    Title: 원소 값들의 최대 합, Time: 175ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231202/원소 값들의 최대 합/maximum-sum-of-element-values.java
+++ 231202/원소 값들의 최대 합/maximum-sum-of-element-values.java
@@ -4,22 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), m = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0 ; i < arr.length ; i++){
+        int[] arr = new int[n+1];
+        for(int i = 1 ; i < arr.length ; i++){
             arr[i] = sc.nextInt();
         }
 
         int maxVal = Integer.MIN_VALUE;
-        for(int i = 0 ; i < arr.length ; i++){
-            boolean[] chackArr = new boolean[n];
-            int idx = i + 1;
+        for(int i = 1 ; i < arr.length ; i++){
+            boolean[] chackArr = new boolean[n+1];
+            int idx = i;
             int sum = 0;
             for(int j = 0 ; j < m ; j++){
-                chackArr[idx - 1] = true;
-                idx = arr[idx - 1];
+                chackArr[idx] = true;
+                idx = arr[idx];
             }
 
-            for(int j = 0 ; j < chackArr.length ; j++){
+            for(int j = 1 ; j < chackArr.length ; j++){
                 if(chackArr[j]){
                     sum += arr[j];
                 }
