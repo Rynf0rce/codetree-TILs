06816f7ab1574ee63459f4fe5a0a858e3db5dee5
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-03 22:14:25 +0900

    Title: 이상한 폭탄 3, Time: 321ms, Memory: 146MB, Status: Wrong Answer - CodeTree

---
+++ 231203/이상한 폭탄 3/strange-bomb-3.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+    public static int[] arr;
+    public static final int MAX_NUM = 1000000;
+    public static final int MAX_BOMB = 100;
+    public static int numOfBomb(int num, int range){
+        int[] idxArr = new int[MAX_BOMB];
+        int cnt = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] == num){
+                idxArr[cnt++] = i;
+            }
+        }
+        
+        int bombCnt = 1;
+        int maxVal = 0;
+        for(int i = 1 ; i < cnt ; i++){
+            if(idxArr[i] - idxArr[i-1] > range){
+                maxVal = Math.max(maxVal, bombCnt);
+                bombCnt = 1;
+            }
+            else{
+                bombCnt++;
+            }
+        }
+        maxVal = Math.max(maxVal, bombCnt);
+
+        return maxVal;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(), K = sc.nextInt(), maxNum = Integer.MIN_VALUE, maxBomb = Integer.MIN_VALUE;
+        arr = new int[N];
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0 ; i <= MAX_NUM ; i++){
+            int currentVal = numOfBomb(i, K);
+            
+            maxBomb = Math.max(maxBomb, currentVal);
+            if(currentVal == maxBomb){
+                maxNum = i;
+            }
+        }
+        System.out.println(maxNum);
+    }
+}
