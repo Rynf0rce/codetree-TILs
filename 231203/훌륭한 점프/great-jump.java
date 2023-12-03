ce55a5a65009e2f884657c80e29beb951bb1403d
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-03 13:14:13 +0900

    Title: 훌륭한 점프, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231203/훌륭한 점프/great-jump.java
@@ -0,0 +1,36 @@
+// 답을 직접 구하기가 어려운 경우에는 답을 일일히 정해놓고 완전 탐색으로 확인 하는 것도 가능.
+import java.util.*;
+
+public class Main {
+    public static final int MAX_RANGE = 100;
+    public static int[] arr;
+    public static boolean isPossible(int maxVal, int range){
+        int[] idxArr = new int[MAX_RANGE];
+        int idx = 0;
+        for(int i = 0; i < arr.length ; i++){
+            if(arr[i] <= maxVal){
+                idxArr[idx++] = i;
+            }
+        }
+        for(int i = 1 ; i < idx ; i++){
+            if(idxArr[i] - idxArr[i-1] > range){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), k = sc.nextInt();
+        arr = new int[n];
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = arr[0] ; i <= MAX_RANGE ; i++){
+            if(isPossible(i, k)){
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
