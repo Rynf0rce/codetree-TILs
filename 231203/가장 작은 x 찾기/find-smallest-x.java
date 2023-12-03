49c6ca01a4a4da0c3aefa86c5da9575e4ecf1199
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-03 10:51:56 +0900

    Title: 가장 작은 x 찾기, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231203/가장 작은 x 찾기/find-smallest-x.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_RANGE = 10;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr2D = new int[n][2];
+        for(int i = 0; i < arr2D.length ; i++){
+            arr2D[i][0] = sc.nextInt();
+            arr2D[i][1] = sc.nextInt();
+        }
+
+        int minVal = 0;
+        for(int i = 1 ; i <= MAX_RANGE ; i++){
+            int currentVal = i;
+            int cnt = 0;
+            for(int j = 0 ; j < arr2D.length ;j++){
+                currentVal *= 2;
+                if(currentVal < arr2D[j][0] || currentVal > arr2D[j][1]){
+                    break;
+                }
+                cnt++;
+            }
+            if(cnt == arr2D.length){
+                minVal = i;
+                break;
+            }
+        }
+        System.out.print(minVal);
+    }
+}
