bf4ad741c8514a088f6496554c2345eb561d5101
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-03 11:04:36 +0900

    Title: 가장 많이 나온 쌍, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231203/가장 많이 나온 쌍/the-most-frequent-pair.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr2D = new int[m][2];
+        for(int i = 0; i < arr2D.length ; i++){
+            arr2D[i][0] = sc.nextInt();
+            arr2D[i][1] = sc.nextInt();
+        }
+
+        int maxVal = Integer.MIN_VALUE;
+        for(int i = 1 ; i <= n ; i++){
+            int cnt = 0;
+            for(int j = 1 ; j <= n ; j++){
+                for(int k = 0 ; k < arr2D.length ; k++){
+                    if(arr2D[k][0] == i && arr2D[k][1] == j){
+                        cnt++;
+                    }
+                }
+                maxVal = Math.max(maxVal , cnt);
+            }
+        }
+        System.out.println(maxVal);
+
+    }
+}
