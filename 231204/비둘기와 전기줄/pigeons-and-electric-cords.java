029b4b8eace74eda323504034a2deea6348b064b
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-04 15:40:00 +0900

    Title: 비둘기와 전기줄, Time: 185ms, Memory: 10MB, Status: Passed - CodeTree

--- 231204/비둘기와 전기줄/pigeons-and-electric-cords.java
+++ 231204/비둘기와 전기줄/pigeons-and-electric-cords.java
@@ -3,30 +3,29 @@ import java.util.*;
 public class Main {
     public static final int NUM_OF_PIGEON = 10;
     public static int[][] arr2D;
-    public static int[] rightArr = new int[NUM_OF_PIGEON + 1];
-    public static int[] leftArr = new int[NUM_OF_PIGEON + 1];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int N = sc.nextInt(), cnt = 0;
         arr2D = new int[N][2];
         for(int i = 0 ; i < arr2D.length ; i++){
             arr2D[i][0] = sc.nextInt();
             arr2D[i][1] = sc.nextInt();
         }
 
-        for(int i = 0 ; i < arr2D.length ; i++){
-            if(arr2D[i][1] == 0 && leftArr[arr2D[i][0]] < rightArr[arr2D[i][0]] + 1){
-                leftArr[arr2D[i][0]]++;
-            }
-            else if(arr2D[i][1] == 1 && rightArr[arr2D[i][0]] < leftArr[arr2D[i][0]] + 1){
-                rightArr[arr2D[i][0]]++;
-            }
-        }
-
-        int sum = 0;
         for(int i = 1 ; i <= NUM_OF_PIGEON ; i++){
-            sum += Math.min(rightArr[i], leftArr[i]);
+            int location = -1;
+            for(int j = 0 ; j < arr2D.length ; j++){
+                if(arr2D[j][0] == i){
+                    if(location == -1){
+                        location = arr2D[j][1];
+                    }
+                    else if(location != arr2D[j][1]){
+                        location = arr2D[j][1];
+                        cnt++;
+                    }
+                }
+            }
         }
-        System.out.println(sum);
+        System.out.println(cnt);
     }
 }
