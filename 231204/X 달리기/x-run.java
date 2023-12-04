de6f64fc0a88e544a575bc30cc10ffe595cf71e9
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-04 17:44:58 +0900

    Title: X 달리기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 231204/X 달리기/x-run.java
+++ 231204/X 달리기/x-run.java
@@ -1,14 +1,45 @@
 import java.util.*;
 
 public class Main {
+    public static final int MAX_VELOCITY = 100;
+    public static int[] velocityArr = new int[101];
     public static int costOfVelocity(int velocity){
-        if(i <= 1){
-            return 1;
-        }
-        return 
+        return velocity * velocity;
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt(), distance = 0, velocity = 1;
+        int X = sc.nextInt();
+        int maxVelocity = 0;
+        for(int i = 1 ; i <= MAX_VELOCITY; i++){
+            if(X >= costOfVelocity(i)){
+                maxVelocity++;
+            }
+        }
+
+        for(int i = 1 ; i <= maxVelocity ; i++){
+            if(i != maxVelocity){
+                velocityArr[i] += 2;
+            }
+            else{
+                velocityArr[i]++;
+            }
+        }
+
+        int currentDistance = X - costOfVelocity(maxVelocity);
+
+        for(int i = maxVelocity ; i >= 1 ; i--){
+            int quotient = currentDistance / i;
+            velocityArr[i] += quotient;
+            currentDistance = currentDistance % i;
+        }
+
+        int output = 0;
+
+        for(int i = 1 ; i <= maxVelocity ; i++){
+            output += velocityArr[i];
+        }
+
+        System.out.println(output);
     }
 }
