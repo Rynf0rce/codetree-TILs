1cb3a48ca7628c50104eba6de026770052598f1d
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-04 13:50:45 +0900

    Title: 비둘기와 전기줄, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231204/비둘기와 전기줄/pigeons-and-electric-cords.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static final int NUM_OF_PIGEON = 10;
+    public static int[][] arr2D;
+    public static int[] rightArr = new int[NUM_OF_PIGEON + 1];
+    public static int[] leftArr = new int[NUM_OF_PIGEON + 1];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        arr2D = new int[N][2];
+        for(int i = 0 ; i < arr2D.length ; i++){
+            arr2D[i][0] = sc.nextInt();
+            arr2D[i][1] = sc.nextInt();
+        }
+
+        for(int i = 0 ; i < arr2D.length ; i++){
+            if(arr2D[i][1] == 0){
+                leftArr[arr2D[i][0]]++;
+            }
+            else{
+                rightArr[arr2D[i][0]]++;
+            }
+        }
+
+        int sum = 0;
+        for(int i = 1 ; i <= NUM_OF_PIGEON ; i++){
+            sum += Math.min(rightArr[i], leftArr[i]);
+        }
+        System.out.println(sum);
+    }
+}
