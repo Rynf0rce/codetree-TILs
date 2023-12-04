481f459e2382edb5798978e4d8793faefdb5199d
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-04 11:46:29 +0900

    Title: 구간 잘 나누기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 231204/구간 잘 나누기/divide-sections-well.java
+++ 231204/구간 잘 나누기/divide-sections-well.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class Main {
     public static final int MAX_RANGE = 100 * 100;
     public static int[] arr;
+
     public static boolean conFnc(int maxVal, int boundaryNum){
         int cnt = 0;
         int currentVal = 0;
+
         for(int i = 0 ; i < arr.length ; i++){
             if(arr[i] > maxVal || cnt > boundaryNum){
                 return false;
             }
+
             currentVal += arr[i];
 
             if(currentVal > maxVal){
@@ -17,27 +20,23 @@ public class Main {
                 cnt++;
             }
         }
-        return (cnt == boundaryNum);
+        
+        return (cnt <= boundaryNum); // 나누는 수가 딱 맞을 필요는 없음.
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), m = sc.nextInt(), maxVal = 0;;
+        int n = sc.nextInt(), m = sc.nextInt();
         arr = new int[n];
         for(int i = 0; i < arr.length ; i++){
             arr[i] = sc.nextInt();
-            maxVal = Math.max(maxVal, arr[i]);
         }
 
-        int minOfMax = 0;
         for(int i = 1 ; i <= MAX_RANGE ; i++){
             if(conFnc(i,m-1)){
-                minOfMax = i;
+                System.out.println(i);
                 break;
             }
-        }
-        if(minOfMax == 0){
-            minOfMax = maxVal;
-        }
-        System.out.println(minOfMax);
+        }      
     }
 }
