a39f818343f8dd37578da09c1a4f3f67a22d2b08
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-04 11:39:40 +0900

    Title: 구간 잘 나누기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 231204/구간 잘 나누기/divide-sections-well.java
+++ 231204/구간 잘 나누기/divide-sections-well.java
@@ -21,17 +21,23 @@ public class Main {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), m = sc.nextInt();
+        int n = sc.nextInt(), m = sc.nextInt(), maxVal = 0;;
         arr = new int[n];
         for(int i = 0; i < arr.length ; i++){
             arr[i] = sc.nextInt();
+            maxVal = Math.max(maxVal, arr[i]);
         }
 
+        int minOfMax = 0;
         for(int i = 1 ; i <= MAX_RANGE ; i++){
             if(conFnc(i,m-1)){
-                System.out.println(i);
+                minOfMax = i;
                 break;
             }
         }
+        if(minOfMax == 0){
+            minOfMax = maxVal;
+        }
+        System.out.println(minOfMax);
     }
 }
