8f81c9c88d4c0ca9dc2a4a82a87e0107631d216e
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-04 17:30:25 +0900

    Title: X 달리기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231204/X 달리기/x-run.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static int costOfVelocity(int velocity){
+        if(i <= 1){
+            return 1;
+        }
+        return 
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt(), distance = 0, velocity = 1;
+    }
+}
