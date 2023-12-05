628e919b38e2c01e7f6d07d7e230dea34abf2226
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-05 11:00:21 +0900

    Title: 독서실의 거리두기 3, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231205/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
@@ -0,0 +1,40 @@
+// 이전 완전 탐색에서는 모든 경우에 1을 넣고 가장 가까운 사람간의 거리를 최대로 하는 알고리즘을 구현.
+// 여기서는 완전 탐색보다는 논리적으로 구해보도록 하자.
+import java.util.*;
+
+public class Main {
+    public static final int MAX_SEAT = 1000;
+    public static int[] arr = new int[MAX_SEAT];
+    public static int[] gapArr = new int[MAX_SEAT];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String str = sc.next();
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = str.charAt(i) - '0';
+        }
+
+        int cnt = 0, idx = 0;
+        boolean triger = true;
+        for(int i = 0 ; i < N ; i++){
+            cnt++;
+            if(triger && arr[i] == 1){
+                triger = false;
+            }
+            else if(!triger && arr[i] == 1){
+                gapArr[idx++] = cnt - 1;
+                cnt = 1;
+            }
+        }
+
+        int minVal = Integer.MIN_VALUE;
+
+        for(int i = 0 ; i < idx ; i++){
+            minVal = Math.max(minVal, gapArr[i] / 2);
+        }
+
+        System.out.println(minVal);
+
+        
+    }
+}
