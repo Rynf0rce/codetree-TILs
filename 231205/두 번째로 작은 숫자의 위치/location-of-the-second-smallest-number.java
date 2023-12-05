0918da462158c4338a61557de65d6113f994dc58
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-05 16:38:45 +0900

    Title: 두 번째로 작은 숫자의 위치, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231205/두 번째로 작은 숫자의 위치/location-of-the-second-smallest-number.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_RANGE = 101;
+    public static int[] cntArr = new int[MAX_RANGE];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(), output = -1;
+        int[] arr = new int[N];
+        for(int i = 0 ; i < N ; i ++){
+            arr[i] = sc.nextInt();
+            cntArr[arr[i]]++;
+        }
+
+        boolean triger = true;
+        for(int i = 1 ; i < MAX_RANGE - 1 ; i++){
+            if(cntArr[i] > 0){
+                if(triger){
+                    triger = false;
+                    continue;
+                }
+                else{
+                    if(cntArr[i] == 1){
+                        output = i;
+                    }
+                    break;
+                }
+            }
+        }
+        
+        if(output != -1){
+            for(int i = 0 ; i < N ; i++){
+                if(arr[i] == output){
+                    output = i;
+                }
+            }
+        }
+        System.out.println(output + 1);
+    }
+}
