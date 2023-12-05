676cd5c87faa273f1ba5eb5d03c87177ce74bd71
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-05 17:31:52 +0900

    Title: L, R 그리고 B, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 231205/L, R 그리고 B/l-r-and-b.java
+++ 231205/L, R 그리고 B/l-r-and-b.java
@@ -3,6 +3,14 @@ import java.util.*;
 public class Main {
     public static final int WIDTH = 10;
     public static int[][] arr2D = new int[3][2];
+    public static boolean inRange(int row, int column){
+        if(row >= Math.min(arr2D[2][0], arr2D[0][0]) && row <= Math.max(arr2D[2][0], arr2D[0][0]) ){
+            if(column >= Math.min(arr2D[2][1], arr2D[0][1]) && column <= Math.max(arr2D[2][1], arr2D[0][1]) ){
+                return true;
+            }   
+        }
+        return false;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         for(int i = 0 ; i < WIDTH ; i++){
@@ -24,7 +32,7 @@ public class Main {
         }
         int widthRow = Math.abs(arr2D[2][0] - arr2D[0][0]), widthColumn = Math.abs(arr2D[2][1] - arr2D[0][1]);
         int distance = widthRow + widthColumn - 1;
-        if((widthRow == 0 && arr2D[2][0] == arr2D[1][0]) || (widthColumn == 0 && arr2D[2][1] == arr2D[1][1])){
+        if(inRange(arr2D[1][0], arr2D[1][1]) && ((widthRow == 0 && arr2D[2][0] == arr2D[1][0]) || (widthColumn == 0 && arr2D[2][1] == arr2D[1][1]))){
             distance += 2;
         }
         System.out.print(distance);
