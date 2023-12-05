f5e467c846fffc3ee7036095bac8c34069a1b923
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-05 09:41:02 +0900

    Title: 순 간 이 동, Time: 108ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231205/순 간 이 동/teleportation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt(), B = sc.nextInt(), x = sc.nextInt(), y = sc.nextInt();
+        int min_A = Math.min(Math.abs(x-A), Math.abs(y-A)), min_B = Math.min(Math.abs(x-B),Math.abs(y-B));
+        System.out.println(min_A + min_B);
+    }
+}
