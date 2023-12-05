c76ea9dce14e06fe0b5c5c12f938626bc9b91142
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-05 14:06:21 +0900

    Title: 독서실의 거리두기 2, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 231205/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
+++ 231205/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -1,27 +1,34 @@
-// 이전 완전 탐색에서는 모든 경우에 1을 넣고 가장 가까운 사람간의 거리를 최대로 하는 알고리즘을 구현.
-// 여기서는 완전 탐색보다는 논리적으로 구해보도록 하자.
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(), idx = 0;
-        int[] arr = new int[N];
-        String str = sc.next();
-        for(int i = 0 ; i < N ; i++){
-            if(str.charAt(i) == '1'){
-                arr[idx++] = i;
+        int N = sc.nextInt(), idx = 0, cnt = 0 ;
+        char[] arr = sc.next().toCharArray();
+        int[] rangeArr = new int[N];
+
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] == '1'){
+                rangeArr[idx++] = cnt;
+                cnt = 0;
             }
+            if(i == arr.length - 1){
+                rangeArr[idx++] = cnt;
+            }
+            cnt++;
         }
-        
-        int maxVal = Integer.MIN_VALUE;
-        for(int i = 1 ; i < idx ; i++){
+
+        int start = 0, end = 0, maxVal = Integer.MIN_VALUE;
+        for(int i = 0 ; i < idx ; i++){
             int minVal = Integer.MAX_VALUE;
-            for(int j = 1 ; j < idx ; j++){
-                int currentVal = arr[j] - arr[j-1];
-                if(i == j && currentVal > 1){
+            if(i == 0 || i == idx - 1){
+                minVal = Math.min(minVal, rangeArr[i]);
+            }
+            for(int j = 1 ; j < idx -1 ; j++){
+                int currentVal = rangeArr[j];
+                if(j == i){
                     currentVal /= 2;
-                } 
+                }
                 minVal = Math.min(minVal, currentVal);
             }
             maxVal = Math.max(maxVal, minVal);
