fe4c4c0e352bf4699a8551c96f6afb5fbbdd5b5f
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-05 10:19:09 +0900

    Title: 순 간 이 동, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 231205/순 간 이 동/teleportation.java
+++ 231205/순 간 이 동/teleportation.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt(), B = sc.nextInt(), x = sc.nextInt(), y = sc.nextInt();
+        int[] arr = new int[4];
+        int formalDistance = Math.abs(B-A);
         int min_A = Math.min(Math.abs(x-A), Math.abs(y-A)), min_B = Math.min(Math.abs(x-B),Math.abs(y-B));
-        System.out.println(min_A + min_B);
+        System.out.println(Math.min(formalDistance, min_A + min_B));
     }
 }
