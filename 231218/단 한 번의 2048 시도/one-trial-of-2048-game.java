36ce483db70e2f2c741d2583ccd3c3e401ae12b4
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-18 10:54:50 +0900

    Title: 단 한 번의 2048 시도, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

--- 231218/단 한 번의 2048 시도/one-trial-of-2048-game.java
+++ 231218/단 한 번의 2048 시도/one-trial-of-2048-game.java
@@ -1,111 +1,97 @@
 import java.util.*;
 
 public class Main {
-    public static int[][] area = new int[4][4];
-    public static void gravity(char dir){
-        int[][] temp = new int[4][4];
-        switch(dir){
-            case 'L' :
-                for(int i = 0 ; i < 4 ; i++){
-                    int idx = 0;
-                    int value = 0;
-                    for(int j = 0 ; j < 4 ; j++){
-                        if(area[i][j] != 0){
-                            if(value != area[i][j]){
-                                value = area[i][j];
-                                temp[i][idx++] = area[i][j];
-                            }
-                            else{
-                                temp[i][--idx] = value * 2;
-                                idx++;
-                                value = 0;
-                            }
-                        }
-                    }
-                }
-                break;
-            case 'R' :
-                for(int i = 0 ; i < 4 ; i++){
-                    int idx = 3;
-                    int value = 0;
-                    for(int j = 3 ; j >= 0 ; j--){
-                        if(area[i][j] != 0){
-                            if(area[i][j] != 0){
-                                if(value != area[i][j]){
-                                    value = area[i][j];
-                                    temp[i][idx--] = area[i][j];
-                                }
-                                else{
-                                    temp[i][++idx] = value * 2;
-                                    idx--;
-                                    value = 0;
-                                }
-                            }
-                        }
-                    }
-                }
-                break;
-            case 'U' :
-                for(int i = 0 ; i < 4 ; i++){
-                    int idx = 0;
-                    int value = 0;
-                    for(int j = 0 ; j < 4 ; j++){
-                        if(area[j][i] != 0){
-                            if(area[j][i] != 0){
-                                if(value != area[j][i]){
-                                    value = area[j][i];
-                                    temp[idx++][i] = area[j][i];
-                                }
-                                else{
-                                    temp[--idx][i] = value * 2;
-                                    idx++;
-                                    value = 0;
-                                }
-                            }
-                        }
-                    }
-                }
-                break;
-            case 'D' :
-                for(int i = 0 ; i < 4 ; i++){
-                    int idx = 3;
-                    int value = 0;
-                    for(int j = 3 ; j >= 0 ; j--){
-                        if(area[j][i] != 0){
-                            if(value != area[j][i]){
-                                temp[idx--][i] = area[j][i];
-                                value = area[j][i];
-                            }
-                            else{
-                                temp[++idx][i] = value * 2;
-                                idx--;
-                                value = 0;
-                            }
-                        }
-                    }
-                }
-                break;
+    public static final int ASCII_NUM = 128;
+    public static final int MAX_N = 4;
+
+    public static int n = 4;
+
+    public static int[][] area = new int[MAX_N][MAX_N];
+    public static int[][] tempArea = new int[MAX_N][MAX_N];
+
+    public static void spin(){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                tempArea[i][j] = 0;
+            }
         }
 
-        for(int i = 0 ; i < 4 ; i++){
-            for(int j = 0 ; j < 4 ; j++){
-                area[i][j] = temp[i][j];
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                tempArea[i][j] = area[n - 1 - j][i];
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                area[i][j] = tempArea[i][j];
             }
         }
     }
+
+    public static void drop(){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                tempArea[i][j] = 0;
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            int idx = n - 1;
+            int value = 0;
+            for(int j = n - 1 ; j >= 0 ; j--){
+                if(area[j][i] != 0){
+                    if(value != area[j][i]){
+                        value = area[j][i];
+                        tempArea[idx--][i] = area[j][i];
+                    }
+                    else{
+                        tempArea[idx + 1][i] = value * 2;
+                        value = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                area[i][j] = tempArea[i][j];
+            }
+        }
+    }
+
+    public static void gravity(int spinCnt){
+        for(int i = 0 ; i < spinCnt ; i++){
+            spin();
+        }
+
+        drop();
+
+        for(int i = 0 ; i < 4 - spinCnt ; i++){
+            spin();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for(int i = 0 ; i < 4 ; i++){
-            for(int j = 0 ; j < 4 ; j++){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
                 area[i][j] = sc.nextInt();
             }
         }
+
         char dir = sc.next().charAt(0);
 
-        gravity(dir);
+        int[] dirMapper = new int[ASCII_NUM];
+        dirMapper['D'] = 0;
+        dirMapper['R'] = 1;
+        dirMapper['U'] = 2;
+        dirMapper['L'] = 3;
 
-        for(int i = 0 ; i < 4 ; i++){
-            for(int j = 0 ; j < 4 ; j++){
+        gravity(dirMapper[dir]);
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
                 System.out.print(area[i][j] + " ");
             }
             System.out.println();
