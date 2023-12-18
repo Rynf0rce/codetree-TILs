d34e22f911b72f03ce481e3f241c2fff452ea144
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-18 17:09:22 +0900

    Title: 2차원 폭발 게임, Time: 952ms, Memory: 82MB, Status: Passed - CodeTree

--- 231218/2차원 폭발 게임/The-2D-bomb-game.java
+++ 231218/2차원 폭발 게임/The-2D-bomb-game.java
@@ -4,22 +4,55 @@ public class Main {
     public static final int MAX_RANGE = 100;
     public static int[][] area = new int[MAX_RANGE][MAX_RANGE];
 
+    public static boolean gravity(int N){
+        int[][] temp = new int[MAX_RANGE][MAX_RANGE];
+        boolean affected = false;
+        for(int i = 0 ; i < N ; i++){
+            int idx = N - 1;
+            for(int j = N - 1; j >= 0 ; j--){
+                if(area[j][i] != 0){
+                    temp[idx--][i] = area[j][i];
+                }
+            }
+        }
+
+        for(int i = 0 ; i < N ; i++){
+            for(int j = 0 ; j < N ; j++){
+                if(area[i][j] != temp[i][j]){
+                    affected = true;
+                }
+                area[i][j] = temp[i][j];
+            }
+        }
+
+        return affected;
+    }
+
     public static void bomb(int N, int M){
         int[][] temp = new int[MAX_RANGE][MAX_RANGE];
         for(int i = 0 ; i < N ; i++){
-            int value = area[N - 1][i];
+            int value = -1;
             int cnt = 1;
             int idx = N - 1;
-            for(int j = N - 2 ; j >= 0 ; j--){
+            for(int j = N - 1 ; j >= 0 ; j--){
                 if(area[j][i] != 0){
+                    if(value == -1){
+                        value = area[j][i];
+                        cnt = 1;
+                        continue;
+                    }
+
+
                     if(value == area[j][i]){
                         cnt++;
                     }
                     else{
-                        if(cnt < M){
-                            for(int k = 0 ; k < cnt ; k++){
-                                temp[idx--][i] = value;
-                            }
+                        if(cnt >= M){
+                            value = 0;
+                        }
+
+                        for(int k = 0 ; k < cnt ; k++){
+                            temp[idx--][i] = value;
                         }
 
                         value = area[j][i];
@@ -27,7 +60,7 @@ public class Main {
                     }
                 } 
             }
-            if(cnt < M){
+            if(cnt < M && value > 0){
                 for(int k = 0 ; k < cnt ; k++){
                     temp[idx--][i] = value;
                 }
@@ -58,8 +91,15 @@ public class Main {
 
     public static void conFnc(int N, int M){
         bomb(N, M);
+        while(gravity(N)){
+            bomb(N, M);
+        }
+
         spin(N);
-        bomb(N, M);
+
+        while(gravity(N)){
+            bomb(N, M);
+        }
     }
 
     public static void main(String[] args) {
