7523b8b66db5693019a93e02ccb1974f1854c8e9
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-21 20:26:33 +0900

    Title: 겹치지 않게 선분 고르기, Time: 194ms, Memory: 12MB, Status: Passed - CodeTree

--- 231221/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 231221/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -18,49 +18,31 @@ class pair implements Comparable<pair>{
 public class Main {
     public static final int MAX_INPUT = 15;
     public static ArrayList<pair> vector = new ArrayList<>();
-    public static ArrayList<Boolean> select = new ArrayList<>();
+    public static ArrayList<Integer> select = new ArrayList<>();
     public static int n = 0;
 
-    public static boolean outRange(int x1, int x2, int y1, int y2){
-        return (y1 > x2 || y2 < x1);
+    public static boolean outRange(pair p1, pair p2){
+        return (p1.x2 < p2.x1 || p2.x2 < p1.x1);
     }
     
-    public static int chackFold(){
+    public static int conFnc(int idx){
         int cnt = 0;
-        int x2 = 0;
-        for(int i = 0 ; i < select.size() ; i++){
-            if(select.get(i)){
-                cnt++;
-                if(x2 == 0){
-                    x2 = vector.get(i).x2;
-                }
-                else{
-                    if(x2 >= vector.get(i).x1){
-                        return -1;
-                    }
-                }
+
+        if(idx >= n){
+            return select.size() + 1;
+        }
+
+        for(int i = idx + 1 ; i < n ; i++){
+            if(outRange(vector.get(idx), vector.get(i))){
+                select.add(i);
+                cnt = Math.max(cnt, conFnc(i));
+                select.remove(select.size() - 1);
             }
         }
-        
+
+        cnt = Math.max(cnt, conFnc(idx + 1));
+
         return cnt;
-    }
-
-    public static int conFnc(){
-        int maxVal = 0;
-        if(select.size() >= n){
-            return chackFold();
-        }
-        for(int i = 0 ; i < n ; i++){
-            select.add(true);
-            maxVal = Math.max(maxVal, conFnc());
-            select.remove(select.size() - 1);
-
-            select.add(false);
-            maxVal = Math.max(maxVal, conFnc());
-            select.remove(select.size() - 1);
-        }
-        
-        return maxVal;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -73,6 +55,6 @@ public class Main {
 
         Collections.sort(vector);
 
-        System.out.println(conFnc());
+        System.out.println(conFnc(0));
     }
 }
