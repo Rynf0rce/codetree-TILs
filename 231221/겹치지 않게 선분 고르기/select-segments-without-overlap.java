b137d2a06140fbd362e991c5d0c7ec4cb080a88b
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-21 19:46:53 +0900

    Title: 겹치지 않게 선분 고르기, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231221/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 231221/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class pair{
+class pair implements Comparable<pair>{
     int x1;
     int x2;
 
@@ -8,26 +8,34 @@ class pair{
         this.x1 = x1;
         this.x2 = x2;
     }
+
+    @Override
+    public int compareTo(pair input){
+        return this.x1 - input.x1;
+    }
 }
 
 public class Main {
     public static final int MAX_INPUT = 15;
     public static ArrayList<pair> vector = new ArrayList<>();
     public static ArrayList<Boolean> select = new ArrayList<>();
-    public static HashSet<Integer> lineMap = new HashSet<>();
     public static int n = 0;
+
+    public static boolean outRange(int x1, int x2, int y1, int y2){
+        return (y1 > x2 || y2 < x1);
+    }
     
     public static int chackFold(){
-        lineMap.clear();
         int cnt = 0;
+        int x2 = 0;
         for(int i = 0 ; i < select.size() ; i++){
             if(select.get(i)){
                 cnt++;
-                for(int j = vector.get(i).x1 ; j <= vector.get(i).x2 ; j++){
-                    if(!lineMap.contains(j)){
-                        lineMap.add(j);
-                    }
-                    else{
+                if(x2 == 0){
+                    x2 = vector.get(i).x2;
+                }
+                else{
+                    if(x2 > vector.get(i).x1){
                         return -1;
                     }
                 }
@@ -62,6 +70,8 @@ public class Main {
             int x2 = sc.nextInt();
             vector.add(new pair(x1, x2));
         }
+
+        Collections.sort(vector);
 
         System.out.println(conFnc());
     }
