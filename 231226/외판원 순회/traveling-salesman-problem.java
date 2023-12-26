49ce4d893c876efbacfb89207ac2d472de8d1c02
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-26 17:51:13 +0900

    Title: 외판원 순회, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231226/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,46 @@
+// Traveling Salesman problem (TSP)
+
+import java.util.*;
+
+public class Main {
+    public static final int MAX_RANGE = 10;
+    public static int[][] table = new int[MAX_RANGE][MAX_RANGE];
+    public static ArrayList<Integer> vector = new ArrayList<>();
+    public static boolean[] visited = new boolean[MAX_RANGE];
+    public static int minVal = Integer.MAX_VALUE;
+    public static int n;
+
+    public static void TSP(int curIdx, int distance){
+        if(vector.size() >= n){
+            minVal = Math.min(minVal, distance + table[curIdx][0]);
+            return;
+        }
+
+        for(int i = 1 ; i < n ; i++){
+            if(visited[i]){
+                continue;
+            }
+
+            visited[i] = true;
+            vector.add(i);
+            TSP(i, distance + table[curIdx][i]);
+            vector.remove(vector.size() - 1);
+            visited[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                table[i][j] = sc.nextInt();
+            }
+        }
+
+        vector.add(0);
+        TSP(0, 0);
+
+        System.out.print(minVal);
+    }
+}
