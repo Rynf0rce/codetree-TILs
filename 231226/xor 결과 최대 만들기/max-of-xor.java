72fd22d55e516c383daf71527ca710d69ff59674
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-26 09:38:46 +0900

    Title: xor 결과 최대 만들기, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 231226/xor 결과 최대 만들기/max-of-xor.java
+++ 231226/xor 결과 최대 만들기/max-of-xor.java
@@ -1,6 +1,47 @@
 import java.util.*;
 
 public class Main {
+    public static int n;
+    public static int m;
+    public static int[] input;
+    public static int maxVal = Integer.MIN_VALUE;
+
+
+    public static void combination(int idx, int cnt, int currVal){
+        if(cnt >= m){
+            maxVal = Math.max(maxVal, currVal);
+            return;
+        }
+
+        if(idx >= n){
+            return;
+        }
+        
+        combination(idx + 1, cnt, currVal);
+
+        combination(idx + 1, cnt + 1, currVal ^ input[idx]);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        input = new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+            input[i] = sc.nextInt();
+        }
+
+        combination(0, 0, 0);
+        System.out.print(maxVal);
+    }
+}
+
+/* 처음
+
+import java.util.*;
+
+public class Main {
     public static final int MAX_INPUT = 1000000;
     public static int[] table = new int[20];
     public static int n;
@@ -72,3 +113,5 @@ public class Main {
 
     }
 }
+
+*/
