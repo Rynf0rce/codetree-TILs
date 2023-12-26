13652c5172399ffba84477412f37474e4eba6c8d
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-26 13:02:50 +0900

    Title: 단순한 동전 챙기기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 231226/단순한 동전 챙기기/collect-coins-easy.java
+++ 231226/단순한 동전 챙기기/collect-coins-easy.java
@@ -43,7 +43,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        for(int i = 0 ; i < 4; i++){
+        for(int i = 0 ; i < N; i++){
             String str = sc.next();
             for(int j = 0 ; j < str.length() ; j++){
                 if(str.charAt(j) >= '0' && str.charAt(j) <= '9'){
