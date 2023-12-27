95153a29fdf5aa3dd644db4025bc42536705ab7f
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-27 09:42:19 +0900

    Title: 그래프 탐색, Time: 472ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231227/그래프 탐색/graph-traversal.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_NODES = 1000;
+    public static ArrayList<Integer>[] graph = new ArrayList[MAX_NODES + 1];
+    public static boolean[] visited = new boolean[MAX_NODES + 1];
+    public static int n;
+
+    public static void DFS(int curIdx){
+        for(int i = 0 ; i < graph[curIdx].size() ; i++){
+            int nextIdx = graph[curIdx].get(i);
+            if(visited[nextIdx]){
+                continue;
+            }
+
+            visited[nextIdx] = true;
+            DFS(nextIdx);
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for(int i = 1 ; i <= n ; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i = 0 ; i < m ; i++){
+            int startIdx = sc.nextInt();
+            int endIdx = sc.nextInt();
+
+            graph[startIdx].add(endIdx);
+            graph[endIdx].add(startIdx);
+        }
+
+        int rootIdx = 1;
+        visited[rootIdx] = true;
+        DFS(1);
+
+        int output = 0;
+        for(int i = 2 ; i <= n ; i++){
+            if(visited[i]){
+                output++;
+            }
+        }
+        System.out.println(output);
+    }
+}
