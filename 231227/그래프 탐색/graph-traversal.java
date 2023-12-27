4bef9ba2b83cc8ed0093ea1fa33df55dd8c3df55
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-27 09:43:53 +0900

    Title: 그래프 탐색, Time: 474ms, Memory: 23MB, Status: Passed - CodeTree

--- 231227/그래프 탐색/graph-traversal.java
+++ 231227/그래프 탐색/graph-traversal.java
@@ -5,6 +5,54 @@ public class Main {
     public static ArrayList<Integer>[] graph = new ArrayList[MAX_NODES + 1];
     public static boolean[] visited = new boolean[MAX_NODES + 1];
     public static int n;
+    public static int cnt = 0;
+
+    public static void DFS(int curIdx){
+        for(int i = 0 ; i < graph[curIdx].size() ; i++){
+            int nextIdx = graph[curIdx].get(i);
+            if(visited[nextIdx]){
+                continue;
+            }
+
+            visited[nextIdx] = true;
+            cnt++;
+            DFS(nextIdx);
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for(int i = 1 ; i <= n ; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i = 0 ; i < m ; i++){
+            int startIdx = sc.nextInt();
+            int endIdx = sc.nextInt();
+
+            graph[startIdx].add(endIdx);
+            graph[endIdx].add(startIdx);
+        }
+
+        int rootIdx = 1;
+        visited[rootIdx] = true;
+        DFS(1);
+
+        System.out.println(cnt);
+    }
+}
+
+/*
+import java.util.*;
+
+public class Main {
+    public static final int MAX_NODES = 1000;
+    public static ArrayList<Integer>[] graph = new ArrayList[MAX_NODES + 1];
+    public static boolean[] visited = new boolean[MAX_NODES + 1];
+    public static int n;
 
     public static void DFS(int curIdx){
         for(int i = 0 ; i < graph[curIdx].size() ; i++){
@@ -48,3 +96,4 @@ public class Main {
         System.out.println(output);
     }
 }
+*/
