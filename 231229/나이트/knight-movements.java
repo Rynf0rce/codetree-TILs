4e8e0872cf9945296820e8470585905d02a9da1f
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-29 10:16:21 +0900

    Title: 나이트, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231229/나이트/knight-movements.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class point{
+    int row;
+    int col;
+
+    public point(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+}
+
+public class Main {
+    public static final int RAX_RANGE = 100;
+    public static final int NUM_DIRECTION = 8;
+
+    public static int[][] table = new int[RAX_RANGE][RAX_RANGE];
+    public static int[][] visited = new int[RAX_RANGE][RAX_RANGE];
+    public static int[] arrRow = new int[]{-2, -1, 1, 2, 2, 1, -1, -2};
+    public static int[] arrCol = new int[]{1, 2, 2, 1, -1, -2, -2, -1};
+
+    public static Queue<point> q = new LinkedList<>();
+
+    public static int n;
+
+    public static boolean inRange(int row, int col){
+        return (row >= 0 && col >= 0 && row < n && col < n);
+    }
+
+    public static boolean canGo(int row, int col){
+        if(!inRange(row, col) || visited[row][col] > 0){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void push(int row, int col, int distance){
+        visited[row][col] = distance;
+        q.add(new point(row, col));
+    }
+
+    public static void BFS(){
+        while(!q.isEmpty()){
+            point curPoint = q.poll();
+            for(int i = 0 ; i < NUM_DIRECTION ; i++){
+                int postRow = curPoint.row + arrRow[i];
+                int postCol = curPoint.col + arrCol[i];
+                if(canGo(postRow, postCol)){
+                    push(postRow, postCol, visited[curPoint.row][curPoint.col] + 1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int r1 = sc.nextInt() - 1, c1 = sc.nextInt() - 1, r2 = sc.nextInt() - 1, c2 = sc.nextInt() - 1;
+
+        if(canGo(r1, c1)){
+            push(r1, c1, 1);
+        }
+
+        BFS();
+
+        System.out.print(visited[r2][c2] == 0 ? -1 : visited[r2][c2] - 1);
+        
+    }
+}
