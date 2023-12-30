683746dc0dfa3a9ae50828d10adb18448fa6dea2
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2023-12-30 17:12:17 +0900

    Title: 사각형 채우기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 231230/사각형 채우기/rectangle-fill.java
+++ 231230/사각형 채우기/rectangle-fill.java
@@ -9,7 +9,7 @@ public class Main {
         arr[0] = 1;
         arr[1] = 1;
         for(int i = 2 ; i <= n ; i++){
-            arr[i] = arr[i - 1] + arr[i - 2];
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
         }
         System.out.println(arr[n]);
     }
