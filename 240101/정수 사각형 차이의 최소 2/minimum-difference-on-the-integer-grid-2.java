74dfca6f5979f5681feb7ca33791a8fb163d3b43
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-01 14:46:49 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240101/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
+++ 240101/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -1,40 +1,12 @@
 import java.util.*;
 
-class range{
-    int min;
-    int max;
-
-    public range(int min, int max){
-        this.min = min;
-        this.max = max;
-    }
-
-    public int width(){
-        return max - min;
-    }
-}
-
 public class Main {
     public static final int MAX_RANGE = 100;
     public static int[][] table = new int[MAX_RANGE][MAX_RANGE];
-    public static range[][] DP = new range[MAX_RANGE][MAX_RANGE];
+    public static int[][] minTable = new int[MAX_RANGE][MAX_RANGE];
+    public static int[][] maxTable = new int[MAX_RANGE][MAX_RANGE];
     public static int n;
-
-    public static range calRange(range preRange, int row, int col){
-        int value = table[row][col];
-        int min = preRange.min;
-        int max = preRange.max;
-        if(value > max){
-            return new range(min, value);
-        }
-        else if(value < min){
-            return new range(value, max);
-        }
-        else{
-            return preRange;
-        }
-    }
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
@@ -44,30 +16,36 @@ public class Main {
             }
         }
 
-        DP[0][0] = new range(table[0][0], table[0][0]);
+        minTable[0][0] = table[0][0];
+        maxTable[0][0] = table[0][0];
 
         for(int i = 1 ; i < n ; i++){
-            DP[0][i] = calRange(DP[0][i-1], 0, i);
-        }
-
-        for(int i = 1 ; i < n ; i++){
-            DP[i][0] = calRange(DP[i-1][0], i, 0);
+            minTable[0][i] = Math.min(table[0][i], minTable[0][i - 1]);
+            maxTable[0][i] = Math.max(table[0][i], maxTable[0][i - 1]);
+            minTable[i][0] = Math.min(table[i][0], minTable[i - 1][0]);
+            maxTable[i][0] = Math.max(table[i][0], maxTable[i - 1][0]);
         }
 
         for(int i = 1 ; i < n ; i++){
             for(int j = 1 ; j < n ; j++){
-                range upRange = calRange(DP[i - 1][j], i, j);
-                range rigthRange = calRange(DP[i][j - 1], i, j);
-
-                if(upRange.width() < rigthRange.width()){
-                    DP[i][j] = upRange;
+                int minOfMax = Math.max(minTable[i - 1][j], minTable[i][j - 1]);
+                if(table[i][j] > minOfMax){
+                    minTable[i][j] = minOfMax;
                 }
                 else{
-                    DP[i][j] = rigthRange;
+                    minTable[i][j] = table[i][j];
+                }
+                
+                int maxOfMin = Math.min(maxTable[i - 1][j], maxTable[i][j - 1]);
+                if(table[i][j] < maxOfMin){
+                    maxTable[i][j] = maxOfMin;
+                }
+                else{
+                    maxTable[i][j] = table[i][j];
                 }
             }
         }
 
-        System.out.print(DP[n - 1][n - 1].width());
+        System.out.print(Math.abs(maxTable[n - 1][n - 1] - minTable[n - 1][n - 1]));
     }
 }
