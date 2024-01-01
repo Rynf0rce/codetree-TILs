2f55da44574c98109f342e0162a736a88265ddc2
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-01 16:52:48 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 348ms, Memory: 21MB, Status: Passed - CodeTree

--- 240101/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
+++ 240101/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -1,51 +1,71 @@
 import java.util.*;
 
-public class Main {
+ public class Main {
     public static final int MAX_RANGE = 100;
+    public static final int MAX_INT = MAX_RANGE * 3;
     public static int[][] table = new int[MAX_RANGE][MAX_RANGE];
-    public static int[][] minTable = new int[MAX_RANGE][MAX_RANGE];
-    public static int[][] maxTable = new int[MAX_RANGE][MAX_RANGE];
+    public static int[][] DP = new int[MAX_RANGE][MAX_RANGE];
     public static int n;
-    
+
+    public static int conFnc(int minimal){
+        if(table[0][0] < minimal || table[n - 1][n - 1] < minimal){
+            return 0;
+        }
+
+        DP[0][0] = table[0][0];
+
+        for(int i = 1 ; i < n ; i++){
+            if(table[0][i] < minimal){
+                DP[0][i] = MAX_INT;
+            }
+            else{
+                DP[0][i] = Math.max(DP[0][i - 1], table[0][i]);
+            }
+
+            if(table[i][0] < minimal){
+                DP[i][0] = MAX_INT;
+            }
+            else{
+                DP[i][0] = Math.max(DP[i - 1][0], table[i][0]);
+            }
+        }
+
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 1 ; j < n ; j++){
+                if(table[i][j] < minimal){
+                    DP[i][j] = MAX_INT;
+                }
+                else{
+                    int upMaxVal = Math.max(DP[i - 1][j], table[i][j]);
+                    int rightMaxVal = Math.max(DP[i][j - 1], table[i][j]);
+                    DP[i][j] = Math.min(upMaxVal, rightMaxVal);
+                }
+            }
+        }
+
+        int output = DP[n - 1][n - 1] - minimal;
+        return (output < 1 ? 0 : DP[n - 1][n - 1] - minimal);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+        int minVal = Integer.MAX_VALUE;
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < n ; j++){
                 table[i][j] = sc.nextInt();
             }
         }
 
-        minTable[0][0] = table[0][0];
-        maxTable[0][0] = table[0][0];
-
-        for(int i = 1 ; i < n ; i++){
-            minTable[0][i] = Math.min(table[0][i], minTable[0][i - 1]);
-            maxTable[0][i] = Math.max(table[0][i], maxTable[0][i - 1]);
-            minTable[i][0] = Math.min(table[i][0], minTable[i - 1][0]);
-            maxTable[i][0] = Math.max(table[i][0], maxTable[i - 1][0]);
-        }
-
-        for(int i = 1 ; i < n ; i++){
-            for(int j = 1 ; j < n ; j++){
-                int minOfMax = Math.max(minTable[i - 1][j], minTable[i][j - 1]);
-                if(table[i][j] > minOfMax){
-                    minTable[i][j] = minOfMax;
-                }
-                else{
-                    minTable[i][j] = table[i][j];
-                }
-                
-                int maxOfMin = Math.min(maxTable[i - 1][j], maxTable[i][j - 1]);
-                if(table[i][j] < maxOfMin){
-                    maxTable[i][j] = maxOfMin;
-                }
-                else{
-                    maxTable[i][j] = table[i][j];
-                }
+        for(int i = 1 ; i <= 100 ; i++){
+            int output = conFnc(i);
+            if(output == 0){
+                break;
             }
+
+            minVal = Math.min(minVal, output);
         }
 
-        System.out.print(Math.abs(maxTable[n - 1][n - 1] - minTable[n - 1][n - 1]));
+        System.out.println(minVal);
     }
 }
