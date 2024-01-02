7fd7a30cb509feecfb8897ae3ad50bee0645381e
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-02 15:57:33 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240102/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_RANGE = 1000;
+    public static final int INVAILED = Integer.MIN_VALUE;
+    public static int[] arr = new int[MAX_RANGE + 1];
+    public static int[] DP = new int[MAX_RANGE + 1];
+    public static boolean[] upDown = new boolean[MAX_RANGE + 1];
+
+    public static int n;
+
+    public static void initialize(){
+        for(int i = 0 ; i < n ; i++){
+            DP[i] = INVAILED;
+            upDown[i] = true;
+        }
+        DP[0] = 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        for(int i = 1 ; i <= n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        initialize();
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(arr[j] == INVAILED || arr[i] == arr[j] || ( (arr[i] > arr[j]) && upDown[j] == false) ){
+                    continue;
+                }
+
+                // DP[i] = Math.max(DP[i], DP[j] + 1);
+
+                if(DP[i] < DP[j] + 1){
+                    DP[i] = DP[j] + 1;
+                    if(arr[i] < arr[j]){
+                        upDown[i] = false;
+                    }
+                    else{
+                        upDown[i] = true;
+                    }
+                } 
+            }
+        }
+
+        int output = INVAILED;
+
+        for(int i = 1 ; i <= n ; i++){
+            output = Math.max(output, DP[i]);
+            // System.out.println(DP[i] + " " + upDown[i]);
+        }
+
+        System.out.print(output);
+    }
+}
