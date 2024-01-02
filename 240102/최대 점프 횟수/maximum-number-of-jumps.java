b32f05e8fd109c3dc8223d4de13d4dfc6843c2d3
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-02 13:54:03 +0900

    Title: 최대 점프 횟수, Time: 251ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240102/최대 점프 횟수/maximum-number-of-jumps.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_RANGE = 1000;
+    public static final int INVALID = Integer.MIN_VALUE;
+    public static int[] arr = new int[MAX_RANGE];
+    public static int[] DP = new int[MAX_RANGE];
+    public static int n;
+
+    public static void initalize(){
+        for(int i = 1 ; i < n ; i++){
+            DP[i] = INVALID;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int output = INVALID;
+
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        initalize();
+
+        for(int i = 1 ; i < n ; i++){
+            boolean triger = true;
+            for(int j = 0 ; j < i ; j++){
+                if(i <= arr[j] + j){
+                    DP[i] = Math.max(DP[i], DP[j] + 1);
+                    triger = false;
+                }
+            }
+            if(triger){
+                break;
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            output = Math.max(output, DP[i]);
+        }
+        
+        System.out.print(output);
+    }
+}
