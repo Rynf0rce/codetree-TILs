7fc00163d35efa7c4fa75392f4fb9043a0f226e5
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-02 16:35:34 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 215ms, Memory: 13MB, Status: Passed - CodeTree

--- 240102/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.java
+++ 240102/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.java
@@ -4,60 +4,50 @@ public class Main {
     public static final int MAX_RANGE = 1000;
     public static final int INVAILED = Integer.MIN_VALUE;
     public static int[] arr = new int[MAX_RANGE + 1];
-    public static int[] DP = new int[MAX_RANGE + 1];
-    public static boolean[] upDown = new boolean[MAX_RANGE + 1];
+    public static int[] plusDP = new int[MAX_RANGE + 1];
+    public static int[] minusDP = new int[MAX_RANGE + 1];
 
     public static int n;
 
     public static void initialize(){
         for(int i = 0 ; i < n ; i++){
-            DP[i] = INVAILED;
-            upDown[i] = true;
+            plusDP[i] = 1;
+            minusDP[i] = 1;
         }
-        DP[0] = 0;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
 
-        for(int i = 1 ; i <= n ; i++){
+        for(int i = 0 ; i < n ; i++){
             arr[i] = sc.nextInt();
         }
 
         initialize();
 
-        for(int i = 1 ; i <= n ; i++){
+        for(int i = 1 ; i < n ; i++){
             for(int j = 0 ; j < i ; j++){
-                if(arr[j] == INVAILED || arr[i] == arr[j] ){
+                if(arr[i] == arr[j] ){
                     continue;
                 }
 
-                if((arr[i] > arr[j]) && upDown[j] == false){
-                    DP[i] = Math.max(DP[i], 2);
-                    upDown[i] = true;
-                    continue;
+                if((arr[i] > arr[j])){
+                    plusDP[i] = Math.max(plusDP[i], plusDP[j] + 1);
                 }
-
-                // DP[i] = Math.max(DP[i], DP[j] + 1);
-
-                if(DP[i] < DP[j] + 1){
-                    DP[i] = DP[j] + 1;
-                    if(arr[i] < arr[j]){
-                        upDown[i] = false;
-                    }
-                    else{
-                        upDown[i] = true;
-                    }
-                } 
+                else{
+                    minusDP[i] = Math.max(minusDP[i], plusDP[j] + 1);
+                    minusDP[i] = Math.max(minusDP[i], minusDP[j] + 1);
+                }
             }
         }
 
         int output = INVAILED;
 
-        for(int i = 1 ; i <= n ; i++){
-            output = Math.max(output, DP[i]);
-            // System.out.println(DP[i] + " " + upDown[i]);
+        for(int i = 1 ; i < n ; i++){
+            output = Math.max(output, plusDP[i]);
+            output = Math.max(output, minusDP[i]);
+            // System.out.println(i + " " + plusDP[i] + " " + minusDP[i]);
         }
 
         System.out.print(output);
