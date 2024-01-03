ee24d8b498021257e95d225525278559f4628351
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-03 10:26:59 +0900

    Title: 동전 거슬러주기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240103/동전 거슬러주기/coin-change.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_COIN = 100;
+    public static final int MAX_CHANGE = 10000;
+    public static final int INVALUED = Integer.MAX_VALUE;
+    public static int[] coinArr = new int[MAX_COIN + 1];
+    public static int[] changeArr = new int[MAX_CHANGE + 1];
+    public static int N, M;
+
+    public static void initialize(){
+        for(int i = 1 ; i <= M ; i++){
+            changeArr[i] = INVALUED;
+        }
+    } 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        for(int i = 0 ; i < N ; i++){
+            coinArr[i] = sc.nextInt();
+        }
+
+        initialize();
+
+        for(int i = 1 ; i <= M ; i++){
+            for(int j = 0 ; j < N ; j++){
+                if(i < coinArr[j] || changeArr[j] == INVALUED){
+                    continue;
+                }
+                changeArr[i] = Math.min(changeArr[i], changeArr[i - coinArr[j]] + 1);
+            }
+            // System.out.print(changeArr[i] + " ");
+        }
+        // System.out.println();
+
+        System.out.println(changeArr[M]);
+
+    }
+}
