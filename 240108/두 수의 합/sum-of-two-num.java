852ecc475c808e9ccdd4bf20ea716e9c6fb04c87
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-08 17:45:33 +0900

    Title: 두 수의 합, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240108/두 수의 합/sum-of-two-num.java
+++ 240108/두 수의 합/sum-of-two-num.java
@@ -8,65 +8,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashMap<Integer, Integer> m = new HashMap<>();
+        HashMap<Integer, Integer> inMap = new HashMap<>();
 
         n = sc.nextInt();
         k = sc.nextInt();
+
         int cnt = 0;
 
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 1 ; i <= n ; i++){
             int value = sc.nextInt();
-            m.put(i, value);
+            inMap.put(i, value);
         }
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i + 1; j < n ; j++){
-                if(m.get(i) + m.get(j) == k){
-                    cnt++;
-                }
-            }
-        }
-
-        System.out.println(cnt);
-    }
-}
-
-/* BFS 방법 Time Limit Exceeded
-
-import java.util.*;
-
-public class Main {
-    public static int MAX_INPUT = 100000;
-    public static int[] arr = new int[MAX_INPUT];
-
-    public static int n, k;
-    public static int cnt = 0;
-
-    public static void BFS(int idx, int selected, int sum){
-        if(selected == 2){
-            if(sum == k){
+        for(int i = 1 ; i <= n ; i++){
+            if(inMap.containsKey( k - inMap.get(i) )){
                 cnt++;
             }
-            return;
         }
 
-        for(int i = idx ; i < n ; i++){
-            BFS(i + 1, selected + 1, sum + arr[i]);
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        k = sc.nextInt();
-
-        for(int i = 0 ; i < n ; i++){
-            arr[i] = sc.nextInt();
-        }
-
-        BFS(0, 0, 0);
-
-        System.out.print(cnt);
+        System.out.println(cnt / 2);
     }
 }
-*/
