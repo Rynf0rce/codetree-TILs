0d2fe48c473e442cbac1823b0d8baa5275b19dab
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-08 17:49:57 +0900

    Title: 두 수의 합, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240108/두 수의 합/sum-of-two-num.java
+++ 240108/두 수의 합/sum-of-two-num.java
@@ -21,7 +21,7 @@ public class Main {
         }
 
         for(int i = 1 ; i <= n ; i++){
-            if(inMap.containsKey( k - inMap.get(i) )){
+            if(inMap.containsValue( k - inMap.get(i) )){
                 cnt++;
             }
         }
