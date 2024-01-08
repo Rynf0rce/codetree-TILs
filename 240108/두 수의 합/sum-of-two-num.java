86677f09ec109988fa7021904f92f7302e364249
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-08 18:32:32 +0900

    Title: 두 수의 합, Time: 1063ms, Memory: 54MB, Status: Passed - CodeTree

--- 240108/두 수의 합/sum-of-two-num.java
+++ 240108/두 수의 합/sum-of-two-num.java
@@ -1,28 +1,31 @@
 import java.util.*;
 
 public class Main {
-    public static int MAX_INPUT = 100000;
+    public static final int MAX_INPUT = 100000;
     public static int[] arr = new int[MAX_INPUT];
-
-    public static int n, k;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashMap<Integer, Integer> inMap = new HashMap<>();
 
-        n = sc.nextInt();
-        k = sc.nextInt();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
 
         int cnt = 0;
 
-        for(int i = 1 ; i <= n ; i++){
-            int value = sc.nextInt();
-            inMap.put(i, value);
-        }
+        for(int i = 0 ; i < n ; i++) {
+            if (map.containsKey(k - arr[i])) {
+                cnt += map.get(k - arr[i]);
+            }
 
-        for(int i = 1 ; i <= n ; i++){
-            if(inMap.containsValue( k - inMap.get(i) )){
-                cnt++;
+            if (k - arr[i] == arr[i]) {
+                cnt--;
             }
         }
 
