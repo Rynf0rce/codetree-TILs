486ec3f393abc2344a582653d3845e6371160ced
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-11 14:47:57 +0900

    Title: top k 숫자, Time: 1093ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 240111/top k 숫자/top-k-elements.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeSet<Integer> s = new TreeSet<>();
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        for(int i = 0 ; i < n ; i++){
+            int num = sc.nextInt();
+            s.add(num);
+        }
+
+        for(int i = 0 ; i < k ; i++){
+            System.out.print(s.last() + " ");
+            s.remove(s.last());
+        }
+    }
+}
