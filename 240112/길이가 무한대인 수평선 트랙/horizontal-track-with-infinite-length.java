10e806e084d1ba2ea8febbcdf3e7371f88e4f086
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-12 17:09:03 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 343ms, Memory: 21MB, Status: Wrong Answer - CodeTree

---
+++ 240112/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class info implements Comparable<info>{
+    int pos;
+    int speed;
+
+    public info(int pos, int speed){
+        this.pos = pos;
+        this.speed = speed;
+    }
+
+    public boolean isSame(info i){
+        return this.pos == i.pos && this.speed == i.speed;
+    }
+
+    @Override
+    public int compareTo(info i){
+        if(this.pos == i.pos){
+            return this.speed - i.speed;
+        }
+        return this.pos - i.pos;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeSet<info> preSet = new TreeSet<>();
+        TreeSet<info> postSet = new TreeSet<>();
+
+        int N = sc.nextInt();
+        int T = sc.nextInt();
+
+        for(int i = 0 ; i < N ; i++){
+            int pos = sc.nextInt();
+            int speed = sc.nextInt();
+            preSet.add(new info(pos, speed));
+        }
+
+        while(T-- > 0){
+            for(info i : preSet){
+                info addInfo = new info(i.pos + i.speed, i.speed);
+                postSet.add(addInfo);
+                if(!postSet.last().isSame(addInfo)){
+                    postSet.remove(addInfo);
+                }
+            }
+
+            preSet = (TreeSet<info>)postSet.clone();
+            postSet.clear();
+        }
+        System.out.println(preSet.size());
+    }
+}
