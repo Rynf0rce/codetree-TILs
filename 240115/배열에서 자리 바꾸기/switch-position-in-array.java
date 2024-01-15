80ecee92b2b92a6c80b8f422173f7352c5a72f83
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-15 12:36:44 +0900

    Title: 배열에서 자리 바꾸기, Time: 5094ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

---
+++ 240115/배열에서 자리 바꾸기/switch-position-in-array.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+class Node{
+    Node left;
+    int idx;
+    Node right;
+
+    public Node(int idx){
+        this.left = null;
+        this.idx = idx;
+        this.right = null;
+    }
+}
+
+public class Main {
+    public static void connect(Node n1, Node n2){
+        if(n1 != null){
+            n1.right = n2;
+        }
+    
+        if(n2 != null){
+            n2.left = n1;
+        }
+    }
+
+    public static void popAndInterchange(Node n1, Node n2, Node n3, Node n4){
+        boolean chack = false;
+        Node n1Left = n1.left;
+        Node n2Right = n2.right;
+        Node n3Left = n3.left;
+        Node n4Right = n4.right;
+
+        if(n2.right == n3){
+            chack = true;
+        }
+
+        connect(n1.left, n2.right);
+        n1.left = n2.right = null;
+        connect(n3.left, n4.right);
+        n3.left = n4.right = null;
+
+        if(chack){
+            connect(n1Left, n3);
+            connect(n2, n4Right);
+            connect(n4, n1);
+        }
+        else{
+            connect(n4, n2Right);
+            connect(n1Left, n3);
+            connect(n3Left, n1);
+            connect(n2, n4Right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Integer, Node> nodeMap = new HashMap<>();
+        
+        int N = sc.nextInt();
+        for(int i = 1 ; i <= N ; i++){
+            nodeMap.put(i, new Node(i));
+            if(nodeMap.containsKey(i-1)){
+                connect(nodeMap.get(i-1), nodeMap.get(i));
+            }
+        }
+
+        int Q = sc.nextInt();
+        for(int i = 0 ; i < Q ; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+            popAndInterchange(nodeMap.get(a), nodeMap.get(b), nodeMap.get(c), nodeMap.get(d));
+        }
+
+        // 출력부
+        Node test = new Node(0);
+        for(int i = 1 ; i <= N ; i++){
+            if(nodeMap.get(i).left == null){
+                test = nodeMap.get(i);
+                break;
+            }
+        }
+
+        while(test.right != null){
+            System.out.print(test.idx + " ");
+            test = test.right;
+        }
+        System.out.print(test.idx);
+    }
+}
