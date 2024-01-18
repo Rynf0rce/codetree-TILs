5e1804e12ba6e7742602977912d7defef284d080
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-18 14:48:10 +0900

    Title: 최대 직사각형 합, Time: 3091ms, Memory: 33MB, Status: Time Limit Exceed - CodeTree

---
+++ 240118/최대 직사각형 합/max-rect-sum-in-grid.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_EDGE = 300;
+    public static final int MIN_VALUE = -1000;
+    public static int[][] table = new int[MAX_EDGE + 1][MAX_EDGE + 1];
+    public static int[][] prefixSum = new int[MAX_EDGE + 1][MAX_EDGE + 1];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                table[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                prefixSum[i][j] = table[i][j] - prefixSum[i - 1][j - 1] + prefixSum[i - 1][j] + prefixSum[i][j - 1];
+            }
+        }
+
+        // for(int i = 0 ; i <= n ; i++){
+        //     for(int j = 0 ; j <= n ; j++){
+        //         System.out.print(prefixSum[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        int maxVal = MIN_VALUE * MAX_EDGE * MAX_EDGE;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                for(int k = i ; k <= n ; k++){
+                    for(int h = j ; h <= n ; h++){
+                        maxVal = Math.max(maxVal, prefixSum[k][h] + prefixSum[i - 1][j - 1] - prefixSum[i - 1][h] - prefixSum[k][j - 1]);
+                    }
+                }
+            }
+        }
+        System.out.print(maxVal);
+    }
+}
