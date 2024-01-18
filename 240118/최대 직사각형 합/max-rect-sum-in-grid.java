19eca994dc50279edf381c27442a4bedd8abb412
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-18 19:41:15 +0900

    Title: 최대 직사각형 합, Time: 939ms, Memory: 34MB, Status: Passed - CodeTree

--- 240118/최대 직사각형 합/max-rect-sum-in-grid.java
+++ 240118/최대 직사각형 합/max-rect-sum-in-grid.java
@@ -5,54 +5,55 @@ public class Main {
     public static final int MIN_VALUE = -1000;
     public static int[][] table = new int[MAX_EDGE + 1][MAX_EDGE + 1];
     public static int[][] prefixSum = new int[MAX_EDGE + 1][MAX_EDGE + 1];
+    public static int[] DP = new int[MAX_EDGE + 1];
+    public static int n;
+
+    public static void initialize(){
+        for(int i = 0 ; i <= n ; i++){
+            DP[i] = 0;
+        }
+    }
+    
+    public static int valueInRectangle(int r1, int r2){
+        int maxVal = MAX_EDGE * MAX_EDGE * MIN_VALUE;
+
+        for(int i = 1 ; i <= n ; i++){
+            int value = prefixSum[r2][i] - prefixSum[r1 - 1][i];
+            DP[i] = Math.max(DP[i - 1] + value, value);
+            maxVal = Math.max(maxVal, DP[i]);
+        }
+
+        return maxVal;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int maxVal = MIN_VALUE * MAX_EDGE * MAX_EDGE;
+        n = sc.nextInt();
+        int maxVal = MAX_EDGE * MAX_EDGE * MIN_VALUE;
 
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= n ; j++){
                 table[i][j] = sc.nextInt();
-                maxVal = Math.max(maxVal, table[i][j]);
             }
-        }
-
-        if(maxVal <= 0){
-            System.out.print(maxVal);
-            System.exit(0);
         }
 
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= n ; j++){
-                if(prefixSum[i - 1][j - 1] == 0){
-                    prefixSum[i][j] = Math.max(prefixSum[i - 1][j] + table[i][j], prefixSum[i][j - 1] + table[i][j]);
+                if(i == 1){
+                    prefixSum[i][j] = table[i][j];
                 }
                 else{
-                    prefixSum[i][j] = table[i][j] - prefixSum[i - 1][j - 1] + prefixSum[i - 1][j] + prefixSum[i][j - 1];
-                }
-                if(prefixSum[i][j] < 0){
-                    prefixSum[i][j] = 0;
+                    prefixSum[i][j] = prefixSum[i - 1][j] + table[i][j];
                 }
             }
         }
 
-        // for(int i = 0 ; i <= n ; i++){
-        //     for(int j = 0 ; j <= n ; j++){
-        //         System.out.print(prefixSum[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-
-        
         for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= n ; j++){
-                for(int k = i ; k <= n ; k++){
-                    for(int h = j ; h <= n ; h++){
-                        maxVal = Math.max(maxVal, prefixSum[k][h] + prefixSum[i - 1][j - 1] - prefixSum[i - 1][h] - prefixSum[k][j - 1]);
-                    }
-                }
+            for(int j = i ; j <= n ; j++){
+                maxVal = Math.max(maxVal, valueInRectangle(i, j));
             }
         }
+
         System.out.print(maxVal);
     }
 }
