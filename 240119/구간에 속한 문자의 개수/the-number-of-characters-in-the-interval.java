7e5f17f3e00f25b7e750b0b2fad8c92d5db8399f
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-19 10:10:04 +0900

    Title: 구간에 속한 문자의 개수, Time: 3019ms, Memory: 93MB, Status: Time Limit Exceed - CodeTree

--- 240119/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.java
+++ 240119/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.java
@@ -1,55 +1,67 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static final int MAX_EDGE = 1000;
     public static char[][] table = new char[MAX_EDGE + 1][MAX_EDGE + 1];
-    public static int[][] preSum_A = new int[MAX_EDGE + 1][MAX_EDGE + 1];
-    public static int[][] preSum_B = new int[MAX_EDGE + 1][MAX_EDGE + 1];
-    public static int[][] preSum_C = new int[MAX_EDGE + 1][MAX_EDGE + 1];
+    public static int[][][] preSum = new int[MAX_EDGE + 1][MAX_EDGE + 1][3];
     public static int n, m;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        int k = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int arr[] = new int[st.countTokens()];
+        int count=0;
+
+		while(st.hasMoreTokens()) {
+			arr[count++] = Integer.parseInt(st.nextToken());
+		}
+       
+        n = arr[0];
+        m = arr[1];
+        int k = arr[2];
 
         for(int i = 1 ; i <= n ; i++){
-            String str = sc.next();
+            String str = br.readLine();
             for(int j = 1 ; j <= m ; j++){
                 table[i][j] = str.charAt(j - 1);
-            }
-        }
-
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= m ; j++){
                 if(table[i][j] == 'a'){
-                    preSum_A[i][j] = 1 + preSum_A[i - 1][j] + preSum_A[i][j - 1] - preSum_A[i - 1][j - 1];
-                    preSum_B[i][j] = preSum_B[i - 1][j] + preSum_B[i][j - 1] - preSum_B[i - 1][j - 1];
-                    preSum_C[i][j] = preSum_C[i - 1][j] + preSum_C[i][j - 1] - preSum_C[i - 1][j - 1];
+                    preSum[i][j][0] = 1 + preSum[i - 1][j][0] + preSum[i][j - 1][0] - preSum[i - 1][j - 1][0];
+                    preSum[i][j][1] = preSum[i - 1][j][1] + preSum[i][j - 1][1] - preSum[i - 1][j - 1][1];
+                    preSum[i][j][2] = preSum[i - 1][j][2] + preSum[i][j - 1][2] - preSum[i - 1][j - 1][2];
                 }
                 else if(table[i][j] == 'b'){
-                    preSum_A[i][j] = preSum_A[i - 1][j] + preSum_A[i][j - 1] - preSum_A[i - 1][j - 1];
-                    preSum_B[i][j] = 1 + preSum_B[i - 1][j] + preSum_B[i][j - 1] - preSum_B[i - 1][j - 1];
-                    preSum_C[i][j] = preSum_C[i - 1][j] + preSum_C[i][j - 1] - preSum_C[i - 1][j - 1];
+                    preSum[i][j][0] = preSum[i - 1][j][0] + preSum[i][j - 1][0] - preSum[i - 1][j - 1][0];
+                    preSum[i][j][1] = 1 + preSum[i - 1][j][1] + preSum[i][j - 1][1] - preSum[i - 1][j - 1][1];
+                    preSum[i][j][2] = preSum[i - 1][j][2] + preSum[i][j - 1][2] - preSum[i - 1][j - 1][2];
                 }
                 else{
-                    preSum_A[i][j] = preSum_A[i - 1][j] + preSum_A[i][j - 1] - preSum_A[i - 1][j - 1];
-                    preSum_B[i][j] = preSum_B[i - 1][j] + preSum_B[i][j - 1] - preSum_B[i - 1][j - 1];
-                    preSum_C[i][j] = 1 + preSum_C[i - 1][j] + preSum_C[i][j - 1] - preSum_C[i - 1][j - 1];
+                    preSum[i][j][0] = preSum[i - 1][j][0] + preSum[i][j - 1][0] - preSum[i - 1][j - 1][0];
+                    preSum[i][j][1] = preSum[i - 1][j][1] + preSum[i][j - 1][1] - preSum[i - 1][j - 1][1];
+                    preSum[i][j][2] = 1 + preSum[i - 1][j][2] + preSum[i][j - 1][2] - preSum[i - 1][j - 1][2];
                 }
             }
         }
-
+        
         for(int i = 0 ; i < k ; i++){
-            int r1 = sc.nextInt();
-            int c1 = sc.nextInt();
-            int r2 = sc.nextInt();
-            int c2 = sc.nextInt();
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+		    int[] condition = new int[st1.countTokens()];
 
-            System.out.print(preSum_A[r2][c2] + preSum_A[r1 - 1][c1 - 1] - preSum_A[r2][c1 - 1] - preSum_A[r1 - 1][c2] + " ");
-            System.out.print(preSum_B[r2][c2] + preSum_B[r1 - 1][c1 - 1] - preSum_B[r2][c1 - 1] - preSum_B[r1 - 1][c2] + " ");
-            System.out.print(preSum_C[r2][c2] + preSum_C[r1 - 1][c1 - 1] - preSum_C[r2][c1 - 1] - preSum_C[r1 - 1][c2] + "\n");
+            int idx = 0;
+
+            while(st1.hasMoreTokens()) {
+                condition[idx++] = Integer.parseInt(st1.nextToken());
+            }
+
+            int r1 = condition[0];
+            int c1 = condition[1];
+            int r2 = condition[2];
+            int c2 = condition[3];
+
+            System.out.print(preSum[r2][c2][0] + preSum[r1 - 1][c1 - 1][0] - preSum[r2][c1 - 1][0] - preSum[r1 - 1][c2][0] + " ");
+            System.out.print(preSum[r2][c2][1] + preSum[r1 - 1][c1 - 1][1] - preSum[r2][c1 - 1][1] - preSum[r1 - 1][c2][1] + " ");
+            System.out.print(preSum[r2][c2][2] + preSum[r1 - 1][c1 - 1][2] - preSum[r2][c1 - 1][2] - preSum[r1 - 1][c2][2] + "\n");
         }
     }
 }
