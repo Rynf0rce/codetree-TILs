bb7813ab34688a77a4d25a5422ec16d165f27138
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-21 11:18:17 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 123ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 240121/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100000;
+    public static final int NUM_SELECT = 3;
+    public static int[] arr = new int[MAX_LENGTH + 1];
+    public static int[][] DP = new int[NUM_SELECT + 2][MAX_LENGTH + 1];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1 ; i <= n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        DP[1][1] = arr[1];
+        for(int i = 1 ; i <= NUM_SELECT ; i++){
+            for(int j = 2 ; j <= n ; j++){
+                DP[i][j] = Math.max(DP[i][j - 1], DP[i - 1][j - 2] + arr[j]);
+            }
+        }
+
+        // for(int i = 0 ; i <= NUM_SELECT ; i++){
+        //     for(int j = 0 ; j <= n ; j++){
+        //         System.out.print(DP[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        System.out.println(DP[NUM_SELECT][n]);
+    }
+}
