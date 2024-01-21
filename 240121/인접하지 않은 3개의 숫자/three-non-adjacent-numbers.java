88fd351bc770e6ae90d3fc0b5f6a1b92819800dc
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-21 11:35:35 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 724ms, Memory: 34MB, Status: Passed - CodeTree

--- 240121/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.java
+++ 240121/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.java
@@ -2,6 +2,42 @@ import java.util.*;
 
 public class Main {
     public static final int MAX_LENGTH = 100000;
+    public static int[] arr = new int[MAX_LENGTH + 2];
+    public static int[] leftArr = new int[MAX_LENGTH + 4];
+    public static int[] rightArr = new int[MAX_LENGTH + 4];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0 ; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        leftArr[0] = arr[0];
+        for(int i = 1 ; i < n; i++){
+            leftArr[i] = Math.max(leftArr[i - 1], arr[i]);
+        }
+
+        rightArr[n - 1] = arr[n - 1];
+        for(int i = n - 2; i >= 0; i--){
+            rightArr[i] = Math.max(rightArr[i + 1], arr[i]);
+        }
+        
+        int maxVal = 0;
+        for(int i = 2 ; i < n - 2 ; i++){
+            maxVal = Math.max(maxVal, leftArr[i - 2] + arr[i] + rightArr[i + 2]);
+        }
+
+        System.out.print(maxVal);
+    }
+}
+
+/* DP로 풀기
+
+import java.util.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100000;
     public static final int NUM_SELECT = 3;
     public static int[] arr = new int[MAX_LENGTH + 1];
     public static int[][] DP = new int[NUM_SELECT + 2][MAX_LENGTH + 1];
@@ -24,13 +60,8 @@ public class Main {
             }
         }
 
-        // for(int i = 0 ; i <= NUM_SELECT ; i++){
-        //     for(int j = 0 ; j <= n ; j++){
-        //         System.out.print(DP[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-
         System.out.println(DP[NUM_SELECT][n]);
     }
 }
+
+*/
