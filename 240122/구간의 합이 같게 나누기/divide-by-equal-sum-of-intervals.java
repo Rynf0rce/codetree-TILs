9e98a77f213c5d786c0006b696ef71fab395fc8b
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-22 08:28:21 +0900

    Title: 구간의 합이 같게 나누기, Time: 263ms, Memory: 23MB, Status: Passed - CodeTree

---
+++ 240122/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.java
@@ -0,0 +1,91 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100000;
+    public static final long[] arr = new long[MAX_LENGTH];
+
+    public static long[] leftArr = new long[MAX_LENGTH];
+    public static long[] rightArr = new long[MAX_LENGTH];
+
+    public static int[] conLeftArr = new int[MAX_LENGTH];
+    public static int[] conRightArr = new int[MAX_LENGTH];
+
+    public static int combination(int i){
+        return ( i * (i - 1) * (i - 2) ) / ( 3 * 2 * 1);
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int idx = 0;
+        long sum = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        while(st.hasMoreTokens()){
+            int num = Integer.parseInt(st.nextToken());
+            sum += num;
+            arr[idx++] = num;
+        }
+
+        leftArr[0] = arr[0];
+        int cnt = 0;
+        if(arr[0] == sum / 4){
+            cnt++;
+            conLeftArr[0] = cnt;
+        }
+        for(int i = 1 ; i < n ; i++){
+            leftArr[i] = arr[i] + leftArr[i - 1];
+            conLeftArr[i] = cnt;
+
+            if(leftArr[i] == sum / 4){
+                cnt++;
+                conLeftArr[i] = cnt;
+            }
+        }
+
+        rightArr[n - 1] = arr[n - 1];
+        cnt = 0;
+        if(arr[n - 1] == sum / 4){
+            cnt++;
+            conRightArr[n - 1] = cnt;
+        }
+        for(int i = n - 2; i >= 0 ; i--){
+            rightArr[i] = rightArr[i + 1] + arr[i];
+
+            conRightArr[i] = cnt;
+
+            if(rightArr[i] == sum / 4){
+                cnt++;
+                conRightArr[i] = cnt;
+            }
+        }
+
+        // for(int i = 0 ; i < n ; i++){
+        //     System.out.print(conLeftArr[i] + " ");
+        // }
+        // System.out.println();
+
+        // for(int i = 0 ; i < n ; i++){
+        //     System.out.print(conRightArr[i] + " ");
+        // }
+        // System.out.println();
+
+
+        long output = 0;
+        for(int i = 1 ; i < n - 2 ; i++){
+            if(leftArr[i] == sum / 2){
+                output += conLeftArr[i - 1] * conRightArr[i + 2];
+            }
+        }
+
+        bw.write(output + "");
+
+        br.close();
+        bw.close();
+    }
+}
