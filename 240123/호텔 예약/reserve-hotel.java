b32f613574b4aa2948eb5cd975a7a52ab13c2efd
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-23 11:28:48 +0900

    Title: 호텔 예약, Time: 133ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240123/호텔 예약/reserve-hotel.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+class stay implements Comparable<stay>{
+    int time;
+    int val;
+    int idx;
+
+    public stay(int time, int val, int idx){
+        this.time = time;
+        this.val = val;
+        this.idx = idx;
+    }
+
+    @Override
+    public int compareTo(stay s){
+        if(this.time == s.time){
+            return this.idx - s.idx;
+        }
+        else{
+            return this.time - s.time;
+        }
+    }
+}
+
+public class Main {
+    public static final int MAX_PEOPLE = 100000;
+    public static PriorityQueue<Integer> roomQueue = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 1 ; i <= MAX_PEOPLE ; i++){
+            roomQueue.add(i);
+        }
+
+        int n = Integer.parseInt(st.nextToken());
+        ArrayList<stay> stayList = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            stayList.add(new stay(s, 1, i));
+            stayList.add(new stay(e, -1, i));
+        }
+
+        Collections.sort(stayList);
+        HashMap<Integer, Integer> asignedMap = new HashMap<>();
+        int maxVal = 0;
+        for(int i = 0 ; i < stayList.size() ; i++){
+            if(stayList.get(i).val == 1){
+                int roomNum = roomQueue.poll();
+                asignedMap.put(stayList.get(i).idx, roomNum);
+                maxVal = Math.max(maxVal, roomNum);
+            }
+            else{
+                roomQueue.add(asignedMap.get(stayList.get(i).idx));
+            }
+        }
+
+        bw.write(maxVal + "");
+
+        br.close();
+        bw.close();
+    }
+}
