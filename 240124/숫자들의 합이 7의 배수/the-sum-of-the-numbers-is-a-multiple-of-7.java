66186fc4ac9b8e22375c27d218f6338451d65b3a
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-24 11:03:29 +0900

    Title: 숫자들의 합이 7의 배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240124/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.java
+++ 240124/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.java
@@ -29,7 +29,7 @@ public class Main {
             // System.out.print(prefixArr[i] + " ");
             remainIdxSet[prefixArr[i]].add(i);
         }
-        System.out.println();
+        //System.out.println();
 
         int maxVal = 0;
         for(int i = 0 ; i < MULTIPLE_VAL ; i++){
