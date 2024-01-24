90b72110752b6169f37a004300b54dd4b8313d6e
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-24 11:35:15 +0900

    Title: 숫자들의 합이 7의 배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240124/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.java
+++ 240124/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.java
@@ -2,42 +2,40 @@ import java.util.*;
 
 public class Main {
     public static final int MAX_LENGTH = 50000;
-    public static final int MULTIPLE_VAL = 7;
-    public static TreeSet<Integer>[] remainIdxSet = new TreeSet[MULTIPLE_VAL];
-    public static int[] arr = new int[MAX_LENGTH + 2];
-    public static int[] prefixArr = new int[MAX_LENGTH];
+    public static final int MUTIPLE_VALUE = 7;
+
+    public static int[] arr = new int[MAX_LENGTH + 1];
+    public static int[] prefixSum = new int[MAX_LENGTH + 1];
+    public static TreeSet<Integer>[] remainIdxSet = new TreeSet[MUTIPLE_VALUE];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
+
         for(int i = 1 ; i <= N ; i++){
             arr[i] = sc.nextInt();
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
         }
 
-        for(int i = 0 ; i < MULTIPLE_VAL ; i++){
+        for(int i = 0 ; i < MUTIPLE_VALUE ; i++){
             remainIdxSet[i] = new TreeSet<Integer>();
         }
 
-        prefixArr[1] = arr[1];
-        for(int i = 2 ; i <= N; i++){
-            prefixArr[i] = prefixArr[i - 1] + arr[i];
+        for(int i = 0 ; i <= N ; i++){
+            prefixSum[i] %= MUTIPLE_VALUE;
         }
 
-        for(int i = 0 ; i < N ; i++){
-            prefixArr[i] %= MULTIPLE_VAL;
-            // System.out.print(prefixArr[i] + " ");
-            remainIdxSet[prefixArr[i]].add(i);
+        for(int i = 0 ; i <= N ; i++){
+            remainIdxSet[prefixSum[i]].add(i);
         }
-        //System.out.println();
 
         int maxVal = 0;
-        for(int i = 0 ; i < MULTIPLE_VAL ; i++){
+        for(int i = 0 ; i < MUTIPLE_VALUE ; i++){
             if(remainIdxSet[i].size() >= 2){
-                maxVal = Math.max(maxVal, remainIdxSet[i].last() - remainIdxSet[i].first() );
+                // System.out.println(remainIdxSet[i].first() + " " + remainIdxSet[i].last());
+                maxVal = Math.max(remainIdxSet[i].last() - remainIdxSet[i].first(), maxVal);
             }
         }
-
         System.out.print(maxVal);
     }
 }
