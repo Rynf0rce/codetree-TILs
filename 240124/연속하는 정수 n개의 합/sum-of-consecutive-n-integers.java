759383d2268069ad9939493b8401bc4a58860fc8
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-24 13:20:01 +0900

    Title: 연속하는 정수 n개의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240124/연속하는 정수 n개의 합/sum-of-consecutive-n-integers.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 10000;
+    public static int[] arr = new int[MAX_LENGTH];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int j = -1;
+        int sum = 0;
+        int cnt = 0;
+        for(int i = 0 ; i < n ; i++){
+            while(j + 1 < n && sum < m){
+                sum += arr[j + 1];
+                j++;
+            }
+
+            if(j >= n && sum < m){
+                break;
+            }
+            
+            if(sum == m){
+                cnt++;
+            }
+            sum -= arr[i];
+        }
+
+        bw.write(cnt + "");
+        
+        br.close();
+        bw.close();
+    }
+}
