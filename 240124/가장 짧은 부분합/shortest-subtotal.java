6ee18eb981bff365bc20c6217214b3dc0bb18c65
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-24 12:13:35 +0900

    Title: 가장 짧은 부분합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240124/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_ELEMENTS = 100000;
+    public static int[] arr =  new int[MAX_ELEMENTS];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        int j = -1;
+        int minLength = MAX_ELEMENTS + 1;
+        for(int i = 0 ; i < n ; i++){
+            while(j + 1 < n && sum + arr[j + 1] < s){
+                sum += arr[j + 1];
+                j++;
+            }
+
+            if(sum < s){
+                sum += arr[j + 1];
+                j++;
+            }
+
+            minLength = Math.min(minLength, j - i + 1);
+            sum -= arr[i];
+        }
+
+        bw.write(minLength + "");
+
+        br.close();
+        bw.close();
+    }
+}
