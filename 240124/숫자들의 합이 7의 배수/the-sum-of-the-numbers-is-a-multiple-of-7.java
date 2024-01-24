894a3d7d3f0bcaaac914190df81c91f92e102bb8
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-24 10:26:44 +0900

    Title: 숫자들의 합이 7의 배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240124/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.java
+++ 240124/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.java
@@ -2,14 +2,21 @@ import java.util.*;
 
 public class Main {
     public static final int MAX_LENGTH = 50000;
+    public static final int MULTIPLE_VAL = 7;
+    public static TreeSet<Integer>[] remainIdxSet = new TreeSet[7];
     public static int[] arr = new int[MAX_LENGTH];
     public static int[] prefixArr = new int[MAX_LENGTH];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
         for(int i = 0 ; i < N ; i++){
             arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0 ; i < MULTIPLE_VAL ; i++){
+            remainIdxSet[i] = new TreeSet<Integer>();
         }
 
         prefixArr[0] = arr[0];
@@ -17,15 +24,18 @@ public class Main {
             prefixArr[i] = prefixArr[i - 1] + arr[i];
         }
 
+        for(int i = 0 ; i < N ; i++){
+            prefixArr[i] %= MULTIPLE_VAL;
+            remainIdxSet[prefixArr[i]].add(i);
+        }
+
         int maxVal = 0;
-        for(int i = 1 ; i < N ; i++){
-            for(int j = i - 1 ; j >= 0 ; j--){
-                if( (prefixArr[i] - prefixArr[j]) % 7 == 0 ){
-                    maxVal = Math.max(maxVal, i - j);
-                }
+        for(int i = 0 ; i < MULTIPLE_VAL ; i++){
+            if(remainIdxSet[i].size() >= 2){
+                maxVal = Math.max(maxVal, remainIdxSet[i].last() - remainIdxSet[i].first());
             }
         }
-        
-        System.out.println(maxVal);
+
+        System.out.print(maxVal);
     }
 }
