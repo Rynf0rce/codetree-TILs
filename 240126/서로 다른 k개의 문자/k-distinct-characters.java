325738bad6b4a288ce2779389a0e006a8bad67f9
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-26 12:40:52 +0900

    Title: 서로 다른 k개의 문자, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240126/서로 다른 k개의 문자/k-distinct-characters.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Character, Integer> alphaMap = new HashMap<>();
+        char[] arr = sc.next().toCharArray();
+        int k = sc.nextInt();
+
+        int j = -1;
+        int output = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+            while(j + 1 < arr.length && (alphaMap.containsKey(arr[j + 1]) || (alphaMap.size() <= 1) )){
+                alphaMap.put(arr[j + 1], alphaMap.getOrDefault(arr[j + 1], 0) + 1);
+                j++;
+            }
+
+            output = Math.max(output, j - i + 1);
+            if(alphaMap.get(arr[i]) == 1){
+                alphaMap.remove(arr[i]);
+            }
+            else{
+                alphaMap.put(arr[i], alphaMap.get(arr[i]) - 1);
+            } 
+        }
+        System.out.println(output);
+    }
+}
