91931354ae88aefe9a6af0242eeeb31d5bde16c5
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-26 13:07:01 +0900

    Title: 겹치는 원소가 적은 부분 수열, Time: 323ms, Memory: 21MB, Status: Wrong Answer - CodeTree

---
+++ 240126/겹치는 원소가 적은 부분 수열/subsequences-with-few-overlapping-elements.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100000;
+    public static int[] arr = new int[MAX_LENGTH];
+    public static HashMap<Integer, Integer> elementMap = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int j = -1;
+        int output = 0;
+        for(int i = 0 ; i < n ; i++){
+            while(j + 1 < n && elementMap.getOrDefault(arr[j + 1], 0) < 2){
+                elementMap.put(arr[j + 1], elementMap.getOrDefault(arr[j + 1], 0) + 1);
+                j++;
+            }
+            
+            output = Math.max(output, j - i + 1);
+            // System.out.println(j + " " + i);
+            elementMap.put(arr[i], elementMap.get(arr[i]) - 1);
+        }
+
+        bw.write(output + "");
+
+        br.close();
+        bw.close();
+    }
+}
