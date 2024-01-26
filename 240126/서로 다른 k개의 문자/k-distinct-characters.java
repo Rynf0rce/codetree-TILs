1118ce5975628d652f0f784bce3afed8b51d7c21
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-26 12:42:17 +0900

    Title: 서로 다른 k개의 문자, Time: 367ms, Memory: 14MB, Status: Passed - CodeTree

--- 240126/서로 다른 k개의 문자/k-distinct-characters.java
+++ 240126/서로 다른 k개의 문자/k-distinct-characters.java
@@ -10,7 +10,7 @@ public class Main {
         int j = -1;
         int output = 0;
         for(int i = 0 ; i < arr.length ; i++){
-            while(j + 1 < arr.length && (alphaMap.containsKey(arr[j + 1]) || (alphaMap.size() <= 1) )){
+            while(j + 1 < arr.length && (alphaMap.containsKey(arr[j + 1]) || (alphaMap.size() < k) )){
                 alphaMap.put(arr[j + 1], alphaMap.getOrDefault(arr[j + 1], 0) + 1);
                 j++;
             }
