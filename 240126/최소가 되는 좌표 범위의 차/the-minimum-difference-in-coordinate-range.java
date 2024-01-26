e2b083b5bb0860f20d38bbc3992514d0d9fec410
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-26 11:24:41 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 95ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 240126/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.java
+++ 240126/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.java
@@ -3,8 +3,21 @@ import java.io.*;
 
 public class Main {
     public static final int MAX_RANGE = 1000000;
-    public static TreeSet<Integer>[] ySet = new TreeSet[MAX_RANGE + 1];
+    public static TreeSet<Integer>[] xSet = new TreeSet[MAX_RANGE + 1];
     public static int[] xArr = new int[MAX_RANGE];
+    public static int D;
+
+    public static boolean inRange(int x1, int x2){
+        if(x1 > x2){
+            return false;
+        }
+        
+        if(xSet[x2].last() - xSet[x1].first() >= D){
+            return true;
+        }
+
+        return false;
+    }
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,40 +25,36 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
         int N = Integer.parseInt(st.nextToken());
-        int D = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
 
         for(int i = 0 ; i < N ; i++){
             st = new StringTokenizer(br.readLine(), " ");
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            if(ySet[x] == null){
-                ySet[x] = new TreeSet<Integer>();
+            if(xSet[x] == null){
+                xSet[x] = new TreeSet<Integer>();
             }
 
-            ySet[x].add(y);
+            xSet[x].add(y);
             xArr[i] = x;
         }
 
         Arrays.sort(xArr, 0, N);
 
-        int j = 1;
+        int j = 0;
         int minVal = MAX_RANGE + 1;
         for(int i = 0 ; i < N ; i++){
             while(j + 1 < N){
-                if(j == i){
-                    j++;
-                }
-                
-                if( Math.abs(ySet[xArr[i]].first() - ySet[xArr[j]].last()) >= D || Math.abs(ySet[xArr[i]].last() - ySet[xArr[j]].first()) >= D ){
-                    minVal = Math.min(minVal, xArr[j] - xArr[i]);
+                if(inRange(xArr[i], xArr[j + 1])){
+                    minVal = Math.min(minVal, xArr[j + 1] - xArr[i]);
                     break;
                 }
                 j++;
             }
         }
 
-        bw.write( (minVal == MAX_RANGE + 1 ? -1 : minVal) + "");
+        bw.write(minVal + "");
 
         br.close();
         bw.close();
