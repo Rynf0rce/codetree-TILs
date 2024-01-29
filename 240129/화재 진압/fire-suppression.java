5d2c03e6752999b1ea7a427b903f6bdfc6bedb35
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-29 09:13:19 +0900

    Title: 화재 진압, Time: 810ms, Memory: 31MB, Status: Passed - CodeTree

---
+++ 240129/화재 진압/fire-suppression.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100000;
+    public static final long INVALUED = Long.MAX_VALUE;
+    public static long[] placeArr = new long[MAX_LENGTH];
+    public static TreeSet<Long> stationSet = new TreeSet<>();
+
+    public static long max(long num1, long num2){
+        if(num1 >= num2){
+            return num1;
+        }
+        else{
+            return num2;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++){
+            placeArr[i] = Long.parseLong(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < m ; i++){
+            stationSet.add(Long.parseLong(st.nextToken()));
+        }
+
+        long answer = 0;
+        for(int i = 0 ; i < n ; i++){
+            long curDistance = INVALUED;
+            if(stationSet.ceiling(placeArr[i]) != null && curDistance > stationSet.ceiling(placeArr[i]) - placeArr[i]){
+                curDistance = stationSet.ceiling(placeArr[i]) - placeArr[i];
+            }
+
+            if(stationSet.floor(placeArr[i]) != null && curDistance > placeArr[i] - stationSet.floor(placeArr[i])){
+                curDistance = placeArr[i] - stationSet.floor(placeArr[i]);
+            }
+
+            if(curDistance != INVALUED){
+                answer = max(answer, curDistance);
+            }
+        }
+
+        bw.write(answer + "");
+
+        br.close();
+        bw.close();
+    }
+}
