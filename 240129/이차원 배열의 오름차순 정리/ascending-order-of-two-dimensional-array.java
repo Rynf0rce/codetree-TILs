08eb715b3a2b05fd2fbc49d7ff07424186b23422
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-29 17:40:50 +0900

    Title: 이차원 배열의 오름차순 정리, Time: 180ms, Memory: 10MB, Status: Passed - CodeTree

--- 240129/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.java
+++ 240129/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.java
@@ -1,23 +1,28 @@
 import java.util.*;
 
 public class Main {
-    public static int n, k;
-    public static int myIdx(int num){
-        int idx = 0;
+    public static long n, k;
+    public static long myIdx(long num){
+        long idx = 0;
         for(int i = 1 ; i <= n ; i++){
-            idx += Math.min(num, i * n) / i;
+            if(num > i * n){
+                idx += n;
+            }
+            else{
+                idx += num / i;
+            }
         }
         return idx;
     }
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        k = sc.nextInt();
-        int start = 1;
-        int end = n * n;
-        int mid = 0;
-        int ans = n * n + 1;
+        n = sc.nextLong();
+        k = sc.nextLong();
+        long start = 1;
+        long end = n * n;
+        long mid = 0;
+        long ans = n * n + 1;
         while(start <= end){
             mid = (end + start) / 2;
             if(myIdx(mid) >= k){
