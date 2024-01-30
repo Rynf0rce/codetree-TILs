8467f1fc2db2f4eee7db6b46a16d91bdf919e2e5
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-01-30 13:25:33 +0900

    Title: 가장 가까운 두 점 사이의 거리를 최대화하기, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240130/가장 가까운 두 점 사이의 거리를 최대화하기/maximize-dist-of-nearest-points.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class line implements Comparable<line>{
+    int start;
+    int end;
+
+    public line(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public int compareTo(line l){
+        if(this.start == l.start){
+            return this.end - l.end;
+        }
+        else{
+            return this.start - l.start;
+        }
+    }
+}
+
+public class Main {
+    public static final int MAX_LINE = 100000;
+    public static int MAX_X = 1000000000;
+    public static line[] arr = new line[MAX_LINE];
+
+    public static boolean isPossible(int length, int lineNum){
+        int x1 = arr[0].start;
+        int x2 = arr[0].end;
+        for(int i = 1 ; i < lineNum ; i++){
+            x1 += length;
+            x2 += length;
+
+            x1 = Math.max(x1, arr[i].start);
+            x2 = Math.min(x2, arr[i].end);
+
+            if(x2 < x1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0 ; i < n ; i++){
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            arr[i] = new line(x1, x2);
+        }
+
+        Arrays.sort(arr, 0 , n);
+
+        int start = 1;
+        int end = MAX_X;
+        int ans = 0;
+        while(start <= end){
+            int mid = (end + start) / 2;
+            if(isPossible(mid, n)){
+                start = mid + 1;
+                ans = Math.max(ans, mid);
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
