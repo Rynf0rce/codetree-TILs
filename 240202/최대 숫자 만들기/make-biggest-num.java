5ecc973acbe7cce91227fd89a993b3ab6f49432c
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-02 17:45:10 +0900

    Title: 최대 숫자 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240202/최대 숫자 만들기/make-biggest-num.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static ArrayList<String> inputList = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i < n ; i++){
+            inputList.add(br.readLine());
+        }
+
+
+        Collections.sort(inputList, new Comparator<String>(){
+            @Override
+            public int compare(String a, String b){
+                long n1 = Integer.parseInt(a.concat(b));
+                long n2 = Integer.parseInt(b.concat(a));
+
+                if(n1 > n2){
+                    return 1;
+                }
+                else{
+                    return -1;
+                }
+                
+            }
+        });
+
+
+        String ans = "";
+        for(int i = n - 1 ; i >= 0 ; i--){
+            String str = inputList.get(i);
+            //System.out.println(str);
+            bw.write(str);
+            // ans.concat();
+        }
+
+        
+
+        br.close();
+        bw.close();
+
+
+    }
+}
