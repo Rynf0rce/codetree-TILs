864c65c8048e5650f88c8b3a02e9907055e5d7a8
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-02 14:28:48 +0900

    Title: 높이 차를 최소화 하기, Time: 1671ms, Memory: 147MB, Status: Wrong Answer - CodeTree

--- 240202/높이 차를 최소화 하기/minimize-the-height-difference.java
+++ 240202/높이 차를 최소화 하기/minimize-the-height-difference.java
@@ -1,16 +1,13 @@
 import java.util.*;
+import java.io.*;
 
 class point{
     int row;
     int col;
-    int min;
-    int max;
-
-    public point(int row, int col, int min, int max){
+    
+    public point(int row, int col){
         this.row = row;
         this.col = col;
-        this.min = min;
-        this.max = max;
     }
 }
 
@@ -18,103 +15,107 @@ public class Main {
     public static final int MAX_LENGTH = 100;
     public static final int MAX_HEIGHT = 500;
     public static int[][] table = new int[MAX_LENGTH][MAX_LENGTH];
-    public static boolean[][] visited = new boolean[MAX_LENGTH][MAX_LENGTH];
-
+    public static boolean[][] vistied = new boolean[MAX_LENGTH][MAX_LENGTH];
+    public static Queue<point> q = new LinkedList<>();
     public static int[] arrRow = new int[]{-1, 0, 1, 0};
     public static int[] arrCol = new int[]{0, 1, 0, -1};
-    public static Queue<point> q = new LinkedList<>();
 
     public static int n, m;
+    public static int inMin = MAX_HEIGHT;
+    public static int inMax = 0;
 
     public static void initialize(){
+        q.clear();
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                visited[i][j] = false;
+                vistied[i][j] = false;
             }
         }
     }
 
-    public static boolean canGo(point curPoint, int postRow, int postCol, int range){
-        if(postRow < 0 || postCol < 0 || postRow >= n || postCol >= m){
+    public static boolean inRange(int row, int col){
+        return row >= 0 && col >= 0 && row < n && col < m;
+    }
+
+    public static boolean canGo(int row, int col, int min, int max){
+        if(!inRange(row, col)){
             return false;
         }
 
-        int tempMin = Math.min(curPoint.min, table[postRow][postCol]);
-        int tempMax = Math.max(curPoint.max, table[postRow][postCol]);
-        if(tempMax - tempMin > range){
+        if(table[row][col] < min || table[row][col] > max){
             return false;
         }
 
         return true;
     }
 
-    public static boolean canReach(int range){
-        q.clear();
-        initialize();
-        q.add(new point(0, 0, table[0][0], table[0][0]));
-        while(!q.isEmpty()){
-            point curPoint = q.poll();
+    public static boolean BFS(int length){
+        for(int j = inMin ; j <= inMax ; j++){
+            initialize();
+            q.add(new point(0, 0));
+            while(!q.isEmpty()){
+                point curPoint = q.poll();
+                
+                if(vistied[curPoint.row][curPoint.col]){
+                    continue;
+                }
 
-            if(visited[curPoint.row][curPoint.col]){
-                continue;
-            }
+                if(curPoint.row == n - 1 && curPoint.col == m - 1){
+                    return true;
+                }
 
-            visited[curPoint.row][curPoint.col] = true;
+                vistied[curPoint.row][curPoint.col] = true;
 
-            if(curPoint.row == n - 1 && curPoint.col == m - 1){
-                return true;
-            }
+                for(int i = 0 ; i < 4 ; i++){
+                    int postRow = curPoint.row + arrRow[i];
+                    int postCol = curPoint.col + arrCol[i];
 
-            for(int i = 0 ; i < 4 ; i++){
-                int postRow = curPoint.row + arrRow[i];
-                int postCol = curPoint.col + arrCol[i];
-
-                if(canGo(curPoint, postRow, postCol, range)){
-                    q.add(new point(postRow, postCol, Math.min(curPoint.min, table[postRow][postCol]), Math.max(curPoint.max, table[postRow][postCol])));
+                    if(canGo(postRow, postCol, j, j + length)){
+                        q.add(new point(postRow, postCol));
+                    }
                 }
             }
         }
+
         return false;
     }
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
         for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
             for(int j = 0 ; j < m ; j++){
-                table[i][j] = sc.nextInt();
+                table[i][j] = Integer.parseInt(st.nextToken());
+                inMin = Math.min(inMin, table[i][j]);
+                inMax = Math.max(inMax, table[i][j]);
             }
         }
-
-        // System.out.println(canReach(307));
 
         int start = 0;
         int end = MAX_HEIGHT;
         int ans = MAX_HEIGHT;
-
-        for(int i = 0 ; i <= 500 ; i++){
-            if(canReach(i)){
-                System.out.print(i);
-                System.exit(0);
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(BFS(mid)){
+                // System.out.println("yes");
+                end = mid - 1;
+                ans = Math.min(ans, mid);
+            }
+            else{
+                // System.out.println("no");
+                start = start + 1;
             }
         }
 
-        // while(start <= end){
-        //     int mid = (end + start) / 2;
-        //     if(canReach(mid)){
-        //         end = mid - 1;
-        //         ans = Math.min(ans, mid);
-        //     }
-        //     else{
-        //         start = mid + 1;
-        //     }
-        //     System.out.println(start + " " + end);
-        // }
-
-        // System.out.print(ans);
+        bw.write(ans + "");
+        
+        br.close();
+        bw.close();
     }
 }
