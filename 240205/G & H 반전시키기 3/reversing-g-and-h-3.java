29d24d462800ab82cd52919fa0447bb9cce5f6db
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-05 19:40:45 +0900

    Title: G & H 반전시키기 3, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240205/G & H 반전시키기 3/reversing-g-and-h-3.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_CNT = 4;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        char[] inArr = br.readLine().toCharArray();
+        char[] targetArr = br.readLine().toCharArray();
+
+        int ans = 0;
+        int cnt = 0;
+        boolean triger = false;
+        for(int i = 0 ; i < n ; i++){
+            if(inArr[i] != targetArr[i]){
+                cnt++;
+            }
+            else{
+                if(cnt > 0){
+                    cnt = 0;
+                    ans++;
+                }
+            }
+
+            if(cnt >= MAX_CNT){
+                ans++;
+                cnt = 0;
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
