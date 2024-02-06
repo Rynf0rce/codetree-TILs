9da6cce0f2ac3c4e6f750651d632f38e8eaa94d7
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-06 18:02:40 +0900

    Title: 천 개의 정거장, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240206/천 개의 정거장/thousand-stops.java
+++ 240206/천 개의 정거장/thousand-stops.java
@@ -2,27 +2,42 @@ import java.util.*;
 import java.io.*;
 
 class vertex implements Comparable<vertex>{
-    int idx, weight, bus;
+    int idx;
+    int bus;
+    long weight;
+    HashSet<Integer> hs;
 
-    public vertex(int idx, int weight, int bus){
+    public vertex(int idx, int bus, long weight){
         this.idx = idx;
-        this.weight = weight;
         this.bus = bus;
+        this.weight = weight;
+        this.hs = new HashSet<>();
     }
 
+    
     @Override
     public int compareTo(vertex v){
-        return this.weight - v.weight;
+        if(this.weight > v.weight){
+            return 1;
+        }
+        else if(this.weight == v.weight){
+            return 0;
+        }
+        else{
+            return -1;
+        }
     }
+
 }
 
-public class Main{
+public class Main {
     public static final int MAX_VERTEX = 1000;
-    public static final int MAX_COST = (int)1e9 + 1;
+    public static final long MAX_VALUE = (long)1e13;
     public static ArrayList<vertex>[] vertexList = new ArrayList[MAX_VERTEX + 1];
+    public static long[] costArr = new long[MAX_VERTEX + 1];
+    public static boolean[] visited = new boolean[MAX_VERTEX + 1];
+    public static int[] cntArr = new int[MAX_VERTEX + 1];
     public static PriorityQueue<vertex> pq = new PriorityQueue<>();
-    public static int[] pathArr = new int[MAX_VERTEX + 1];
-    public static int[] dist = new int[MAX_VERTEX + 1];
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,10 +48,10 @@ public class Main{
 
         for(int i = 1 ; i <= MAX_VERTEX ; i++){
             vertexList[i] = new ArrayList<vertex>();
-            dist[i] = MAX_COST;
+            costArr[i] = MAX_VALUE;
         }
 
-        for(int i = 0 ; i < N ; i++){
+        for(int i = 1 ; i <= N ; i++){
             st = new StringTokenizer(br.readLine(), " ");
             int cost = Integer.parseInt(st.nextToken());
             int num = Integer.parseInt(st.nextToken());
@@ -48,62 +63,46 @@ public class Main{
                 }
                 else{
                     int curIdx = Integer.parseInt(st.nextToken());
-                    vertexList[preIdx].add(new vertex(curIdx, cost, i));
+                    vertexList[preIdx].add(new vertex(curIdx, 1, cost));
                     preIdx = curIdx;
                 }
             }
         }
 
-        dist[A] = 0;
-        pathArr[A] = -1;
-        pq.add(new vertex(A, dist[A], -1));
+        //costArr[A] = 0;
+        // pq.add(new vertex(A, 0, 0)); // idx, bus, weight;
+        vertex temp = new vertex(A, 0, 0);
+        pq.add(temp);
         while(!pq.isEmpty()){
             vertex curVertex = pq.poll();
-            if(curVertex.weight != dist[curVertex.idx]){
+
+            if(curVertex.hs.contains(curVertex.idx)){
                 continue;
+            }
+
+            if(costArr[curVertex.idx] > curVertex.weight){
+                costArr[curVertex.idx] = curVertex.weight;
+                cntArr[curVertex.idx] = curVertex.hs.size();
             }
 
             for(int i = 0 ; i < vertexList[curVertex.idx].size() ; i++){
                 vertex postVertex = vertexList[curVertex.idx].get(i);
-                if(curVertex.bus == postVertex.bus && dist[postVertex.idx] > curVertex.weight){
-                    dist[postVertex.idx] = dist[curVertex.idx];
-                    pathArr[postVertex.idx] = curVertex.idx;
-                    pq.add(new vertex(postVertex.idx, dist[postVertex.idx], postVertex.bus));
+                vertex inVertex = new vertex(postVertex.idx, postVertex.bus, curVertex.weight);
+                inVertex.hs = curVertex.hs;
+                inVertex.hs.add(curVertex.idx);
+                if(curVertex.bus != postVertex.bus){
+                    inVertex.weight += postVertex.weight;
                 }
-                else if(dist[postVertex.idx] > curVertex.weight + postVertex.weight){
-                    dist[postVertex.idx] = curVertex.weight + postVertex.weight;
-                    pathArr[postVertex.idx] = curVertex.idx;
-                    pq.add(new vertex(postVertex.idx, dist[postVertex.idx], postVertex.bus));
-                }
+                pq.add(inVertex);
             }
-
-            // for(int i = 1 ; i <= 5 ; i++){
-            //     System.out.print(dist[i] + " ");
-            // }
-            // System.out.println();
-            // for(int i = 1 ; i <= 5 ; i++){
-            //     System.out.print(pathArr[i] + " ");
-            // }
-            // System.out.println();
         }
-
-        if(dist[B] == MAX_COST){
-            System.out.println("-1 -1");
-            System.exit(0);
-        }
-        
-        int time = 1;
-        int path = B;
-        while(pathArr[path] != A){
-            path = pathArr[path];
-            time++;
-        }
-        System.out.print(dist[B] + " " + time);
 
         // for(int i = 1 ; i <= 5 ; i++){
-        //     System.out.print(dist[i] + " ");
+        //     System.out.print(costArr[i] + " ");
         // }
 
-        // System.out.println(dist[B]);
+        System.out.print(costArr[B] + " " + cntArr[B]);
+
+
     }
 }
