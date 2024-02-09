ea443292cd6fb6cef5bed426ccac320e3d2a1616
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-09 11:24:06 +0900

    Title: 간선의 길이를 2배 늘리기, Time: 500ms, Memory: 13MB, Status: Passed - CodeTree

--- 240209/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.java
+++ 240209/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.java
@@ -1,169 +1,90 @@
 import java.util.*;
 import java.io.*;
 
-class node implements Comparable<node>{
-    int idx;
-    long weight;
-
-    public node(int idx, long weight){
-        this.idx = idx;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(node n){
-        if(this.weight > n.weight){
-            return 1;
-        }
-        else if(this.weight == n.weight){
-            return 0;
-        }
-        else{
-            return -1;
-        }
-    }
-}
-
-class load implements Comparable<load>{
-    int start;
-    int end;
-    long weight;
-
-    public load(int start, int end, long weight){
-        this.start = start;
-        this.end = end;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(load l){
-        if(this.weight > l.weight){
-            return 1;
-        }
-        else if(this.weight == l.weight){
-            return 0;
-        }
-        else{
-            return -1;
-        }
-    }
-}
-
-
 public class Main {
     public static final int MAX_NODE = 250;
-    public static final long INVALID = (long)1e12;
-    public static ArrayList<node>[] nodeList = new ArrayList[MAX_NODE + 1];
-    public static PriorityQueue<node> pq = new PriorityQueue<>();
-    public static TreeSet<load> loadSet = new TreeSet<>();
+    public static final long INVALIED = (long) 1e11;
+    public static long[][] graph = new long[MAX_NODE + 1][MAX_NODE + 1];
     public static long[] dist = new long[MAX_NODE + 1];
-    public static int[] path = new int[MAX_NODE + 1];
+    public static boolean[] visited = new boolean[MAX_NODE + 1];
     public static int N = -1;
 
-    public static void dijkstra(){
-        for(int i = 2 ; i <= MAX_NODE ; i++){
-            dist[i] = INVALID;
-            path[i] = 1;
+    public static void initialize(){
+        for(int i = 1 ; i <= N ; i++){
+            dist[i] = INVALIED;
+            visited[i] = false;
         }
+    }
 
-        pq.add(new node(1, dist[1]));
-        while(!pq.isEmpty()){
-            node curNode = pq.poll();
-            if(curNode.weight != dist[curNode.idx]){
-                continue;
+    public static void dijkstra(){
+        initialize();
+        dist[1] = 0;
+        for(int i = 0 ; i < N ; i++){
+            int minIdx = -1;
+            for(int j = 1 ; j <= N ; j++){
+                if(visited[j]){
+                    continue;
+                }
+
+                if(minIdx == -1 || dist[minIdx] > dist[j]){
+                    minIdx = j;
+                }
             }
 
-            for(int i = 0 ; i < nodeList[curNode.idx].size() ; i++){
-                node postNode = nodeList[curNode.idx].get(i);
-                if(dist[postNode.idx] > postNode.weight + curNode.weight){
-                    dist[postNode.idx] = postNode.weight + curNode.weight;
-                    pq.add(new node(postNode.idx, dist[postNode.idx]));
-                    path[postNode.idx] = curNode.idx;
+            visited[minIdx] = true;
+
+            for(int j = 1 ; j <= N ; j++){
+                if(graph[minIdx][j] == INVALIED){
+                    continue;
+                }
+
+                if(dist[j] > dist[minIdx] + graph[minIdx][j]){
+                    dist[j] = dist[minIdx] + graph[minIdx][j];
                 }
             }
         }
     }
 
-    public static void upScale(){
-        int curIdx = N;
-        loadSet.clear();
-        while(curIdx != 1){
-            loadSet.add(new load(path[curIdx], curIdx, dist[curIdx] - dist[path[curIdx]]));
-            curIdx = path[curIdx];
-        }
-
-        for(int i = 0 ; i < nodeList[loadSet.last().start].size() ; i++){
-            node savedNode = nodeList[loadSet.last().start].get(i);
-            if(savedNode.idx == loadSet.last().end){
-                nodeList[loadSet.last().start].remove(savedNode);
-                nodeList[loadSet.last().start].add(new node(savedNode.idx, savedNode.weight * 2));
-            }
-        }
-
-        for(int i = 0 ; i < nodeList[loadSet.last().end].size() ; i++){
-            node savedNode = nodeList[loadSet.last().end].get(i);
-            if(savedNode.idx == loadSet.last().start){
-                nodeList[loadSet.last().end].remove(savedNode);
-                nodeList[loadSet.last().end].add(new node(savedNode.idx, savedNode.weight * 2));
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        for(int i = 1 ; i <= N ; i++){
-            nodeList[i] = new ArrayList<>();
-        }
 
+        for(int i = 1 ; i <= N ; i++){
+            for(int j = 1 ; j <= N ; j++){
+                graph[i][j] = INVALIED;
+            }
+        }
+        initialize();
+
+        int M = Integer.parseInt(st.nextToken());
         for(int i = 0 ; i < M ; i++){
             st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             long weight = Long.parseLong(st.nextToken());
-
-            nodeList[start].add(new node(end, weight));
-            nodeList[end].add(new node(start, weight));
+            graph[start][end] = weight;
+            graph[end][start] = weight;
         }
 
         dijkstra();
+        long preVal = dist[N];
 
-        long ans = dist[N];
+        long postVal = 0;
+        for(int i = 1 ; i <= N ; i++){
+            for(int j = i + 1 ; j <= N ; j++){
+                if(graph[i][j] != INVALIED){
+                    graph[i][j] *= 2;
+                    graph[j][i] *= 2;
+                    dijkstra();
+                    postVal = Math.max(postVal, dist[N]);
+                    graph[i][j] /= 2;
+                    graph[j][i] /= 2;
+                }
+            }
+        }
 
-        // for(int i = 1 ; i <= N ; i++){
-        //     System.out.print(dist[i] + " ");
-        // }
-
-        // System.out.println();
-
-        // for(int i = 1 ; i <= N ; i++){
-        //     System.out.print(path[i] + " ");
-        // }
-
-        // System.out.println();
-
-        upScale();
-
-        // for(load l : loadSet){
-        //     System.out.println(l.start + " " + l.end + " " + l.weight);
-        // }
-
-        dijkstra();
-
-        // for(int i = 1 ; i <= N ; i++){
-        //     System.out.print(dist[i] + " ");
-        // }
-
-        // System.out.println();
-
-        // for(int i = 1 ; i <= N ; i++){
-        //     System.out.print(path[i] + " ");
-        // }
-
-        // System.out.println();
-
-        System.out.print(dist[N] - ans);
+        System.out.print(postVal - preVal);
+        
     }
 }
