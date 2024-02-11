cddf0fde615b4d9167af79f8fb30b21c985b636c
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-11 17:24:34 +0900

    Title: 이동 가능한 점들의 쌍, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240211/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.java
+++ 240211/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.java
@@ -33,7 +33,7 @@ public class Main {
         for(int i = 1 ; i <= N ; i++){
             for(int j = 1; j <= N ; j++){
                 for(int k = 1 ; k <= N ; k++){
-                    if(dist[j][k] >= dist[j][i] + dist[i][k]){
+                    if(dist[j][k] > dist[j][i] + dist[i][k]){
                         dist[j][k] = dist[j][i] + dist[i][k];                        
                     }
                 }
