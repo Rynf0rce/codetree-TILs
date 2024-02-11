e59ab8a146ec80c6f110798848f908a212b19d24
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-11 17:24:11 +0900

    Title: 이동 가능한 점들의 쌍, Time: 134ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240211/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.java
+++ 240211/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.java
@@ -3,9 +3,8 @@ import java.io.*;
 
 public class Main {
     public static final int MAX_NODE = 200;
-    public static final int INVALID = (int) 10000000000L;
-    public static int[][] dist = new int[MAX_NODE + 1][MAX_NODE + 1];
-    public static boolean[][] visited = new boolean[MAX_NODE + 1][MAX_NODE + 1];
+    public static final long INVALID = (long)1e11;
+    public static long[][] dist = new long[MAX_NODE + 1][MAX_NODE + 1];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,40 +19,46 @@ public class Main {
                 dist[i][j] = INVALID;
             }
             dist[i][i] = 0;
-            visited[i][i] = true;
         }
 
         for(int i = 0 ; i < N ; i++){
             st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
+            long weight = Long.parseLong(st.nextToken());
             dist[start][end] = weight;
         }
 
-        for(int i = 1 ; i <= P ; i++){
+
+        for(int i = 1 ; i <= N ; i++){
             for(int j = 1; j <= N ; j++){
                 for(int k = 1 ; k <= N ; k++){
-                    if(dist[j][k] > dist[j][i] + dist[i][k]){
-                        visited[j][i] = true;
-                        visited[i][k] = true;
-                        visited[j][k] = true;
-                        dist[j][k] = dist[j][i] + dist[i][k];
+                    if(dist[j][k] >= dist[j][i] + dist[i][k]){
+                        dist[j][k] = dist[j][i] + dist[i][k];                        
                     }
                 }
             }
         }
         
         int cnt = 0;
-        long ans = 0;
+        long sumVal = 0;
         for(int i = 0 ; i < Q ; i++){
             st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            if(dist[start][end] != INVALID && visited[start][end]){
+            long minVal = INVALID;
+            for(int j = 1 ; j <= P ; j++){
+                if(dist[start][j] == INVALID || dist[j][end] == INVALID){
+                    continue;
+                }
+
+                minVal = Math.min(minVal, dist[start][j] + dist[j][end]);
+            }
+
+            if(minVal != INVALID){
                 cnt++;
-                ans += dist[start][end];
-            }            
+                sumVal += minVal;
+            }         
         }
 
         // for(int i = 1 ; i <= N ; i++){
@@ -63,14 +68,7 @@ public class Main {
         //     System.out.println();
         // }
 
-        // for(int i = 1 ; i <= N ; i++){
-        //     for(int j = 1 ; j <= N ; j++){
-        //         System.out.print(visited[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-
         System.out.println(cnt);
-        System.out.print(ans);
+        System.out.print(sumVal);
     }
 }
