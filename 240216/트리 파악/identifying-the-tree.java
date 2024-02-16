8751e019f4253363540d0a30b3c778853f5b87a1
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-16 11:15:09 +0900

    Title: 트리 파악, Time: 631ms, Memory: 50MB, Status: Passed - CodeTree

--- 240216/트리 파악/identifying-the-tree.java
+++ 240216/트리 파악/identifying-the-tree.java
@@ -1,45 +1,46 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static final int MAX_NODE = 100000;
     public static ArrayList<Integer>[] nodeList = new ArrayList[MAX_NODE + 1];
-    public static int[] parent = new int[MAX_NODE + 1];
     public static boolean[] visited = new boolean[MAX_NODE + 1];
-    public static ArrayList<Integer> leafList = new ArrayList<>();
     public static int total_depth = 0;
 
     public static void traversal(int idx, int depth){
         boolean isLeaf = true;
+
         for(int i = 0 ; i < nodeList[idx].size() ; i++){
             int curIdx = nodeList[idx].get(i);
             if(!visited[curIdx]){
                 isLeaf = false;
                 visited[curIdx] = true;
-                parent[curIdx] = idx;
                 traversal(curIdx, depth + 1);
             }
         }
+
         if(isLeaf){
-            leafList.add(idx);
             total_depth += depth;
         }
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
         for(int i = 1 ; i <= n ; i++){
             nodeList[i] = new ArrayList<>();
         }
 
+        StringTokenizer st;
         for(int i = 1 ; i < n ; i++){
-            int start = sc.nextInt();
-            int end = sc.nextInt();
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
             nodeList[start].add(end);
             nodeList[end].add(start);
         }
 
         visited[1] = true;
-        parent[1] = 1;
         traversal(1, 0);
 
         System.out.println(total_depth % 2);
