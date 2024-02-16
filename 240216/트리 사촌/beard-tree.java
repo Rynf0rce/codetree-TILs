bfdcdb9e219a6b95c338418f29fc694f09ada81b
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-16 12:28:09 +0900

    Title: 트리 사촌, Time: 147ms, Memory: 13MB, Status: Passed - CodeTree

--- 240216/트리 사촌/beard-tree.java
+++ 240216/트리 사촌/beard-tree.java
@@ -4,6 +4,51 @@ import java.io.*;
 public class Main {
     public static final int MAX_NODE = 1000;
     public static final int MAX_IDX = 1000000;
+    public static int[] arr = new int[MAX_NODE + 1];
+    public static int[] parentArr = new int[MAX_IDX + 1];
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 1 ; i <= n ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int parents = 0;
+        int child = 1;
+        while(child != n){
+            if(arr[child + 1] > arr[child] + 1){
+                parents++;
+            }
+            child++;
+            parentArr[arr[child]] = arr[parents];
+        }
+
+        int ans = 0;
+        for(int i = 1 ; i <= MAX_IDX ; i++){
+            if(parentArr[parentArr[i]] == 0 || parentArr[i] == 0){
+                continue;
+            }
+            
+            if(parentArr[parentArr[m]] == parentArr[parentArr[i]] && parentArr[m] != parentArr[i]){
+                ans++;
+            }
+        }
+        System.out.print(ans);
+    }
+}
+
+/*
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_NODE = 1000;
+    public static final int MAX_IDX = 1000000;
     public static ArrayList<Integer>[] nodeList = new ArrayList[MAX_IDX + 1];
     public static int[] arr = new int[MAX_NODE + 1];
     public static boolean[] visited = new boolean[MAX_IDX + 1];
@@ -58,3 +103,4 @@ public class Main {
         System.out.print(ans);
     }
 }
+*/
