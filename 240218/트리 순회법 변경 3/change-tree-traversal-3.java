2692f194f851e924b4eb68a0211bd9d76e0640a3
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-18 14:54:46 +0900

    Title: 트리 순회법 변경 3, Time: 1043ms, Memory: 37MB, Status: Passed - CodeTree

---
+++ 240218/트리 순회법 변경 3/change-tree-traversal-3.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_NODE = 100000;
+    public static int[] preArr = new int[MAX_NODE + 1];
+    public static int[] inArr = new int[MAX_NODE + 1];
+    public static int[] postArr = new int[MAX_NODE + 1];
+    public static int n = -1;
+    public static int point = -1;
+
+    public static void dfs(int postStart, int postEnd, int inStart, int inEnd){
+        // System.out.println(postStart + " " + postEnd + " " + inStart + " " + inEnd);
+        if(postEnd < postStart){
+            return;
+        }
+
+        if(postStart == postEnd){
+            preArr[point--] = postArr[postEnd];
+            return;
+        }
+
+        int rootIdx = -1;
+        for(int i = inStart ; i <= inEnd ; i++){
+            if(postArr[postEnd] == inArr[i]){
+                rootIdx = i;
+                break;
+            }
+        }
+
+        int left = rootIdx - inStart;
+        int right = inEnd - rootIdx;
+
+        dfs(postEnd - right, postEnd - 1, rootIdx + 1, inEnd);
+        dfs(postStart, postEnd - right - 1, inStart , rootIdx - 1);
+
+        preArr[point--] = postArr[postEnd];
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        point = n;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 1 ; i <= n ; i++){
+            inArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 1 ; i <= n ; i++){
+            postArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(1, n, 1, n);
+
+        for(int i = 1 ; i <= n ; i++){
+            System.out.print(preArr[i] + " ");
+        }
+    }
+}
