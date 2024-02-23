7bcec49890b1a0ae58918c76440990ce0fd0c955
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-23 19:31:08 +0900

    Title: 칸 합치기 2, Time: 1183ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240223/칸 합치기 2/can.java
+++ 240223/칸 합치기 2/can.java
@@ -2,49 +2,30 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int MAX_NODE = 100000;
-    public static int[] uf = new int[MAX_NODE + 1];
-    public static int[] cnt = new int[MAX_NODE + 1];
-    
-    public static int find(int idx){
-        if(uf[idx] == idx){
-            return idx;
-        }
-        return uf[idx] = find(uf[idx]);
-    }
-
-    public static void union(int a, int b){
-        int rootA = find(a);
-        int rootB = find(b);
-        if(rootA != rootB){
-            uf[rootA] = rootB;
-            cnt[rootB] += cnt[rootA];
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static HashSet<Integer> nodeSet = new HashSet<>();
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         for(int i = 1 ; i <= n ; i++){
-            uf[i] = i;
-            cnt[i] = 1;
+            nodeSet.add(i);
         }
 
-        int ans = n;
+        StringBuilder sb = new StringBuilder();
         for(int i = 0 ; i < m ; i++){
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            for(int j = b ; j >= 0 ; j--){
-                if(find(a) == find(j)){
-                    break;
-                }
-                ans--;
-                union(j, a);
+            for(int j = b ; j > a ; j--){
+                nodeSet.remove(j);
             }
-            System.out.println(ans);
+            sb.append(nodeSet.size() + "\n");
         }
+        bw.write(sb.toString());
+        
+        br.close();
+        bw.close();
     }
 }
