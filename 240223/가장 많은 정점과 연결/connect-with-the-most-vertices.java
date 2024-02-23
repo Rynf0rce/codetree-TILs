0cd4feb0f1377e94abe6aceba3287d258fbabd58
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-23 10:48:31 +0900

    Title: 가장 많은 정점과 연결, Time: 110ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240223/가장 많은 정점과 연결/connect-with-the-most-vertices.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_NODE = 100000;
+    public static int[] uf = new int[MAX_NODE + 1];
+    public static int[] minArr = new int[MAX_NODE + 1];
+    public static boolean[] visited = new boolean[MAX_NODE + 1];
+
+    public static int find(int idx){
+        if(uf[idx] == idx){
+            return idx;
+        }
+        int rootIdx = find(uf[idx]);
+        uf[idx] = rootIdx;
+        return rootIdx;
+    }
+
+    public static void union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+        if(rootA != rootB){
+            minArr[rootB] = Math.min(minArr[rootB], minArr[rootA]);
+            uf[rootA] = rootB;
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 1 ; i <= n ; i++){
+            uf[i] = i;
+            minArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0 ; i < m ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            union(a, b);
+        }
+
+        int ans = 0;
+        for(int i = 1 ; i <= n ; i++){
+            int curIdx = find(i);
+            if(visited[curIdx]){
+                continue;
+            }
+            visited[curIdx] = true;
+            ans += minArr[curIdx];
+        }
+
+        System.out.print(ans);
+    }
+}
