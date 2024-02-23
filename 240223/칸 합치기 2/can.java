869596353fca7a578635a971158ad44be587299d
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-23 21:26:08 +0900

    Title: 칸 합치기 2, Time: 516ms, Memory: 34MB, Status: Passed - CodeTree

--- 240223/칸 합치기 2/can.java
+++ 240223/칸 합치기 2/can.java
@@ -2,7 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static HashSet<Integer> nodeSet = new HashSet<>();
+    public static final int MAX_NODE = 100000;
+    public static int[] uf = new int[MAX_NODE + 1];
+    
+    public static int find(int idx){
+        if(uf[idx] == idx){
+            return idx;
+        }
+        return uf[idx] = find(uf[idx]);
+    }
+
+    public static void union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+        uf[rootA] = rootB;
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -10,21 +25,29 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         for(int i = 1 ; i <= n ; i++){
-            nodeSet.add(i);
+            uf[i] = i;
         }
 
+        int ans = n;
         StringBuilder sb = new StringBuilder();
         for(int i = 0 ; i < m ; i++){
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            for(int j = b ; j > a ; j--){
-                nodeSet.remove(j);
+            int j = a;
+            while(j < b){
+                int root = find(j);
+                if(root >= b){
+                    break;
+                }
+                union(j, b);
+                ans--;
+                j = root + 1;
             }
-            sb.append(nodeSet.size() + "\n");
+            sb.append(ans + "\n");
         }
-        bw.write(sb.toString());
-        
+
+        bw.write(sb.toString()); 
         br.close();
         bw.close();
     }
