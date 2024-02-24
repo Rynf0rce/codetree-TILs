14e1560ac5d271db8696538682796c3d3f0a26fb
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-24 09:58:30 +0900

    Title: 연결된 정점 2, Time: 2024ms, Memory: 38MB, Status: Passed - CodeTree

---
+++ 240224/연결된 정점 2/connected-vertex-2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_NODE = 100000;
+    public static int[] uf = new int[MAX_NODE + 1];
+    public static int[] cnt = new int[MAX_NODE + 1];
+    public static int find(int idx){
+        if(uf[idx] == idx){
+            return idx;
+        }
+        return uf[idx] = find(uf[idx]);
+    }
+
+    public static void union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+        if(rootA != rootB){
+            uf[rootA] = rootB;
+            cnt[rootB] += cnt[rootA];
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1 ; i <= MAX_NODE ; i++){
+            uf[i] = i;
+            cnt[i] = 1;
+        }
+        
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            union(a, b);
+            System.out.println(cnt[find(b)]);
+        }
+
+    }
+}
