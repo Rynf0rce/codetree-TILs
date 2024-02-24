92a138d54b4b18498ffce69dd26c7150e45e8ee6
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-24 10:47:29 +0900

    Title: 최소 스패닝 트리, Time: 635ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240224/최소 스패닝 트리/minimum-spanning-tree.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+class path implements Comparable<path>{
+    int left, right, weight;
+    public path(int left, int right, int weight){
+        this.left = left;
+        this.right = right;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(path p){
+        return this.weight - p.weight;
+    }
+}
+
+public class Main {
+    public static int MAX_NODE = 10000;
+    public static int[] uf = new int[MAX_NODE + 1];
+    public static int[] cnt = new int[MAX_NODE + 1];
+    public static PriorityQueue<path> pq = new PriorityQueue<>();
+
+    public static int find(int idx){
+        if(uf[idx] == idx){
+            return idx;
+        }
+        return uf[idx] = find(uf[idx]);
+    }
+
+    public static void union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+        if(rootA != rootB){
+            uf[rootA] = rootB;
+            cnt[rootB] = cnt[rootA];
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for(int i = 1 ; i <= n ; i++){
+            uf[i] = i;
+        }
+
+        for(int i = 0 ; i < m ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            pq.add(new path(a, b, weight));
+        }
+
+        int ans = 0;
+        while(!pq.isEmpty()){
+            path curPath = pq.poll();
+            if(find(curPath.left) == find(curPath.right)){
+                continue;
+            }
+
+            union(curPath.left, curPath.right);
+            ans += curPath.weight;
+            if(cnt[find(curPath.left)] == n){
+                break;
+            }
+        }
+        System.out.print(ans);
+    }
+}
