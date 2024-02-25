d8b0cbab369148176abb522fc92df6b08d3a52c1
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-25 11:58:19 +0900

    Title: 최소 스패닝 트리 8, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240225/최소 스패닝 트리 8/minimum-spanning-tree-8.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_NODE = 500;
+    public static final int INF = (int)1e9;
+    public static int[][] table = new int[MAX_NODE + 1][MAX_NODE + 1];
+    public static boolean[] visited = new boolean[MAX_NODE + 1];
+    public static int[] dist = new int[MAX_NODE + 1];
+    public static int n = -1;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for(int i = 0 ; i < m ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            table[a][b] = w;
+            table[b][a] = w;
+        }
+
+        for(int i = 1 ; i <= n ; i++){
+            dist[i] = INF;
+        }
+
+        int ans = 0;
+        dist[1] = 0;
+        for(int i = 1 ; i <= n ; i++){
+            int minIdx = -1;
+            for(int j = 1 ; j <= n ; j++){
+                if(visited[j]){
+                    continue;
+                }
+
+                if(minIdx == -1 || dist[j] < dist[minIdx]){
+                    minIdx = j;
+                }
+            }
+
+            visited[minIdx] = true;
+            ans += dist[minIdx];
+            for(int j = 1 ; j <= n ; j++){
+                if(table[minIdx][j] == 0){
+                    continue;
+                }
+
+                dist[j] = Math.min(dist[j], table[minIdx][j]);
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
