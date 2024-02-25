5e0eb511f699c5d5b1e3733a02c2b2ba5480cb06
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-25 12:02:32 +0900

    Title: 최소 스패닝 트리 8, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240225/최소 스패닝 트리 8/minimum-spanning-tree-8.java
+++ 240225/최소 스패닝 트리 8/minimum-spanning-tree-8.java
@@ -32,7 +32,7 @@ public class Main {
         for(int i = 1 ; i <= n ; i++){
             int minIdx = -1;
             for(int j = 1 ; j <= n ; j++){
-                if(visited[j]){
+                if(visited[j] || dist[j] == INF){
                     continue;
                 }
 
