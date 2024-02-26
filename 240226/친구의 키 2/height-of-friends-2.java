5673bd0f3a3ac00bc6580f779dd00e9ef5df14df
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-26 18:35:50 +0900

    Title: 친구의 키 2, Time: 420ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240226/친구의 키 2/height-of-friends-2.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_NODE = 100000;
+    public static ArrayList<Integer>[] nodeList = new ArrayList[MAX_NODE + 1];
+    public static int[] inOrder = new int[MAX_NODE + 1];
+    public static Queue<Integer> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        for(int i = 1 ; i <= n ; i++){
+            nodeList[i] = new ArrayList<>();
+        }
+
+        int m = Integer.parseInt(st.nextToken());
+        for(int i = 0 ; i < m ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            nodeList[start].add(end);
+            inOrder[end]++;
+        }
+
+        for(int i = 1 ; i <= n ; i++){
+            if(inOrder[i] == 0){
+                q.add(i);
+            }
+        }
+
+        boolean flag = false;
+        int cnt = 0;
+        while(!q.isEmpty()){
+            int curIdx = q.poll();
+            cnt++;
+
+            for(int i = 0 ; i < nodeList[curIdx].size() ; i++){
+                int postIdx = nodeList[curIdx].get(i);
+                inOrder[postIdx]--;
+                if(inOrder[postIdx] == 0){
+                    q.add(postIdx);
+                }
+            }
+        }
+
+        System.out.print(cnt == n ? "Consistent" : "Inconsistent");
+    }
+}
