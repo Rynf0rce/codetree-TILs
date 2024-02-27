a9ffe0739c8d9ffc98eb135d1aa790aac9ce1ec1
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-27 16:36:07 +0900

    Title: 새로운 알파벳, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240227/새로운 알파벳/new-alphabet.java
+++ 240227/새로운 알파벳/new-alphabet.java
@@ -8,7 +8,6 @@ public class Main {
     public static int[] panIn = new int[MAX_ALPHABET];
     public static Queue<Integer> q = new LinkedList<>();
     public static String[] arr = new String[MAX_WORD + 1];
-    public static HashSet<Character> alphaSet = new HashSet<>();
     public static HashSet<Integer> inSet = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
@@ -20,9 +19,6 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         for(int i = 0 ; i < n ; i++){
             arr[i] = br.readLine().toString();
-            for(int j = 0 ; j < arr[i].length() ; j++){
-                alphaSet.add(arr[i].charAt(j));
-            }
         }
 
         for(int i = 1 ; i < n ; i++){
@@ -39,21 +35,21 @@ public class Main {
             }
         }
 
-        boolean flag = true;
+        
         for(int idx : inSet){
             if(panIn[idx] == 0){
                 q.add(idx);
             }
-            if(panIn[idx] >= 2){
-                flag = false;
-            }
         }
 
         StringBuilder sb = new StringBuilder();
+        boolean flag = false;
         while(!q.isEmpty()){
+            if(q.size() > 1){
+                flag = true;
+            }
             int curIdx = q.poll();
             sb.append((char)('a'+ curIdx));
-            alphaSet.remove((char)('a' + curIdx));
             for(int i = 0 ; i < nodeList[curIdx].size() ; i++){
                 int postIdx = nodeList[curIdx].get(i);
                 panIn[postIdx]--;
@@ -70,6 +66,6 @@ public class Main {
             }
         }
 
-        System.out.print(alphaSet.isEmpty() && flag ? sb.toString() : "inf");
+        System.out.print(flag ? "inf" : sb.toString());
     }
 }
