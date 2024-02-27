217634408a8dc0c59623944232197858e3649b1b
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-27 14:43:18 +0900

    Title: 친구의 키 4, Time: 965ms, Memory: 62MB, Status: Passed - CodeTree

--- 240227/친구의 키 4/height-of-friends-4.java
+++ 240227/친구의 키 4/height-of-friends-4.java
@@ -7,46 +7,27 @@ class path{
         this.start = start;
         this.end = end;
     }
-    
-    @Override
-    public int hashCode() {
-        return (start + 1) * 100000 + end;
-    }
-
-    public boolean equals(Object obj) {
-        if (obj == null) return false;
-        if (obj == this) return true;
-        if (!(obj instanceof path)) return false;
-        path o = (path) obj;
-        return o.start == this.start && o.end == this.end;
-    }
 }
 
 public class Main {
     public static final int MAX_NODE = 100000;
     public static ArrayList<Integer>[] nodeList = new ArrayList[MAX_NODE + 1];
-    public static int[] panIn = new int[MAX_NODE + 1];
     public static path[] pathArr = new path[MAX_NODE + 1];
-    public static HashSet<path> pathSet = new HashSet<>();
+    public static int[] panIn = new int[MAX_NODE + 1];
     public static Queue<Integer> q = new LinkedList<>();
-    public static boolean[] visited = new boolean[MAX_NODE + 1];
+    public static int n = -1;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+    public static boolean inCycle(int range){
+        q.clear();
         for(int i = 1 ; i <= n ; i++){
-            nodeList[i] = new ArrayList<>();
+            panIn[i] = 0;
+            nodeList[i].clear();
         }
-        
-        for(int i = 1 ; i <= m ; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            pathArr[i] = new path(start, end);
-            nodeList[start].add(end);
-            panIn[end]++;
+
+        for(int i = 1 ; i <= range ; i++){
+            path p = pathArr[i];
+            nodeList[p.start].add(p.end);
+            panIn[p.end]++;
         }
 
         for(int i = 1 ; i <= n ; i++){
@@ -57,40 +38,55 @@ public class Main {
 
         while(!q.isEmpty()){
             int curIdx = q.poll();
-            for(int i = 0 ; i < nodeList[curIdx].size() ; i++){
+            for(int i = 0 ; i < nodeList[curIdx].size(); i++){
                 int postIdx = nodeList[curIdx].get(i);
                 panIn[postIdx]--;
-                pathSet.add(new path(curIdx, postIdx));
                 if(panIn[postIdx] == 0){
                     q.add(postIdx);
                 }
             }
         }
 
-        boolean flag = true;
-        for(int i = 1 ; i <= n ; i++){
-            if(panIn[i] > 0){
-                flag = false;
+        boolean output = false;
+        for(int i = 0 ; i <= n ; i++){
+            if(panIn[i] != 0){
+                output = true;
                 break;
             }
         }
+        return output;
+    }
 
-        if(flag){
-            System.out.print("Consistent");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for(int i = 1 ; i <= n ; i++){
+            nodeList[i] = new ArrayList<>();
         }
-        else{
-            for(int i = 1 ; i <= n ; i++){
-                if(pathSet.contains(pathArr[i])){
-                    continue;
-                }
+        
+        for(int i = 1 ; i <= m ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            pathArr[i] = new path(start, end);
+        }
 
-                if(visited[pathArr[i].start] && visited[pathArr[i].end]){
-                    System.out.print(i);
-                    break;
-                }
-                visited[pathArr[i].start] = true;
-                visited[pathArr[i].end] = true;
+        int left = 1;
+        int right = m;
+        int ans = MAX_NODE + 1;
+        while(left <= right){
+            int mid = (right + left) / 2;
+            if(inCycle(mid)){
+                ans = Math.min(ans, mid);
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
             }
         }
+
+        System.out.print(ans == MAX_NODE + 1 ? "Consistent" : ans);
     }
 }
