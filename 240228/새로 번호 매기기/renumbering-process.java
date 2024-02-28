ab830dd4794e3827ecdfa855ecd69794682d6cc2
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-28 13:19:57 +0900

    Title: 새로 번호 매기기, Time: 1418ms, Memory: 42MB, Status: Passed - CodeTree

--- 240228/새로 번호 매기기/renumbering-process.java
+++ 240228/새로 번호 매기기/renumbering-process.java
@@ -28,7 +28,7 @@ public class Main {
     public static int[] panIn = new int[MAX_NODE + 1];
     public static int[] ans = new int[MAX_NODE + 1];
     public static boolean[] visited = new boolean[MAX_NODE + 1];
-    public static PriorityQueue<node> pq = new PriorityQueue<>();
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -43,41 +43,27 @@ public class Main {
             st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            nodeList[start].add(end);
-            panIn[end]++;
+            nodeList[end].add(start);
+            panIn[start]++;
         }
 
         for(int i = 1 ; i <= n ; i++){
             if(panIn[i] == 0){
-                for(int j = 0 ; j < nodeList[i].size() ; j++){
-                    int postIdx = nodeList[i].get(j);
-                    pq.add(new node(i, postIdx, panIn[postIdx]));
-                }
+                pq.add(i);
             }
         }
 
-        int cnt = 1;
+        int cnt = n;
         while(!pq.isEmpty()){
-            node curNode = pq.poll();
-            
-            if(visited[curNode.left]){
-                continue;
-            }
-            visited[curNode.left] = true;
-            ans[curNode.left] = cnt++;
+            int curIdx = pq.poll();
+            // System.out.print(curIdx + " ");
+            ans[curIdx] = cnt--;
 
-            for(int i = 0 ; i < nodeList[curNode.left].size() ; i++){
-                int postIdx = nodeList[curNode.left].get(i);
+            for(int i = 0 ; i < nodeList[curIdx].size() ; i++){
+                int postIdx = nodeList[curIdx].get(i);
                 panIn[postIdx]--;
                 if(panIn[postIdx] == 0){
-                    for(int j = 0 ; j < nodeList[postIdx].size() ; j++){
-                        int grandPostIdx = nodeList[postIdx].get(j);
-                        pq.add(new node(postIdx, grandPostIdx, panIn[grandPostIdx]));
-                    }
-
-                    if(nodeList[postIdx].size() == 0){
-                        pq.add(new node(postIdx, postIdx, panIn[postIdx]));
-                    }
+                    pq.add(postIdx);
                 }
             }
         }
