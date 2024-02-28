ed210c09435b8173836b055e4b9e37ef1f4e0fdc
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-28 19:24:36 +0900

    Title: 최대로 방문 가능한 경로, Time: 892ms, Memory: 47MB, Status: Passed - CodeTree

--- 240228/최대로 방문 가능한 경로/maximum-visitable-route.java
+++ 240228/최대로 방문 가능한 경로/maximum-visitable-route.java
@@ -7,34 +7,34 @@ public class Main {
     public static int[] inDegree = new int[MAX_NODE + 1];
     public static int[] DP = new int[MAX_NODE + 1];
     public static int[] connect = new int[MAX_NODE + 1];
-    public static Queue<Integer> q = new LinkedList<>();
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>();
     public static ArrayList<Integer> ansList = new ArrayList<>();
 
     public static void topological(){
-        while(!q.isEmpty()){
-            int curIdx = q.poll();
+        while(!pq.isEmpty()){
+            int curIdx = pq.poll();
             for(int i = 0 ; i < nodeList[curIdx].size() ; i++){
                 int postIdx = nodeList[curIdx].get(i);
 
                 inDegree[postIdx]--;
                 if(inDegree[postIdx] == 0){
-                    q.add(postIdx);
+                    pq.add(postIdx);
                 }
             }
         }
     }
 
     public static void findAns(){
-        while(!q.isEmpty()){
-            int curIdx = q.poll();
+        while(!pq.isEmpty()){
+            int curIdx = pq.poll();
             for(int i = 0 ; i < nodeList[curIdx].size() ; i++){
                 int postIdx = nodeList[curIdx].get(i);
 
                 inDegree[postIdx]--;
                 if(inDegree[postIdx] == 0){
-                    q.add(postIdx);
+                    pq.add(postIdx);
                 }
-                
+
                 if(DP[postIdx] < DP[curIdx] + 1){
                     DP[postIdx] = DP[curIdx] + 1;
                     connect[postIdx] = curIdx;                 
@@ -48,6 +48,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
@@ -59,40 +60,51 @@ public class Main {
             st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            nodeList[start].add(end);
-            inDegree[end]++;
+            nodeList[end].add(start);
+            inDegree[start]++;
         }
 
-        for(int i = 2 ; i <= n ; i++){
+        for(int i = 1 ; i < n ; i++){
             if(inDegree[i] == 0){
-                q.add(i);
+                pq.add(i);
                 DP[i] = 1;
             }
         }
 
         topological();
 
-        q.add(1);
-        DP[1] = 1;
+        pq.add(n);
+        DP[n] = 1;
         findAns();
 
-        int idx = n;
+        int idx = 1;
+        boolean flag = true;
         while(true){
             ansList.add(idx);
             idx = connect[idx];
+            if(idx == n){
+                flag = false;
+            }
+
             if(idx == 0){
                 break;
             }
         }
 
-        if(ansList.get(ansList.size() - 1) != 1 || ansList.get(0) != n){
-            System.out.print(-1);
+        StringBuilder sb = new StringBuilder();
+        if(flag){
+            sb.append(-1);
         }
         else{
-            System.out.println(ansList.size());
-            for(int i = ansList.size() - 1 ; i >= 0 ; i--){
-                System.out.print(ansList.get(i) + " ");
+            sb.append(ansList.size() + "\n");
+            for(int i = 0 ; i < ansList.size() ; i++){
+                sb.append(ansList.get(i) + " ");
             }
         }
+
+        bw.write(sb.toString());
+        
+        br.close();
+        bw.close();
     }
 }
