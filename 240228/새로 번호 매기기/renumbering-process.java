2d6f375ffa0f2908795ec9e52a3f529870c50b22
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-02-28 12:34:59 +0900

    Title: 새로 번호 매기기, Time: 784ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240228/새로 번호 매기기/renumbering-process.java
+++ 240228/새로 번호 매기기/renumbering-process.java
@@ -1,12 +1,34 @@
 import java.util.*;
 import java.io.*;
 
+class node implements Comparable<node>{
+    int left, right, in;
+    public node(int left, int right, int in){
+        this.left = left;
+        this.right = right;
+        this.in = in;
+    }
+
+    @Override
+    public int compareTo(node n){
+        if(this.in == n.in){
+            if(this.left == n.left){
+                return this.right - n.right;
+            }
+
+            return this.left - n.left;
+        }
+        return this.in - n.in;
+    }
+}
+
 public class Main {
     public static final int MAX_NODE = 100000;
     public static ArrayList<Integer>[] nodeList = new ArrayList[MAX_NODE + 1];
     public static int[] panIn = new int[MAX_NODE + 1];
     public static int[] ans = new int[MAX_NODE + 1];
-    public static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public static boolean[] visited = new boolean[MAX_NODE + 1];
+    public static PriorityQueue<node> pq = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,20 +49,35 @@ public class Main {
 
         for(int i = 1 ; i <= n ; i++){
             if(panIn[i] == 0){
-                pq.add(i);
+                for(int j = 0 ; j < nodeList[i].size() ; j++){
+                    int postIdx = nodeList[i].get(j);
+                    pq.add(new node(i, postIdx, panIn[postIdx]));
+                }
             }
         }
 
         int cnt = 1;
         while(!pq.isEmpty()){
-            int curIdx = pq.poll();
-            ans[curIdx] = cnt++;
+            node curNode = pq.poll();
+            
+            if(visited[curNode.left]){
+                continue;
+            }
+            visited[curNode.left] = true;
+            ans[curNode.left] = cnt++;
 
-            for(int i = 0 ; i < nodeList[curIdx].size() ; i++){
-                int postIdx = nodeList[curIdx].get(i);
+            for(int i = 0 ; i < nodeList[curNode.left].size() ; i++){
+                int postIdx = nodeList[curNode.left].get(i);
                 panIn[postIdx]--;
                 if(panIn[postIdx] == 0){
-                    pq.add(postIdx);
+                    for(int j = 0 ; j < nodeList[postIdx].size() ; j++){
+                        int grandPostIdx = nodeList[postIdx].get(j);
+                        pq.add(new node(postIdx, grandPostIdx, panIn[grandPostIdx]));
+                    }
+
+                    if(nodeList[postIdx].size() == 0){
+                        pq.add(new node(postIdx, postIdx, panIn[postIdx]));
+                    }
                 }
             }
         }
