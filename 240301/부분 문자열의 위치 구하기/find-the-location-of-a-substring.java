6468973773e2a30cdb6680c045511e0a387b50e5
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-01 17:14:03 +0900

    Title: 부분 문자열의 위치 구하기, Time: 430ms, Memory: 28MB, Status: Passed - CodeTree

--- 240301/부분 문자열의 위치 구하기/find-the-location-of-a-substring.java
+++ 240301/부분 문자열의 위치 구하기/find-the-location-of-a-substring.java
@@ -3,68 +3,57 @@ import java.io.*;
 
 public class Main {
     public static final int MAX_LENGTH = 500000;
-    public static final int[] MOD = new int[]{(int)1e9 + 7, (int)1e9 + 9};
+    public static final int[] MOD = new int[]{(int) 1e9 + 7, (int) 1e9 + 9};
     public static final int[] P = new int[]{31, 37};
     public static long[][] pPow = new long[2][MAX_LENGTH + 1];
-    public static long[] hashIn = new long[2];
-    public static long[] hashCp = new long[2];
+    public static long[] inHash = new long[2];
+    public static long[] targetHash = new long[2];
+    public static int[] exists = new int[2];
 
     public static int toInt(char c){
         return c - 'a' + 1;
     }
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String inStr = String.valueOf(br.readLine());
-        String cpStr = String.valueOf(br.readLine());
-        int inLength = inStr.length();
-        int cpLength = cpStr.length();
-        boolean exist = false;
-        int ans = MAX_LENGTH + 1;
+        String targetStr = String.valueOf(br.readLine());
 
         for(int k = 0 ; k < 2 ; k++){
             pPow[k][0] = 1;
-            for(int i = 1 ; i <= inLength ; i++){
-                pPow[k][i] = (pPow[k][i - 1] * P[k]) % MOD[k];
+            exists[k] = -1;
+            for(int i = 1 ; i <= inStr.length() ; i++){
+                pPow[k][i] = pPow[k][i - 1] * P[k] % MOD[k];
             }
-        }
 
-        for(int k = 0 ; k < 2 ; k++){
-            for(int i = 0 ; i < cpLength ; i++){
-                hashCp[k] = (hashCp[k] + toInt(cpStr.charAt(i)) * pPow[k][cpLength - i - 1]) % MOD[k];
-                hashIn[k] = (hashIn[k] + toInt(inStr.charAt(i)) * pPow[k][cpLength - i - 1]) % MOD[k];
+            // 초기값 정의
+            for(int i = 0 ; i < targetStr.length() ; i++){
+                inHash[k] = (inHash[k] + toInt(inStr.charAt(i)) * pPow[k][targetStr.length() - 1 - i]) % MOD[k];
+                targetHash[k] = (targetHash[k] + toInt(targetStr.charAt(i)) * pPow[k][targetStr.length() - 1 - i]) % MOD[k]; 
             }
-        }
 
-        if(hashCp[0] == hashIn[0] && hashCp[1] == hashIn[1]){
-            ans = 0;
-            exist = true;
-        }
+            if(inHash[k] == targetHash[k]){
+                exists[k] = 0;
+                continue;
+            }
 
-        for(int i = 1 ; i <= inLength - cpLength ; i++){
-            for(int k = 0 ; k < 2 ; k++){
-                hashIn[k] = (hashIn[k] * P[k] - toInt(inStr.charAt(i - 1)) * pPow[k][cpLength] + toInt(inStr.charAt(i + cpLength - 1))) % MOD[k];
+            for(int i = 1 ; i <= inStr.length() - targetStr.length() ; i++){
+                inHash[k] = (inHash[k] * P[k] - toInt(inStr.charAt(i - 1)) * pPow[k][targetStr.length()] + toInt(inStr.charAt(i + targetStr.length() - 1))) % MOD[k];
+                if(inHash[k] < 0){
+                    inHash[k] += MOD[k];
+                }
 
-                if(hashIn[k] < 0){
-                    hashIn[k] += MOD[k];
+                if(inHash[k] == targetHash[k]){
+                    exists[k] = i;
+                    break;
                 }
             }
-
-            if(hashCp[0] == hashIn[0] && hashCp[1] == hashIn[1]){
-                ans = i;
-                exist = true;
-                break;
-            }
         }
 
-        if(exist){
-            System.out.print(ans);
+        if(exists[0] >= 0 && exists[1] >= 0){
+            System.out.print(Math.min(exists[0], exists[1]));
         }
         else{
             System.out.print(-1);
         }
-
-
-        
     }
 }
