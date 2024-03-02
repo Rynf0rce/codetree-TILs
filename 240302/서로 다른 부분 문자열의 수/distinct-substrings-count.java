316b788689f3dc204a9a320400cea4243fd84bd1
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-02 20:26:00 +0900

    Title: 서로 다른 부분 문자열의 수, Time: 4022ms, Memory: 256MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240302/서로 다른 부분 문자열의 수/distinct-substrings-count.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.io.*;
+
+class Tuple{
+    private long a, b;
+    public Tuple(long a, long b){
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Tuple tuple = (Tuple) o;
+        return tuple.a == this.a && tuple.b == this.b;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(a, b);
+    }
+}
+
+public class Main {
+    public static final int MAX_LENGTH = 5000;
+    public static int[] P = new int[]{31, 37};
+    public static int[] MOD = new int[]{(int)1e9 + 7, (int)1e9 + 9};
+    public static long[][] pPow = new long[2][MAX_LENGTH + 1];
+    public static HashMap<Tuple, Integer> ansMap = new HashMap<>();
+    public static String str = " ";
+
+    public static int toInt(char c){
+        return c - 'a' + 1;
+    }
+
+    public static void hashing(int length){
+        long[] h = new long[2];
+        
+        for(int k = 0 ; k < 2 ; k++){
+            for(int i = 0 ; i < length ; i++){
+                h[k] = (h[k] + toInt(str.charAt(i)) * pPow[k][length - 1 - i]) % MOD[k];
+            }
+        }
+
+        Tuple startTuple = new Tuple(h[0], h[1]);
+        ansMap.put(startTuple, ansMap.getOrDefault(startTuple, 0) + 1);
+
+        for(int i = 1 ; i <= str.length() - length ; i++){
+            for(int k = 0 ; k < 2 ; k++){
+                h[k] = (h[k] * P[k] - toInt(str.charAt(i - 1)) * pPow[k][length] + toInt(str.charAt(i + length - 1))) % MOD[k];
+                if(h[k] < 0){
+                    h[k] += MOD[k];
+                }
+            }
+            Tuple inTuple = new Tuple(h[0], h[1]);
+            ansMap.put(inTuple, ansMap.getOrDefault(inTuple, 0) + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str = String.valueOf(br.readLine());
+
+        for(int k = 0 ; k < 2 ; k++){
+            pPow[k][0] = 1;
+            for(int i = 1 ; i <= str.length() ; i++){
+                pPow[k][i] = (pPow[k][i - 1] * P[k]) % MOD[k];
+            }
+        }
+
+        for(int i = 1 ; i <= str.length() ; i++){
+            hashing(i);
+        }
+        
+        System.out.print(ansMap.size());
+    }
+}
