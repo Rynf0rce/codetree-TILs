600d42fcc1b9c2a61282d7544ac302d0af1382b5
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-02 13:22:58 +0900

    Title: 그래프와 문자열, Time: 94ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240302/그래프와 문자열/graph-and-string.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+
+class node{
+    int idx, weight;
+    public node(int idx, int weight){
+        this.idx = idx;
+        this.weight = weight;
+    }
+}
+
+
+
+public class Main {
+    public static final int MAX_NODE = 100000;
+    public static final int[] MOD = new int[]{(int)1e9 + 7, (int)1e9 + 9};
+    public static final int[] P = new int[]{31, 37};
+    public static ArrayList<node>[] nodeList = new ArrayList[MAX_NODE + 1];
+    public static long[][] pPow = new long[2][MAX_NODE + 1];
+    public static long[] tH = new long[2];
+    public static long[] iH = new long[2];
+    public static int[] value = new int[MAX_NODE + 1];
+    public static String str = " ";
+    public static int ans = 0;
+
+    public static int toInt(char c){
+        return c - 'a' - 1;
+    }
+
+    public static void DFS(int idx, int depth, int val){ 
+        value[depth] = val;
+        if(depth <= str.length()){
+            for(int k = 0 ; k < 2 ; k++){
+                iH[k] += val;
+            }
+        }
+        else{
+            for(int k = 0 ; k < 2 ; k++){
+                iH[k] = (iH[k] * P[k] - value[depth - str.length()] * pPow[k][str.length()] + val) % MOD[k];
+            }
+
+            if(iH[0] == tH[0] && iH[1] == tH[1]){
+                ans++;
+            }
+        }
+
+        for(int i = 0 ; i < nodeList[idx].size() ; i++){
+            node postNode= nodeList[idx].get(i);
+            DFS(postNode.idx, depth + 1, postNode.weight);
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        str = String.valueOf(st.nextToken());
+
+        for(int i = 1 ; i <= n ; i++){
+            nodeList[i] = new ArrayList<>();
+        }
+
+        for(int i = 1 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            char c = String.valueOf(st.nextToken()).charAt(0);
+            nodeList[start].add(new node(end, toInt(c)));
+        }
+
+        for(int k = 0 ; k < 2 ; k++){
+            pPow[k][0] = 1;
+            for(int i = 1 ; i < n ; i++){
+                pPow[k][i] = (pPow[k][i - 1] * P[k]) % MOD[k];
+            }
+        }
+
+        for(int k = 0 ; k < 2 ; k++){
+            for(int i = 0 ; i < str.length() ; i++){
+                tH[k] = ( tH[k] + toInt(str.charAt(i)) * pPow[k][str.length() - 1 - i] ) % MOD[k];
+            }
+        }
+
+        DFS(1, 1, 0);
+        System.out.print(ans);
+    }
+}
