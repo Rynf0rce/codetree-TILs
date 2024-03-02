d5ecbdcef00e706284a4681b878d735014cad318
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-02 19:33:49 +0900

    Title: 가장 많이 등장한 부분 문자열, Time: 291ms, Memory: 26MB, Status: Passed - CodeTree

--- 240302/가장 많이 등장한 부분 문자열/most-common-substring.java
+++ 240302/가장 많이 등장한 부분 문자열/most-common-substring.java
@@ -1,13 +1,36 @@
 import java.util.*;
 import java.io.*;
 
+class Tuple{
+    private long x;
+    private long y;
+
+    public Tuple(long x, long y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Tuple tuple = (Tuple) o;
+        return x == tuple.x && y == tuple.y;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(x, y);
+    }
+}
+
 public class Main {
     public static final int MAX_LENGTH = 100000;
-    public static final int MOD = (int) 1e9 + 9;
-    public static final int P = 37;
-    public static long[] pPow = new long[MAX_LENGTH + 1];
+    public static final int[] MOD = new int[]{(int) 1e9 + 7, (int) 1e9 + 9};
+    public static final int[] P = new int[]{31, 37};
+    public static long[][] pPow = new long[2][MAX_LENGTH + 1];
 
-    public static TreeMap<Long, Integer> ansMap = new TreeMap<>();
+    public static HashMap<Tuple, Integer> ansMap = new HashMap<>();
 
     public static int toInt(char c){
         return c - 'a' + 1;
@@ -20,27 +43,36 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         String str = String.valueOf(st.nextToken());
 
-        pPow[0] = 1;
-        for(int i = 1 ; i <= str.length() ; i++){
-            pPow[i] = (pPow[i - 1] * P) % MOD;
+        for(int k = 0 ; k < 2 ; k++){
+            pPow[k][0] = 1;
+            for(int i = 1 ; i <= str.length() ; i++){
+                pPow[k][i] = (pPow[k][i - 1] * P[k]) % MOD[k];
+            }
         }
 
-        long hashVal = 0;
+        long[] hashVal = new long[]{0, 0};
 
-        for(int i = 0 ; i < n ; i++){
-            hashVal = (hashVal + str.charAt(i) * pPow[n - i - 1]) % MOD;
+        for(int k = 0 ; k < 2 ; k++){
+            for(int i = 0 ; i < n ; i++){
+                hashVal[k] = (hashVal[k] + str.charAt(i) * pPow[k][n - i - 1]) % MOD[k];
+            }
         }
 
-        ansMap.put(hashVal, 1);
+        ansMap.put(new Tuple(hashVal[0], hashVal[1]), 1); 
 
         for(int i = 1 ; i <= str.length() - n ; i++){
-            hashVal = (hashVal * P - str.charAt(i - 1) * pPow[n] + str.charAt(n + i - 1)) % MOD;
+            for(int k = 0 ; k < 2 ; k++){
+                hashVal[k] = (hashVal[k] * P[k] - str.charAt(i - 1) * pPow[k][n] + str.charAt(n + i - 1)) % MOD[k];
 
-            if(hashVal < 0){
-                hashVal += MOD;
+                if(hashVal[k] < 0){
+                    hashVal[k] += MOD[k];
+                }
             }
-            ansMap.put(hashVal, ansMap.getOrDefault(hashVal, 0) + 1);
-            ans = Math.max(ansMap.get(hashVal), ans);
+
+            Tuple curTuple = new Tuple(hashVal[0], hashVal[1]);
+
+            ansMap.put(curTuple, ansMap.getOrDefault(curTuple, 0) + 1);
+            ans = Math.max(ansMap.get(curTuple), ans);
         }
 
         System.out.print(ans);
