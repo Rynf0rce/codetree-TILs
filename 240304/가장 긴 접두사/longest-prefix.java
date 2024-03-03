3bbc9ffde39417088cfffa50a9f6518a59afc958
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-04 08:20:09 +0900

    Title: 가장 긴 접두사, Time: 217ms, Memory: 13MB, Status: Passed - CodeTree

--- 240304/가장 긴 접두사/longest-prefix.java
+++ 240304/가장 긴 접두사/longest-prefix.java
@@ -4,6 +4,73 @@ import java.io.*;
 public class Main {
     public static final int MAX_LENGTH = 100000;
     public static int[] f = new int[MAX_LENGTH + 1];
+    public static String text = " ";
+    public static int textLength = -1;
+
+    public static boolean satisfied(int length){
+        StringBuilder sb = new StringBuilder();
+        sb.append(text.substring(1, length + 1));
+        String patten = sb.reverse().toString();
+        int pattenLength = patten.length();
+        patten = "#" + patten;
+
+        f[0] = -1;
+        for(int i = 1 ; i <= pattenLength ; i++){
+            int j = f[i - 1];
+            while(j >= 0 && patten.charAt(j + 1) != patten.charAt(i)){
+                j = f[j];
+            }
+            f[i] = j + 1;
+        }
+
+        int j = 0;
+        for(int i = 1 ; i <= textLength ; i++){
+            while(j >= 0 && patten.charAt(j + 1) != text.charAt(i)){
+                j = f[j];
+            }
+            j = j + 1;
+            if(j == pattenLength){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        text = String.valueOf(br.readLine());
+        textLength = text.length();
+        
+        int start = 1;
+        int end = text.length();
+        int ans = 0;
+
+        text = "#" + text;
+
+        while(start <= end){
+            int mid = (end + start) / 2;
+            if(satisfied(mid)){
+                start = mid + 1;
+                ans = Math.max(ans, mid);
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        System.out.print(ans);
+    }
+}
+
+/* 틀린 코드이지만 테케가 통과 된 코드 -> 수정완료.
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100000;
+    public static int[] f = new int[MAX_LENGTH + 1];
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = String.valueOf(br.readLine());
@@ -28,3 +95,4 @@ public class Main {
         System.out.println(ans);
     }
 }
+*/
