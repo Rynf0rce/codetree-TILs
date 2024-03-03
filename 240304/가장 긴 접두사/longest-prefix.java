da5b0e3a57d57f44f0a40be47753e712157d0eea
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-04 08:18:53 +0900

    Title: 가장 긴 접두사, Time: 83ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240304/가장 긴 접두사/longest-prefix.java
+++ 240304/가장 긴 접두사/longest-prefix.java
@@ -4,62 +4,27 @@ import java.io.*;
 public class Main {
     public static final int MAX_LENGTH = 100000;
     public static int[] f = new int[MAX_LENGTH + 1];
-    public static String text = " ";
-    public static int textLength = -1;
-
-    public static boolean satisfied(int length){
-        StringBuilder sb = new StringBuilder();
-        sb.append(text.substring(1, length + 1));
-        String patten = sb.reverse().toString();
-        int pattenLength = patten.length();
-        patten = "#" + patten;
-
-        f[0] = -1;
-        for(int i = 1 ; i <= pattenLength ; i++){
-            int j = f[i - 1];
-            while(j >= 0 && patten.charAt(j + 1) != patten.charAt(i)){
-                j = f[j];
-            }
-            f[i] = j + 1;
-        }
-
-        int j = 0;
-        for(int i = 1 ; i <= textLength ; i++){
-            while(j >= 0 && patten.charAt(j + 1) != text.charAt(i)){
-                j = f[j];
-            }
-            j = j + 1;
-            if(j == pattenLength){
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        text = String.valueOf(br.readLine());
-        textLength = text.length();
+        String str = String.valueOf(br.readLine());
+        int strLength = str.length();
+        // str = "#" + str;
         
-        int start = 1;
-        int end = text.length();
+        f[0] = -1;
+        int j = 0;
         int ans = 0;
-
-        text = "#" + text;
-
-        while(start <= end){
-            int mid = (end + start) / 2;
-            if(satisfied(mid)){
-                start = mid + 1;
-                ans = Math.max(ans, mid);
+        for(int i = strLength - 1 ; i >= 0 ; i--){
+            if(str.charAt(j) == str.charAt(i)){
+                j++;
             }
             else{
-                end = mid - 1;
+                ans = Math.max(ans, j);
+                j = 0;
             }
         }
-        System.out.print(ans);
+
+        ans = Math.max(ans, j);
+
+        System.out.println(ans);
     }
 }
