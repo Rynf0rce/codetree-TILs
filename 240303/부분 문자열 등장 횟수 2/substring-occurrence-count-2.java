13089b27af6397bcb3ec701787c54763fea96787
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-03 11:04:34 +0900

    Title: 부분 문자열 등장 횟수 2, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240303/부분 문자열 등장 횟수 2/substring-occurrence-count-2.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100000;
+    public static int[] f = new int[MAX_LENGTH + 1];
+    public static String text = " ";
+    public static int textLength = -1;
+
+    public static int cntFnc(String pattern){
+        int ans = 0;
+        int patternLength = pattern.length();
+        pattern = "#" + pattern;
+        
+        for(int i = 0 ; i <= patternLength ; i++){
+            f[i] = 0;
+        }
+
+        f[0] = -1;
+        for(int i = 1 ; i <= patternLength ; i++){
+            int j = f[i - 1];
+
+            while(j >= 0 && pattern.charAt(i) != pattern.charAt(j + 1)){
+                j = f[j];
+            }
+
+            f[i] = j + 1;
+        }
+        
+        int j = 0;
+        for(int i = 1 ; i <= textLength ; i++){
+            while(j >= 0 && text.charAt(i) != text.charAt(j + 1)){
+                j = f[j];
+            }
+            j++;
+
+            if(j == patternLength){
+                ans++;
+                j = f[j];
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        text = String.valueOf(st.nextToken());
+        textLength = text.length();
+        text = "#" + text;
+        
+        int q = Integer.parseInt(st.nextToken());
+        for(int i = 0 ; i < q ; i++){
+            String pattern = String.valueOf(br.readLine());
+            System.out.println(cntFnc(pattern));
+        }
+    }
+}
