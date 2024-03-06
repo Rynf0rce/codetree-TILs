497eb345a617245a73257f2b03e105129173f932
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-06 17:27:57 +0900

    Title: 문자열을 채워 넣는 가짓수, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240306/문자열을 채워 넣는 가짓수/number-of-ways-to-fill-a-string.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+class trieNode{
+    TreeMap<String, trieNode> children;
+    public trieNode(){
+        this.children = new TreeMap<>();
+    }
+}
+
+public class Main {
+    public static trieNode root = new trieNode();
+    public static ArrayList<String> patternList = new ArrayList<>();
+    public static int ans = 0;
+
+    public static void findAns(String str, trieNode t){
+        for(int i = 0 ; i < patternList.size() ; i++){
+            String pattern = patternList.get(i);
+            if( str.length() >= pattern.length() && str.substring(0, pattern.length()).equals(pattern)){
+                if(!t.children.containsKey(pattern)){
+                    t.children.put(pattern, new trieNode());
+                }
+
+                findAns(str.substring(pattern.length()), t.children.get(pattern));
+            }
+        }
+
+        if(str.length() == 0){
+            ans++;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String T = String.valueOf(st.nextToken());
+
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < m ; i++){
+            String str = String.valueOf(st.nextToken());
+            patternList.add(str);
+        }
+
+        findAns(T, root);
+
+        System.out.print(ans);
+    }
+}
