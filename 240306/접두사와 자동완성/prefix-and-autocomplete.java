a6f50e4ecbd7e5e1fce8bfaa851cdf88b57463f4
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-06 10:10:32 +0900

    Title: 접두사와 자동완성, Time: 94ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240306/접두사와 자동완성/prefix-and-autocomplete.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+class trieNode{
+    int cnt;
+    trieNode[] children = new trieNode[26];
+    public trieNode(){
+        this.cnt = 0;
+        for(int i = 0 ; i < this.children.length ; i++){
+            this.children[i] = null;
+        }
+    }
+}
+
+public class Main {
+    public static final int MAX_LENGTH = 100000;
+    public static String[] strArr = new String[MAX_LENGTH];
+    public static trieNode root = new trieNode();
+
+    public static int findAns(String str){
+        int ans = 1;
+        trieNode t = root;
+        for(int i = 0 ; i < str.length() ; i++){
+            int idx = str.charAt(i) - 'a';
+            
+            t = t.children[idx];
+            if(t.cnt > 1){
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++){
+            strArr[i] = String.valueOf(st.nextToken());
+            trieNode t = root;
+            for(int j = 0 ; j < strArr[i].length() ; j++){
+                int idx = strArr[i].charAt(j) - 'a';
+                if(t.children[idx] == null){
+                    t.children[idx] = new trieNode();
+                    t.cnt++;
+                }
+
+                t = t.children[idx];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < n ; i++){
+            sb.append(findAns(strArr[i]) + " ");
+        }
+
+        bw.write(sb.toString());
+
+        br.close();
+        bw.close();
+    }
+}
