2df85e054079413503d9c2053180b61d0890fcdf
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-09 22:17:19 +0900

    Title: 동전의 가치의 합, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 240309/동전의 가치의 합/sum-of-the-values-of-coins.java
+++ 240309/동전의 가치의 합/sum-of-the-values-of-coins.java
@@ -4,33 +4,18 @@ public class Main {
     public static final int MAX_COST = 100000;
     public static final int MAX_INT = (int)1e9;
     public static int[] DP = new int[MAX_COST + 1];
+    public static int[] coin = new int[]{7, 5, 2, 1};
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         for(int i = 1 ; i <= n ; i++){
-            DP[i] = MAX_INT;
-        }
-
-        int ans = 0;
-        while(n != 0){
-            if(n / 7 > 0){
-                ans += n / 7;
-                n = n % 7;
-            }
-            else if(n / 5 > 0){
-                ans += n / 5;
-                n = n % 5;
-            }
-            else if(n / 2 > 0){
-                ans += n / 2;
-                n = n % 2;
-            }
-            else{
-                ans += n;
-                n = 0;
+            DP[i] = MAX_COST;
+            for(int j = 0 ; j < coin.length ; j++){
+                if(i - coin[j] >= 0){
+                    DP[i] = Math.min(DP[i], DP[i - coin[j]] + 1);
+                }
             }
         }
-        System.out.print(ans);
+        System.out.print(DP[n]);
     }
 }
