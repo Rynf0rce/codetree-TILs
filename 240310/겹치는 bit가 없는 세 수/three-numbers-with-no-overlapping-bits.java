12f5f97d36ddfde9f7314f939e5b3541ddf790d3
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-10 20:57:54 +0900

    Title: 겹치는 bit가 없는 세 수, Time: 251ms, Memory: 9MB, Status: Passed - CodeTree

--- 240310/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.java
+++ 240310/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.java
@@ -15,6 +15,35 @@ public class Main {
         long ans = 0;
         for(int i = 0 ; i < n - 2; i++){
             for(int j = i + 1 ; j < n - 1; j++){
+                for(int k = j + 1 ; k < n ; k++){
+                    if(arr[i] + arr[j] + arr[k] == (arr[i] | arr[j] | arr[k])){
+                        ans = Math.max(ans, arr[i] + arr[j] + arr[k]);
+                    }
+                }
+            }
+        }
+        System.out.print(ans);
+    }
+}
+
+/*
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 500;
+    public static long[] arr = new long[MAX_LENGTH];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+        
+        long ans = 0;
+        for(int i = 0 ; i < n - 2; i++){
+            for(int j = i + 1 ; j < n - 1; j++){
                 if( (arr[i] ^ arr[j]) != arr[i] + arr[j]){
                     continue;
                 }
@@ -28,3 +57,4 @@ public class Main {
         System.out.print(ans);
     }
 }
+*/
