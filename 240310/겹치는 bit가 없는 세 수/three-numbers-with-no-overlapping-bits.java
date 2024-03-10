900907192611a0f7cdef7064af01e1a683273aff
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-10 20:54:33 +0900

    Title: 겹치는 bit가 없는 세 수, Time: 252ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240310/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 500;
+    public static long[] arr = new long[MAX_LENGTH];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+        
+        long ans = 0;
+        for(int i = 0 ; i < n - 2; i++){
+            for(int j = i + 1 ; j < n - 1; j++){
+                if( (arr[i] ^ arr[j]) != arr[i] + arr[j]){
+                    continue;
+                }
+                for(int k = j + 1 ; k < n ; k++){
+                    if( (arr[k] ^ arr[i]) == arr[k] + arr[i] && (arr[k] ^ arr[j]) == arr[k] + arr[j]){
+                        ans = Math.max(ans, arr[i] + arr[j] + arr[k]);
+                    }
+                }
+            }
+        }
+        System.out.print(ans);
+    }
+}
