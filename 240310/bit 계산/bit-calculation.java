baa17f86cedf1fddb0bccfb4176c822246db4191
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-10 13:53:30 +0900

    Title: bit 계산, Time: 466ms, Memory: 20MB, Status: Wrong Answer - CodeTree

---
+++ 240310/bit 계산/bit-calculation.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int ans = 0;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            String str = String.valueOf(st.nextToken());
+            long order = -1;
+            switch(str){
+                case "add" :
+                    order = Integer.parseInt(st.nextToken());
+                    ans = ans | (1 << (order - 1));
+                    break;
+                case "delete" :
+                    order = Integer.parseInt(st.nextToken());
+                    if( (ans & (1 << (order - 1))) == 1){
+                        ans = ans - (1 << order - 1);
+                    }
+                    break;
+                case "print" :
+                    order = Integer.parseInt(st.nextToken());
+                    System.out.println((int)(ans >> (order - 1) & 1));
+                    break;
+                case "toggle" :
+                    order = Integer.parseInt(st.nextToken());
+                    ans = ans ^ (1 << (order - 1));
+                    break;
+                case "clear" :
+                    ans = ans & 0;
+                    break;
+            }
+            
+        }
+    }
+}
