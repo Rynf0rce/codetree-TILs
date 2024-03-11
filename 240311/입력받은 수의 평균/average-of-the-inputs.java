fcdce785a1655ecd4cc4e16564a99dafe907d5d4
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-11 20:09:32 +0900

    Title: 입력받은 수의 평균, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240311/입력받은 수의 평균/average-of-the-inputs.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        float sum = 0;
+        for(int i = 0 ; i < n ; i++){
+            int num = sc.nextInt();
+            sum += num;
+        }
+
+        System.out.printf("%.1f\n", sum / n);
+        
+        if(sum / n < 70){
+            System.out.print("fail");
+        }
+    }
+}
