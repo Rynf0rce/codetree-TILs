72105f0516b4eea0b9c5d61acb70175dee6eb62b
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-11 15:46:23 +0900

    Title: 정수와 실수의 곱셈, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240311/정수와 실수의 곱셈/multiplication-of-integers-and-real-numbers.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 9;
+        double m = 358.273;
+        System.out.printf("%d * %.3f = %.3f", n, m, n * m);
+    }
+}
