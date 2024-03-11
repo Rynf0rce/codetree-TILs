b29d32dfb3f32e795fddc0051e8086a8fbc03fa3
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-11 18:34:15 +0900

    Title: 100을 넘기는 순간, Time: 111ms, Memory: 9MB, Status: Passed - CodeTree

--- 240311/100을 넘기는 순간/as-soon-as-hit-100.java
+++ 240311/100을 넘기는 순간/as-soon-as-hit-100.java
@@ -7,13 +7,13 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         double sum = 0;
+        boolean triger = false;
         for(int i = 1 ; i <= n ; i++){
             int num = Integer.parseInt(st.nextToken());
             sum += num;
-            if(num >= 100){
-                System.out.println((int)sum);
-                System.out.printf("%.1f\n", sum / i);
-            }
         }
+
+        System.out.println((int)sum);
+        System.out.printf("%.1f\n", sum / n);
     }
 }
