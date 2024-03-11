bec072646b6ae31e3a4541bb801d5e51db0a0eed
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-11 09:50:14 +0900

    Title: 겹치지 않는 쌍의 수, Time: 352ms, Memory: 16MB, Status: Passed - CodeTree

--- 240311/겹치지 않는 쌍의 수/number-of-non-overlapping-pairs.java
+++ 240311/겹치지 않는 쌍의 수/number-of-non-overlapping-pairs.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int MAX_INT = 30;
+    public static final int MAX_INT = 5000;
     public static int[] arr = new int[MAX_INT];
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
