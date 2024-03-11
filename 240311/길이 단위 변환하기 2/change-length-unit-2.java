35e57f0bf9335ab31f70539d85e6cb49f260024b
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-11 15:50:27 +0900

    Title: 길이 단위 변환하기 2, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240311/길이 단위 변환하기 2/change-length-unit-2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double yb = 91.44;
+        double mi = 160934;
+        System.out.printf("%.1fyd = %.1fcm\n", 6.8, 6.8 * yb);
+        System.out.printf("%.1fmi = %.1fcm", 0.4, 0.4 * mi);
+    }
+}
