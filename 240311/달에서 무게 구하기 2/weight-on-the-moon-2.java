3dce95d298a93366b02c71ad8684f1c593a9fd57
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-11 12:04:48 +0900

    Title: 달에서 무게 구하기 2, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240311/달에서 무게 구하기 2/weight-on-the-moon-2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 147;
+        double ratio = 0.165;
+        double ans = weight * ratio;
+        System.out.printf("%d * %.5f = %.5f", weight, ratio, ans);
+    }
+}
