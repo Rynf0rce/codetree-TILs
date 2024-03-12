32ddc162632bd584e208f5a00b86f35580384d2f
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-12 17:48:46 +0900

    Title: 특정 규칙에 따른 숫자 출력 2, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240312/특정 규칙에 따른 숫자 출력 2/output-numbers-according-to-specific-rule-2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 9;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                System.out.print(num + " ");
+                num -= 2;
+                if(num < 0){
+                    num = 9;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
