b94b877fdeb1de2687a9e8b5daf5ae7b3cf0777c
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-12 18:09:20 +0900

    Title: 문자 재배치 2, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 240312/문자 재배치 2/relocate-characters-2.java
+++ 240312/문자 재배치 2/relocate-characters-2.java
@@ -1,11 +1,18 @@
 import java.util.*;
 import java.io.*;
 public class Main {
+    public static final int MAX_LENGTH = 50;
+    public static char[] arr = new char[MAX_LENGTH];
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String str = String.valueOf(br.readLine());
-        StringBuilder sb = new StringBuilder(str);
-        System.out.print(sb.reverse().toString());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = st.nextToken().charAt(0);
+        }
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
