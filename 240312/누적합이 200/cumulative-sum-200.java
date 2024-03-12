a34674c470637bdf2c468e1233f176e81cbb13a6
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-12 16:48:06 +0900

    Title: 누적합이 200, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240312/누적합이 200/cumulative-sum-200.java
+++ 240312/누적합이 200/cumulative-sum-200.java
@@ -3,16 +3,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int sum = 0;
-        float avg = 0;
+        double sum = 0;
+        double avg = 0;
         for(int i = 1 ; i <= n ; i++){
             sum += sc.nextInt();
             if(sum > 200){
-                avg = (float)sum / i;
+                avg = sum / i;
                 break;
             }
         }
-        System.out.println(sum);
-        System.out.printf("%.1f", avg);
+        System.out.println((int)sum);
+        System.out.print(avg);
     }
 }
