37e2b22b28c34853bda5ecbacc98f432be1fe201
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-12 10:14:42 +0900

    Title: 사각형 채우기 4, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240312/사각형 채우기 4/rectangle-fill-4.java
+++ 240312/사각형 채우기 4/rectangle-fill-4.java
@@ -3,19 +3,19 @@ import java.util.*;
 public class Main {
     public static final int MAX_LENGTH = 1000;
     public static final int MOD = 10007;
-    public static long[] DP = new long[MAX_LENGTH + 1];
+    public static int[][] DP = new int[MAX_LENGTH + 1][6];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        DP[1] = 2;
-        DP[2] = 3;
-        for(int i = 4; i <= n; i += 2){
-            DP[i] = DP[i - 2] * 3;
-            for(int j = i - 4; j >= 0; j -= 2){
-                DP[i] += DP[j] * 2;
-            }
-            DP[i] = DP[i] % MOD;
+        DP[0][0]=1;
+        for(int i = 1 ; i <= n ; i++){
+            DP[i][0] = DP[i - 1][1] % MOD + DP[i - 1][2] % MOD + DP[i - 1][5] % MOD;
+            DP[i][1] = DP[i - 1][0] % MOD + DP[i - 1][4] % MOD;
+            DP[i][2] = DP[i - 1][0] % MOD + DP[i - 1][3] % MOD;
+            DP[i][3] = DP[i - 1][2] % MOD;
+            DP[i][4] = DP[i - 1][1] % MOD;
+            DP[i][5] = DP[i - 1][0] % MOD;
         }
-        System.out.print(DP[n]);
+        System.out.print(DP[n][0]);
     }
 }
