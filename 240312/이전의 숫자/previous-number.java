c3343d80fbc6bbde2c25bee1ab029528d5c2526f
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-12 18:26:28 +0900

    Title: 이전의 숫자, Time: 170ms, Memory: 10MB, Status: Passed - CodeTree

--- 240312/이전의 숫자/previous-number.java
+++ 240312/이전의 숫자/previous-number.java
@@ -1,23 +1,24 @@
 import java.util.*;
 
 public class Main {
-    public static final int MAX_LENGTH = 100;
-    public static int[] arr = new int[MAX_LENGTH + 1];
+    public static Queue<Integer> q = new LinkedList<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int idx = 0;
         for(int i = 0 ; i < n ; i++){
-            arr[i] = sc.nextInt();
-            if(arr[i] == 0){
-                idx = i;
+            int num = sc.nextInt();
+            if(num == 0){
                 break;
+            }
+            q.add(num);
+            if(q.size() == 6){
+                q.poll();
             }
         }
 
         int sum = 0;
-        for(int i = idx - 1 ; i >= idx - 5 ; i--){
-            sum += arr[i];
+        while(!q.isEmpty()){
+            sum += q.poll();
         }
         System.out.print(sum);
     }
