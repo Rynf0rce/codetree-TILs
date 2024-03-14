008ac4fca4944c1fcb7f1d819b03b64519e36500
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-14 19:39:11 +0900

    Title: 지그재그로 이동하기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240314/지그재그로 이동하기/moving-with-zigzag.java
+++ 240314/지그재그로 이동하기/moving-with-zigzag.java
@@ -1,6 +1,13 @@
 import java.util.*;
 
 public class Main {
+    public static boolean inRange(int start, int end, int point){
+        if(start > end){
+            return inRange(end, start, point);
+        }
+        return start <= point && point <= end;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
@@ -8,7 +15,8 @@ public class Main {
         int pos = A;
         int dist = 1;
         int ans = 0;
-        while(A + dist <= B){
+
+        while(!inRange(pos, A + dist, B)){
             int goal = A + dist;
             ans += Math.abs(pos - goal);
             pos = goal;
