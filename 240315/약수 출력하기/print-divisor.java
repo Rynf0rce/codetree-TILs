e0bc121377c371fdee1b433600fb80d8a89afb18
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-15 16:25:20 +0900

    Title: 약수 출력하기, Time: 189ms, Memory: 10MB, Status: Passed - CodeTree

--- 240315/약수 출력하기/print-divisor.java
+++ 240315/약수 출력하기/print-divisor.java
@@ -1,12 +1,28 @@
 import java.util.*;
 public class Main {
+    public static ArrayList<Long> ansList = new ArrayList<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 1 ; i <= n ; i++){
+        for(long i = 1 ; i <= Math.sqrt(n) ; i++){
             if(n % i == 0){
-                System.out.print(i + " ");
+                ansList.add(i);
             }
-        }   
+        }
+        
+        int size = ansList.size();
+        for(int i = 0 ; i < size ; i++){
+            long num = ansList.get(i);
+            long val = n / num;
+            if(val != num){
+                ansList.add(val);
+            }
+        }
+
+        Collections.sort(ansList);
+        for(long num : ansList){
+            System.out.print(num + " ");
+        }
+        
     }
 }
