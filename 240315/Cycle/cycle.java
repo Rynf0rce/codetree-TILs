b9eb68310fc222cb5bcc71b2dbfe15c74227cfec
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-15 19:13:21 +0900

    Title: Cycle, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

--- 240315/Cycle/cycle.java
+++ 240315/Cycle/cycle.java
@@ -1,18 +1,22 @@
 import java.util.*;
 public class Main {
+    public static HashSet<Integer> numSet = new HashSet<>();
     public static HashSet<Integer> ansSet = new HashSet<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int P = sc.nextInt();
-        int cnt = 0;
         int num = N;
         while(!ansSet.contains((num * N) % P)){
             num = (num * N) % P;
-            ansSet.add(num);
-            cnt++;
+            if(numSet.contains(num)){
+                ansSet.add(num);
+            }
+            else{
+                numSet.add(num);
+            }
         }
-        System.out.print(cnt);
+        System.out.print(ansSet.size());
 
     }
 }
