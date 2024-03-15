736b210f92069ac8e18b261f6c8bce876dee9743
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-15 16:09:04 +0900

    Title: 문자 사각형 출력하기 2, Time: 229ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240315/문자 사각형 출력하기 2/print-char-rectangle-2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100;
+    public static char[][] table = new char[MAX_LENGTH][MAX_LENGTH];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char c= 'A';
+        for(int j = 0 ; j < n ; j++){
+            if(j % 2 == 0){
+                for(int i = 0 ; i < n ; i++){
+                    table[i][j] = c++;
+                    if(c > 'Z'){
+                        c = 'A';
+                    }
+                }
+            }
+            else{
+                for(int i = n - 1 ; i >= 0 ; i--){
+                    table[i][j] = c++;
+                    if(c > 'Z'){
+                        c = 'A';
+                    }
+                }
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                System.out.print(table[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
