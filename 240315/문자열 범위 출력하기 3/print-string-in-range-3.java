60c94af854810f5abeaa5e1156f573c6701d43cf
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-15 11:15:03 +0900

    Title: 문자열 범위 출력하기 3, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240315/문자열 범위 출력하기 3/print-string-in-range-3.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = String.valueOf(sc.nextLine());
+        System.out.print(str.substring(4, 17));
+    }
+}
