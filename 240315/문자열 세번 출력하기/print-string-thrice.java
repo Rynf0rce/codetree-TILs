653119d83fe604a0b7c611730cf530deb7142563
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-15 11:12:33 +0900

    Title: 문자열 세번 출력하기, Time: 231ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240315/문자열 세번 출력하기/print-string-thrice.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = String.valueOf(sc.next());
+        for(int i = 0 ; i < 3 ; i++){
+            System.out.println(str);
+        }
+    }
+}
