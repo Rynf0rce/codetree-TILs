898bc6d715b1014142855f65277e00d2b5996d6b
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-15 12:52:40 +0900

    Title: 아스키코드의 곱과 몫, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240315/아스키코드의 곱과 몫/product-and-quotient-of-the-ascii-code.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = (int)sc.next().charAt(0);
+        int b = (int)sc.next().charAt(0);
+        System.out.print(a * b + " ");
+        if(b > a){
+            System.out.print(b % a);
+        }
+        else{
+            System.out.print(a % b);
+        }
+    }
+}
