ddff269f90a3e7d8045cfa0936ca304f05ca897e
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-15 19:29:44 +0900

    Title: 문자 제거하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240315/문자 제거하기 2/del-char-2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.valueOf(st.nextToken()));
+
+        int n = Integer.parseInt(st.nextToken());
+        for(int i = 0 ; i < n ; i++){
+            int num = Integer.parseInt(br.readLine()) - 1;
+            if(sb.length() < num){
+                continue;
+            }
+
+            sb.deleteCharAt(num);
+            System.out.println(sb.toString());
+        }
+    }
+}
