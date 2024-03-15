d17545aa2dd072b085d67980a63fc84c745c6dbc
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-15 19:22:51 +0900

    Title: 최소 최대, Time: 450ms, Memory: 46MB, Status: Passed - CodeTree

--- 240315/최소 최대/min-max.java
+++ 240315/최소 최대/min-max.java
@@ -1,14 +1,16 @@
 import java.util.*;
+import java.io.*;
 public class Main {
     public static final int MAX_INT = 1000000;
     public static final int MIN_INT = -1000000;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int maxVal = MIN_INT;
         int minVal = MAX_INT;
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0 ; i < n ; i++){
-            int num = sc.nextInt();
+            int num = Integer.parseInt(st.nextToken());
             maxVal = Math.max(maxVal, num);
             minVal = Math.min(minVal, num);
         }
