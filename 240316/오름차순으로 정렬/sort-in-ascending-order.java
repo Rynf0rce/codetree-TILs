ed0d6354673b9343fdfcad6f5fababf610c0b844
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-16 15:31:53 +0900

    Title: 오름차순으로 정렬, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240316/오름차순으로 정렬/sort-in-ascending-order.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static final int MAX_LENGTH = 100;
+    public static int[] arr = new int[MAX_LENGTH];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, 0, n);
+        for(int i = 0 ; i < n ; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
