1edd08084f25bf50635f2216447e311c2e0010f8
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-16 21:11:03 +0900

    Title: 진법 변환 6, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240316/진법 변환 6/base-conversion-6.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static Stack<Character> s = new Stack<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        char[] arr = String.valueOf(st.nextToken()).toCharArray();
+        int m = Integer.parseInt(st.nextToken());
+
+        long num = 0;
+        for(int i = arr.length -1 ; i >= 0 ; i--){
+            int val = 0;
+            if(arr[i] >= 'a'){
+                val = (arr[i] -'a') + 10;
+            }
+            else{
+                val = arr[i] - '0';
+            }
+
+            num += val * (int)Math.pow(n, i);
+        }
+        
+        while(num > 0){
+            long c = num % m;
+            if(c >= 10){
+                s.add((char)('a' + c - 10 ));
+            }
+            else{
+                s.add(Long.toString(c).charAt(0));
+            }
+            num /= m;
+        }
+
+        while(!s.isEmpty()){
+            System.out.print(s.pop());
+        }
+    }
+}
