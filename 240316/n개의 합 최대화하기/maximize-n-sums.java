93861c43e689a0fd62597b5fecfdd34d115d8f73
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-16 10:00:45 +0900

    Title: n개의 합 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240316/n개의 합 최대화하기/maximize-n-sums.java
+++ 240316/n개의 합 최대화하기/maximize-n-sums.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
     public static final int MAX_LENGTH = 16;
     public static int[][] table = new int[MAX_LENGTH][MAX_LENGTH];
-    public static int[][] DP = new int[(1 << MAX_LENGTH)][MAX_LENGTH];
+    public static int[][] DP = new int[MAX_LENGTH][(1 << MAX_LENGTH)];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -16,36 +16,30 @@ public class Main {
             }
         }
 
-        for(int i = 0 ; i < (1 << n) ; i++){
-            for(int j = 0 ; j < n ; j++){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < (1 << n) ; j++){
                 DP[i][j] = -1;
             }
         }
 
         DP[0][0] = 0;
 
-        for(int i = 0 ; i < (1 << n) ; i++){
-            for(int j = 0 ; j < n ; j++){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < (1 << n) ; j++){
                 if(DP[i][j] == -1){
                     continue;
                 }
 
                 for(int k = 0 ; k < n ; k++){
-                    if(((i >> k) & 1) == 1){
+                    if(((j >> k) & 1) == 1){
                         continue;
                     }
 
-                    DP[i + (1 << k)][k] = Math.max(DP[i + (1 << k)][k], DP[i][j] + table[j][k]);
+                    DP[i + 1][j + (1 << k)] = Math.max(DP[i + 1][j + (1 << k)], DP[i][j] + table[i][k]);
                 }
             }
         }
 
-        int ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            ans = Math.max(ans, DP[(1 << n) - 1][i]);
-        }
-        System.out.print(ans);
-
-
+        System.out.print(DP[n][(1 << n) - 1]);
     }
 }
