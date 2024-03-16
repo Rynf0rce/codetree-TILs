6283a7d4f321f722d204d32c1f5c490f1a0bef9c
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-16 10:50:59 +0900

    Title: 진법 변환 3, Time: 1061ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240316/진법 변환 3/base-conversion-3.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static Stack<Integer> s = new Stack<>();
+    public static void printBinary(int num, boolean isHead){
+        for(int i = 0 ; i < 3 ; i++){
+            if(((num >> i) & 1) == 1){
+                s.add(1);
+            }
+            else{
+                s.add(0);
+            }
+        }
+
+        if(isHead){
+            while(!s.isEmpty() && !(s.peek() == 1)){
+                s.pop();
+            }
+        }
+
+        while(!s.isEmpty()){
+            System.out.print(s.pop());
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] arr = sc.next().toCharArray();
+        printBinary(arr[0] - '0', true);
+
+        for(int i = 1 ; i < arr.length ; i++){
+            printBinary(arr[i] - '0', false);
+        }
+    }
+}
