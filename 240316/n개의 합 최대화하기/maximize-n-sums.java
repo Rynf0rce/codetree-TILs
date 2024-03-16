a58b8751ad76918aaf6df3699f534d1e716ae5b8
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-16 09:55:21 +0900

    Title: n개의 합 최대화하기, Time: 93ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240316/n개의 합 최대화하기/maximize-n-sums.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 16;
+    public static int[][] table = new int[MAX_LENGTH][MAX_LENGTH];
+    public static int[][] DP = new int[(1 << MAX_LENGTH)][MAX_LENGTH];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0 ; j < n ; j++){
+                table[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0 ; i < (1 << n) ; i++){
+            for(int j = 0 ; j < n ; j++){
+                DP[i][j] = -1;
+            }
+        }
+
+        DP[0][0] = 0;
+
+        for(int i = 0 ; i < (1 << n) ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(DP[i][j] == -1){
+                    continue;
+                }
+
+                for(int k = 0 ; k < n ; k++){
+                    if(((i >> k) & 1) == 1){
+                        continue;
+                    }
+
+                    DP[i + (1 << k)][k] = Math.max(DP[i + (1 << k)][k], DP[i][j] + table[j][k]);
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            ans = Math.max(ans, DP[(1 << n) - 1][i]);
+        }
+        System.out.print(ans);
+
+
+    }
+}
