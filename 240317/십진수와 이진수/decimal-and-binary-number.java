5dba76e05217bd5cd2e278ba46c0016c802808fa
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-17 09:58:38 +0900

    Title: 십진수와 이진수, Time: 156ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240317/십진수와 이진수/decimal-and-binary-number.java
+++ 240317/십진수와 이진수/decimal-and-binary-number.java
@@ -1,24 +1,41 @@
 import java.util.*;
 
 public class Main {
+    public static boolean[] ans = new boolean[1005];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char[] arr = sc.next().toCharArray();
-        long num = 0;
+
+        Stack<Character> s1 = new Stack<>();
+        Stack<Character> s2 = new Stack<>();
+
         for(int i = 0 ; i < arr.length ; i++){
-            num += (arr[i] - '0') * Math.pow(2, arr.length - 1 - i);
+            s1.add(arr[i]);
+            s2.add(arr[i]);
         }
 
-        num *= 17;
-
-        Stack<Long> s = new Stack<>();
-        while(num > 0){
-            s.add(num % 2);
-            num /= 2;
+        for(int i = 0 ; i < 4 ; i++){
+            s1.add('0');
         }
 
-        while(!s.isEmpty()){
-            System.out.print(s.pop());
+        Stack<Integer> sumS = new Stack<>();
+        int upBit = 0;
+        while(!s2.isEmpty()){
+            char a = s1.pop();
+            char b = s2.pop();
+            
+            int val = (a - '0') + (b - '0') + upBit;
+            sumS.add(val % 2);
+            upBit = val / 2;
+        }
+
+        while(!s1.isEmpty()){
+            sumS.add( (s1.peek() - '0' + upBit) % 2);
+            upBit = (s1.pop() - '0' + upBit) / 2;
+        }
+
+        while(!sumS.isEmpty()){
+            System.out.print(sumS.pop());
         }
     }
 }
