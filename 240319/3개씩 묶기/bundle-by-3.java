46ff9fa591a4292cf2ef134657aa85e0391b4756
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-19 16:56:19 +0900

    Title: 3개씩 묶기, Time: 276ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240319/3개씩 묶기/bundle-by-3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100000;
+    public static int[] arr = new int[MAX_LENGTH];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, 0 , n);
+        
+        int cnt = 0;
+        int sum = 0;
+        for(int i = n - 1 ; i >= 0 ; i--){
+            cnt++;
+            if(cnt == 3){
+                cnt = 0;
+                continue;
+            }
+            sum += arr[i];
+        }
+        System.out.print(sum);
+
+    }
+}
