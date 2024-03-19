21c60ec0b2ee8f5638ea29962e1112271d75e2b1
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-19 16:04:04 +0900

    Title: 최대공약수와 최소공배수, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240319/최대공약수와 최소공배수/greatest-common-divisor-and-least-common-multiple.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static int findGCD(int a, int b){
+        if(a > b){
+            return findGCD(b, a);
+        }
+
+        int ans = 1;
+        for(int i = a ; i >= 1 ; i--){
+            if(b % i == 0){
+                ans = i;
+                break;
+            }
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int gcd = findGCD(a, b);
+        System.out.print(gcd + " " + gcd * (a / gcd) * (b / gcd));
+        
+    }
+}
