63f9706424cd0286d8927a1e221b8b4ec1764fcf
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-20 06:40:46 +0900

    Title: 연속된 숫자 만들기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240320/연속된 숫자 만들기/create-consecutive-numbers.java
+++ 240320/연속된 숫자 만들기/create-consecutive-numbers.java
@@ -8,7 +8,7 @@ public class Main {
             arr[i] = sc.nextInt();
         }
         Arrays.sort(arr);
-        if(arr[1] - arr[0] == 1 && arr[2] - arr[1] == 0){
+        if(arr[1] - arr[0] == 1 && arr[2] - arr[1] == 1){
             System.out.println(0);
         }
         else if(arr[1] - arr[0] == 2 || arr[2] - arr[1] == 2){
