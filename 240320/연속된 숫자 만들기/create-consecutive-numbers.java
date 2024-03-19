0451b2d811382337f88c492727f41b96596f1221
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-20 06:40:18 +0900

    Title: 연속된 숫자 만들기, Time: 168ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240320/연속된 숫자 만들기/create-consecutive-numbers.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static int[] arr = new int[3];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0 ; i < 3 ; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        if(arr[1] - arr[0] == 1 && arr[2] - arr[1] == 0){
+            System.out.println(0);
+        }
+        else if(arr[1] - arr[0] == 2 || arr[2] - arr[1] == 2){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(2);
+        }
+
+        int maxCnt = Math.max(arr[1] - arr[0] - 1, arr[2] - arr[1] - 1);
+        System.out.print(maxCnt);
+    }
+}
