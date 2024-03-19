fb9f802efff25c4ab8b91443b3d2748252b0e8a9
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-19 17:45:16 +0900

    Title: 제곱수의 합으로 나타내기, Time: 3063ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 240319/제곱수의 합으로 나타내기/express-as-sum-of-squares.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static int ans = 4;
+    public static void findAns(int num, int cnt){
+        if(cnt > 4){
+            return;
+        }
+
+        for(int i = (int)Math.sqrt(num) ; i >= 1 ; i--){
+            if(num - i * i == 0){
+                ans = Math.min(ans, cnt);
+                return;
+            }
+
+            if(num - i * i > 0){
+                findAns(num - i * i, cnt + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = (int)Math.sqrt(n) - 1  ; i >= 1 ; i--){
+            findAns(n, 1);
+        }
+        System.out.print(ans);
+    }
+}
