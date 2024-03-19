bbd2500c71a45b72a00a71e725ef971c12b943db
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-19 16:16:41 +0900

    Title: 최소공배수 2, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240319/최소공배수 2/least-common-multiple-2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static int findGCD(int a, int b){
+        if(a > b){
+            return findGCD(b, a);
+        }
+
+        int ans = 1;
+        for(int i = a ; i >= 1 ; i--){
+            if(a % i == 0 && b % i == 0){
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int GCD = findGCD(a, b);
+        System.out.print(GCD * (a / GCD) * (b / GCD));
+    }
+}
