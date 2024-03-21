1778152075530e7fb77435dfcdae1368c8ecc361
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-21 20:29:18 +0900

    Title: 체크판위에서 2, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240321/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 15;
+    public static boolean[][] table = new boolean[MAX_LENGTH][MAX_LENGTH];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0 ; j < m ; j++){
+                if(String.valueOf(st.nextToken()).charAt(0) == 'B'){
+                    table[i][j] = true;
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(table[0][0] == table[i][j]){
+                    continue;
+                }
+
+                for(int k = i + 1 ; k < n ; k++){
+                    for(int h = j + 1 ; h < n ; h++){
+                        if(table[i][j] == table[k][h] || k == n - 1 || h == m - 1){
+                            continue;
+                        }
+                        ans++;
+                    }
+                }
+            }
+        }
+        System.out.print(ans);
+    }
+}
