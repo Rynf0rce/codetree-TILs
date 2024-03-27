f57cadd848881760bab4dcc812b6768b5caefbf5
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-03-28 08:33:49 +0900

    Title: Carry 피하기 2, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

--- 240328/Carry 피하기 2/escaping-carry-2.java
+++ 240328/Carry 피하기 2/escaping-carry-2.java
@@ -25,7 +25,7 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int ans = 0;
+        int ans = -1;
         for(int i = 0 ; i < n - 2 ; i++){
             for(int j = i + 1; j < n - 1 ; j++){
                 for(int k = j + 1 ; k < n ; k++){
