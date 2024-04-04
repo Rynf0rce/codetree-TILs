0ed8ae01bdb80daba3c8cf2c7f67d02d9f46a1d6
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-04-05 07:11:03 +0900

    Title: G or H 2, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240405/G or H 2/G-or-H-2.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100;
+    public static int[] arr = new int[MAX_LENGTH + 1];
+    public static int[] cnt = new int[3];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i < N ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int loc = Integer.parseInt(st.nextToken());
+            char c = String.valueOf(st.nextToken()).charAt(0);
+            if(c == 'G'){
+                arr[loc] = 1;
+            }
+            else{
+                arr[loc] = 2;
+            }
+        }
+
+        
+        int ans = 0;
+        for(int i = 0 ; i < MAX_LENGTH ; i++){
+            if(arr[i] == 0){
+                continue;
+            }
+
+            for(int j = 0 ; j < 3 ; j++){
+                cnt[j] = 0;
+            }
+
+            for(int j = i ; j <= MAX_LENGTH ; j++){
+                cnt[arr[j]]++;
+                if(arr[j] > 0 && cnt[1] == cnt[2]){
+                    ans = Math.max(j - i, ans);
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
