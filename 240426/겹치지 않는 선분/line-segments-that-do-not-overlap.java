2139c821c9209c5956666589e6c019f8ce0281ae
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-04-26 13:24:25 +0900

    Title: 겹치지 않는 선분, Time: 624ms, Memory: 25MB, Status: Passed - CodeTree

--- 240426/겹치지 않는 선분/line-segments-that-do-not-overlap.java
+++ 240426/겹치지 않는 선분/line-segments-that-do-not-overlap.java
@@ -1,49 +1,59 @@
 import java.util.*;
 import java.io.*;
 
+class line implements Comparable<line>{
+    int x1, x2;
+    public line(int x1, int x2){
+        this.x1 = x1;
+        this.x2 = x2;
+    }
+
+    @Override
+    public int compareTo(line l){
+        return this.x1 - l.x1;
+    }
+}
+
 public class Main {
-    public static HashMap<Integer, Integer> lineMap = new HashMap<>();
-    public static HashSet<Integer> removeMap = new HashSet<>();
-    public static PriorityQueue<Integer> leftPQ = new PriorityQueue<>(Collections.reverseOrder());
-    public static PriorityQueue<Integer> rightPQ = new PriorityQueue<>(Collections.reverseOrder());
-    public static void main(String[] args) throws IOException {
+    public static final int MAX_LENGTH = 100000;
+    public static final int MIN_POINT = -10000000;
+    public static final int MAX_POINT = 10000000;
+    public static final int INVALID = (int)1e9;
+    public static line[] arr = new line[MAX_LENGTH + 2];
+    public static int[] left = new int[MAX_LENGTH + 2];
+    public static int[] right = new int[MAX_LENGTH + 2];
+    public static boolean[] removed = new boolean[MAX_LENGTH];
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        for(int i = 0 ; i < N ; i++){
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i = 1 ; i <= n ; i++){
             st = new StringTokenizer(br.readLine(), " ");
             int x1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
-            leftPQ.add(x1);
-            rightPQ.add(x2);
-            lineMap.put(x2, x1);
+            arr[i] = new line(x1, x2);
+        }
+
+        Arrays.sort(arr, 1, n + 1);
+
+        left[0] = MIN_POINT;
+        for(int i = 1 ; i <= n ; i++){
+            left[i] = Math.max(left[i - 1], arr[i].x2);
+        }
+
+        right[n + 1] = MAX_POINT;
+        for(int i = n ; i >= 1 ; i--){
+            right[i] = Math.min(right[i + 1], arr[i].x2);
         }
 
         int ans = 0;
-        while(!rightPQ.isEmpty()){
-            int curRight = rightPQ.poll();
-            int target = lineMap.get(curRight);
-
-            if(removeMap.contains(target)){
-                continue;
-            }
-
-            boolean flag = true;
-            while(!leftPQ.isEmpty()){
-                int curLeft = leftPQ.poll();
-                if(target == curLeft){
-                    if(flag){
-                        ans++;
-                    }
-                    break;
-                }
-
-                removeMap.add(curLeft);
-                flag = false;
+        for(int i = 1 ; i <= n ; i++){
+            if(left[i] <= arr[i].x2 && arr[i].x2 <= right[i]){
+                ans++;
             }
         }
-
-        System.out.print(ans);
-        
+        System.out.println(ans);
     }
 }
