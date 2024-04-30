f923b336357b339261d7c82618855c9088160739
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-04-30 15:03:25 +0900

    Title: 소수, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240430/소수/prime-number.java
+++ 240430/소수/prime-number.java
@@ -3,28 +3,32 @@ import java.util.*;
 public class Main {
     public static final int MAX_LENGTH = 10000;
     public static boolean[] primeArr = new boolean[MAX_LENGTH + 1];
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>();
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         for(int i = 2 ; i <= b ; i++){
+            if(primeArr[i]){
+                continue;
+            }
+            pq.add(i);
             for(int j = 2 * i ; j <= b ; j += i){
                 primeArr[j] = true;
             }
         }
 
-        int ans = 0;
-        int minVal = 0;
-        for(int i = Math.max(2, a) ; i <= b ; i++){
-            if(!primeArr[i]){
-                ans += i;
-                if(minVal == 0){
-                    minVal = i;
-                }
-            }
+        if(pq.isEmpty()){
+            System.out.println(-1);
         }
-
-        System.out.println((ans == 0) ? -1 : ans);
-        System.out.println((ans == 0) ? "" : minVal);
+        else{
+            int minVal = pq.peek();
+            int ans = 0;
+            while(!pq.isEmpty()){
+                ans += pq.poll();
+            }
+            System.out.println(ans);
+            System.out.print(minVal);
+        }
     }
 }
