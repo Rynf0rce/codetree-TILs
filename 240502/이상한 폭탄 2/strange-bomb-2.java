a9bdfb343d30a5ef455d1dff79fdf11837d00854
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-05-02 08:31:18 +0900

    Title: 이상한 폭탄 2, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240502/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int MAX_NUM = 1000;
+    public static int[] dist = new int[MAX_NUM + 1];
+    public static boolean[] bombed = new boolean[MAX_NUM + 1];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int ans = 0;
+        for(int i = 0 ; i < N ; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(dist[num] == 0){
+                dist[num] = i;
+            }
+            else{
+                if(dist[num] - i <= K && !bombed[num]){
+                    bombed[num] = true;
+                    ans = Math.max(ans, num);
+                }
+                else{
+                    dist[num] = i;
+                }
+            }
+        }
+        System.out.print(ans);
+    }
+}
