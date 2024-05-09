bdace3a670be3117649f6211fc2b20e7f408aa91
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-05-09 09:01:06 +0900

    Title: 문자열 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240509/문자열 뒤집기/flip-text.java
+++ 240509/문자열 뒤집기/flip-text.java
@@ -9,7 +9,7 @@ public class Main {
         int siteOne = 0;
 
         
-        for(int i = 0 ; i < arr.length - 1 ; i++){
+        for(int i = 0 ; i < arr.length ; i++){
             if(arr[i] == '1' && arr[i] != arr[i + 1]){
                 siteOne++;
             }
