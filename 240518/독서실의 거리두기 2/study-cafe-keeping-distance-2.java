9c23959c0739ee131c9962c8cf54f95d4649d597
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-05-18 20:21:00 +0900

    Title: 독서실의 거리두기 2, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240518/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(), idx = 0, cnt = 0 ;
+        char[] arr = sc.next().toCharArray();
+        int[] rangeArr = new int[N];
+
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] == '1'){
+                rangeArr[idx++] = cnt;
+                cnt = 0;
+            }
+            if(i == arr.length - 1){
+                rangeArr[idx++] = cnt;
+            }
+            cnt++;
+        }
+
+        int maxVal = Integer.MIN_VALUE;
+        for(int i = 0 ; i < idx ; i++){
+            int minVal = Integer.MAX_VALUE;
+            if(i == 0 || i == idx - 1){
+                minVal = Math.min(minVal, rangeArr[i]);
+            }
+            for(int j = 1 ; j < idx -1 ; j++){
+                int currentVal = rangeArr[j];
+                if(j == i){
+                    currentVal /= 2;
+                }
+                minVal = Math.min(minVal, currentVal);
+            }
+            maxVal = Math.max(maxVal, minVal);
+        }
+        System.out.println(maxVal);
+    }
+}
