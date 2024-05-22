ea60464dab78d5643f0efd5671eda1221f18131b
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-05-22 21:49:33 +0900

    Title: 최대 숫자 만들기, Time: 722ms, Memory: 40MB, Status: Passed - CodeTree

--- 240522/최대 숫자 만들기/make-biggest-num.java
+++ 240522/최대 숫자 만들기/make-biggest-num.java
@@ -1,43 +1,50 @@
 import java.util.*;
 import java.io.*;
 
-class Type implements Comparable<Type>{
-    int num;
-    public Type(int num){
-        this.num = num;
-    }
-    @Override
-    public int compareTo(Type t){
-        Long left = Long.parseLong(Integer.toString(this.num) + Integer.toString(t.num));
-        Long right = Long.parseLong(Integer.toString(t.num) + Integer.toString(this.num));
-        if(left > right){
-            return -1;
-        }
-        else if(left == right){
-            return 0;
-        }
-        else{
-            return 1;
-        }
-    }
-}
-
 public class Main {
-    public static PriorityQueue<Type> pq = new PriorityQueue<>();
+    public static ArrayList<String> inputList = new ArrayList<>();
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        for(int i = 0 ; i < n ; i++){
-            pq.add(new Type(Integer.parseInt(br.readLine())));
-        }
         
-        StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()){
-            sb.append(pq.poll().num);
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i < n ; i++){
+            inputList.add(br.readLine());
         }
-        bw.write(sb.toString());
-        bw.close();
+
+
+        Collections.sort(inputList, new Comparator<String>(){
+            @Override
+            public int compare(String a, String b){
+                long n1 = Long.parseLong(a.concat(b));
+                long n2 = Long.parseLong(b.concat(a));
+
+                if(n1 > n2){
+                    return 1;
+                }
+                else{
+                    return -1;
+                }
+                
+            }
+        });
+
+
+        String ans = "";
+        for(int i = n - 1 ; i >= 0 ; i--){
+            String str = inputList.get(i);
+            //System.out.println(str);
+            bw.write(str);
+            // ans.concat();
+        }
+
+        
+
         br.close();
+        bw.close();
+
+
     }
 }
