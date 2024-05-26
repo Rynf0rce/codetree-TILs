1c24814a54a48b2bd6bcf9e53e731bc2c408d285
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-05-26 21:01:35 +0900

    Title: 폭탄 해체 작업, Time: 262ms, Memory: 14MB, Status: Passed - CodeTree

--- 240526/폭탄 해체 작업/the-bomb-dismantling.java
+++ 240526/폭탄 해체 작업/the-bomb-dismantling.java
@@ -1,24 +1,62 @@
 import java.util.*;
 import java.io.*;
+
+class bomb implements Comparable<bomb>{
+    int point;
+    int time;
+
+    public bomb(int point, int time){
+        this.point = point;
+        this.time = time;
+    }
+
+    @Override
+    public int compareTo(bomb b){
+        if(this.time == b.time){
+            return b.point - this.point;
+        }
+        else{
+            return b.time - this.time;
+        }
+    }
+}
+
 public class Main {
-    public static int MAX_LENGTH = 10000;
-    public static int[] arr = new int[MAX_LENGTH + 1];
+    public static PriorityQueue<bomb> bombQueue = new PriorityQueue<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+        
         int n = Integer.parseInt(br.readLine());
+        int maxTime = 0;
         for(int i = 0 ; i < n ; i++){
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine() , " ");
             int point = Integer.parseInt(st.nextToken());
             int time = Integer.parseInt(st.nextToken());
 
-            arr[time] = Math.max(arr[time], point);
+            bombQueue.add(new bomb(point, time));
+        }
+        
+        int time = bombQueue.peek().time;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int ans = 0;
+
+        for(int i = time ; i >= 1 ; i--){
+            while(!bombQueue.isEmpty() && bombQueue.peek().time == i){
+                pq.add(bombQueue.poll().point);
+                // System.out.println("time : " + i);
+            }
+            
+            if(!pq.isEmpty()){
+                ans += pq.poll();
+            }
         }
 
-        int ans = 0;
-        for(int i = 1 ; i <= MAX_LENGTH ; i++){
-            ans += arr[i];
-        }
-        System.out.print(ans);
+        bw.write(ans + "");
+
+        br.close();
+        bw.close();
+
     }
 }
