70adf140440a3d7e8a6750f42a212cfd9e4d0a7d
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-07-10 03:43:04 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 235ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240710/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Main {
+    public static int currentX;
+    public static int currentY;
+    public static int cnt = 1;
+    public static int[][] arr2D;
+    public static int[] arrRow = new int[]{-1, 0, 1, 0}; // 북동남서 (남 동 북 서 순.)
+    public static int[] arrColumn = new int[]{0, 1, 0, -1}; 
+
+    public static boolean inRange(int row, int column){
+        if(row >= 0 && column >= 0 && row < arr2D.length && column < arr2D[0].length){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean chackCorner(int row, int column){
+        // 움직인 위치를 기준위로 3개가 비어있는지 체크, 범위를 초과하는 경우 있는것으로 계산한다.
+        int cnt = 0;
+        for(int i = 0 ; i < arrRow.length ; i++){
+            int tempRow = row + arrRow[i];
+            int tempColumn = column + arrColumn[i];
+            if( !inRange(tempRow,tempColumn) || (inRange(tempRow, tempColumn) && arr2D[tempRow][tempColumn] == 0 )){
+                cnt++;
+            }
+        }
+        if(cnt == 3){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void conFnc(int startRow, int startColumn, int direction){           
+        arr2D[startRow][startColumn] = 1;
+        cnt++;
+
+        while(cnt <= arr2D.length * arr2D[0].length){
+            int tempRow = startRow + arrRow[direction];
+            int tempColumn = startColumn + arrColumn[direction];
+            if(inRange(tempRow, tempColumn)){
+                arr2D[tempRow][tempColumn] = cnt;
+                startRow = tempRow;
+                startColumn = tempColumn;
+                cnt++;
+            }
+
+            if(chackCorner(tempRow, tempColumn)){
+                direction--;
+                if(direction < 0){
+                    direction = 3;
+                }
+            }
+        }
+
+        for(int i = 0; i < arr2D.length ; i++){
+            for(int j = 0 ; j < arr2D[i].length ; j++){
+                System.out.print(arr2D[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int edge = sc.nextInt();
+        // edge = 7;
+        arr2D = new int[edge][edge];
+        conFnc((edge/2), (edge / 2), 1);
+    }
+}
