dcd77e22788d74f5d23135f087d866a0916b94c5
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-08-20 22:44:58 +0900

    Title: 트리 경유, Time: 2167ms, Memory: 46MB, Status: Passed - CodeTree

---
+++ 240820/트리 경유/a-way-through-the-tree.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_NODE = (1<<20);
+    public static boolean[] visited = new boolean[MAX_NODE];
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        for(int i = 0 ; i < q ; i++){
+            int num = Integer.parseInt(br.readLine());
+            int point = num;
+            int visitedPoint = -1;
+            boolean canGo = true;
+            while(point > 0){
+                if(visited[point]){
+                    canGo = false;
+                    visitedPoint = point;
+                }
+                point /= 2;
+            }
+
+            if(canGo){
+                System.out.println(0);
+                visited[num] = true;
+            }
+            else{
+                System.out.println(visitedPoint);
+            }
+        }
+    }
+}
