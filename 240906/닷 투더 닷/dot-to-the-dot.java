5b5c398ea8c872148addb40de5cb99a15c94ea30
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-09-06 07:38:40 +0900

    Title: 닷 투더 닷, Time: 350ms, Memory: 16MB, Status: Passed - CodeTree

--- 240906/닷 투더 닷/dot-to-the-dot.java
+++ 240906/닷 투더 닷/dot-to-the-dot.java
@@ -1,97 +1,103 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
-class vertex implements Comparable<vertex>{
-    int idx;
-    int L;
-    int C;
-    double weight;
+class Node {
+    int index, l, c;
+    
+    public Node(int index, int l, int c) {
+        this.index = index;
+        this.l = l;
+        this.c = c;
+    }
+};
 
-
-    public vertex(int idx, int L, int C, int X){
-        this.idx = idx;
-        this.L = L;
-        this.C = C;
-        if(C == 0){
-            this.weight = 0;
-        }
-        else{
-            this.weight = Double.valueOf(X / C) + L;
-        }
+class Element implements Comparable<Element> {
+    int dist, index;
+    
+    public Element(int dist, int index) {
+        this.dist = dist;
+        this.index = index;
     }
 
     @Override
-    public int compareTo(vertex e){
-        if(this.weight > e.weight){
-            return 1;
-        }
-        else if(this.weight == e.weight){
-            return 0;
-        }
-        else{
-            return -1;
-        }
+    public int compareTo(Element e) {
+        return this.dist - e.dist; 
     }
-}
+};
 
 public class Main {
-    public static final int MAX_VERTEX = 500;
-    public static final int INVALUED = (int)1e9;
-    public static ArrayList<vertex>[] vertexList = new ArrayList[MAX_VERTEX + 1];
-    public static double[] cost = new double[MAX_VERTEX + 1];
-    public static PriorityQueue<vertex> pq = new PriorityQueue<>();
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 500;
+    public static int n, m, x;
+    public static ArrayList<Node>[] graph = new ArrayList[MAX_N + 1];
+    public static PriorityQueue<Element> pq = new PriorityQueue<>();
+    public static ArrayList<Integer> cList = new ArrayList<>();
+    public static int[] dist = new int[MAX_N + 1];
+    public static double ans = 1e9;
 
-    public static double calCost(int B, int X, int A){
-        double ans = (X / A) + B;
-        return ans;
-    }
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
-
-        for(int i = 1 ; i <= MAX_VERTEX ; i++){
-            vertexList[i] = new ArrayList<vertex>();
-            cost[i] = INVALUED;
-        }
-
-        for(int i = 0 ; i < M ; i++){
-            st = new StringTokenizer(br.readLine());
-            int I = Integer.parseInt(st.nextToken());
-            int J = Integer.parseInt(st.nextToken());
-            int L = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
-            vertexList[I].add(new vertex(J, L, C, X));
-            vertexList[J].add(new vertex(I, L, C, X));
-        }
-        
-        cost[1] = 0;
-        pq.add(new vertex(1, 0, INVALUED, X));
-        while(!pq.isEmpty()){
-            vertex curVertex = pq.poll();
-
-            if(curVertex.weight != cost[curVertex.idx]){
+    public static void dijkstra(int k, int cLimit) {
+        for(int i = 1; i <= n; i++)
+            dist[i] = (int)1e9;
+    
+        dist[k] = 0;
+    
+        pq.add(new Element(0, k));
+    
+        while(!pq.isEmpty()) {
+            int minDist = pq.peek().dist;
+            int minIndex = pq.peek().index;
+            pq.poll();
+    
+            if(minDist != dist[minIndex])
                 continue;
-            }
+    
 
-            for(int i = 0 ; i < vertexList[curVertex.idx].size() ; i++){
-                vertex postVertex = vertexList[curVertex.idx].get(i);
-                int B = curVertex.L + postVertex.L;
-                int A = Math.min(curVertex.C, postVertex.C);
-                if(cost[postVertex.idx] > calCost(B, X, A)){
-                    cost[postVertex.idx] = calCost(B, X, A);
-                    pq.add(new vertex(postVertex.idx, B, A, X));
+            for(int j = 0; j < graph[minIndex].size(); j++) {
+                int targetIndex = graph[minIndex].get(j).index;
+                int targetL = graph[minIndex].get(j).l;
+                int targetC = graph[minIndex].get(j).c;
+                
+
+                if(targetC < cLimit)
+                    continue;
+
+                int newDist = dist[minIndex] + targetL;
+                if(dist[targetIndex] > newDist) {
+                    dist[targetIndex] = newDist;
+                    pq.add(new Element(newDist, targetIndex));
                 }
             }
         }
+    }
 
-        System.out.printf("%.0f", cost[N]);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        // for(int i = 1 ; i <= N ; i++){
-        //     System.out.print(cost[i] + " ");
-        // }
+        n = sc.nextInt();
+        m = sc.nextInt();
+        x = sc.nextInt();
+
+        for(int i = 1; i <= n; i++)
+            graph[i] = new ArrayList<>();
+
+        while(m-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int l = sc.nextInt();
+            int c = sc.nextInt();
+            graph[x].add(new Node(y, l, c));
+            graph[y].add(new Node(x, l, c));
+
+            cList.add(c);
+        }
+
+        for(int i = 0; i < cList.size(); i++) {
+            int cLimit = cList.get(i);
+            dijkstra(1, cLimit);
+            ans = Math.min(ans, dist[n] + (double) x / cLimit);
+        }
+
+        System.out.print((int)ans);
     }
 }
