f9a82540258ce08608fe0f309e17df291904be68
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-20 19:45:54 +0900

    Title: 트리 순회법 변경 2, Time: 679ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 241020/트리 순회법 변경 2/change-tree-traversal-2.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_NODE = 100000;
+    public static int[] preArr = new int[MAX_NODE + 1];
+    public static int[] inArr = new int[MAX_NODE + 1];
+    public static int[] postArr = new int[MAX_NODE + 1];
+    public static int postIdx = 1;
+    public static int n = -1;
+
+    public static void dfs(int preStart, int preEnd, int inStart, int inEnd){
+        if(preStart > preEnd){
+            return;
+        }
+
+        if(preStart == preEnd){
+            postArr[postIdx++] = preArr[preStart];
+            return;
+        }
+
+        int rootIdx = -1;
+        for(int i = inStart ; i <= inEnd ; i++){
+            if(preArr[preStart] == inArr[i]){
+                rootIdx = i;
+                break;
+            }
+        }
+
+        dfs(preStart + 1, preStart + (rootIdx - inStart), inStart, rootIdx - 1);
+        dfs(preStart + (rootIdx - inStart) + 1, preEnd, rootIdx + 1, inEnd);
+        postArr[postIdx++] = preArr[preStart];
+
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 1 ; i <= n ; i++){
+            preArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 1 ; i <= n ; i++){
+            inArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(1, n, 1, n);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1 ; i <= n ; i++){
+            sb.append(postArr[i] + " ");
+        }
+        System.out.print(sb.toString());
+
+    }
+}
