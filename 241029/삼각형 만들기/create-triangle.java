617d5fb7de4cf72179392bd14c6e29cb818ea88c
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-29 08:56:14 +0900

    Title: 삼각형 만들기, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241029/삼각형 만들기/create-triangle.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int MAX_POINT = 100;
+    public static int[] arrX = new int[MAX_POINT];
+    public static int[] arrY = new int[MAX_POINT];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            arrX[i] = Integer.parseInt(st.nextToken());
+            arrY[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                for(int k = 0 ; k < n ; k++){
+                    if(i == j || j == k || i == k){
+                        continue;
+                    }
+
+                    if(arrX[i] == arrX[j] || arrX[j] == arrX[k] || arrX[k] == arrX[i] || arrY[i] == arrY[j] || arrY[j] == arrY[k] || arrY[k] == arrY[i]){
+                        ans = Math.max(ans, (arrX[i] * arrY[j] + arrX[j] * arrY[k] + arrX[k] * arrY[i]) - (arrX[j] * arrY[i] + arrX[i] * arrY[k] + arrX[k] * arrY[j]));
+                    }
+                }
+            }
+        }
+        System.out.print(ans);
+    }
+}
