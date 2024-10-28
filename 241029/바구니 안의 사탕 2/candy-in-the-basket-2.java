79909a1362f0087679457380ec250d815f742cc4
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-29 05:13:56 +0900

    Title: 바구니 안의 사탕 2, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241029/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static final int MAX_LENGTH = 100;
+    public static int[] arr = new int[MAX_LENGTH + 2];
+    public static int[] prefix = new int[MAX_LENGTH + 2];
+
+    public static boolean inRange (int idx){
+        return idx >= 0 && idx <= 100;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        for(int i = 0 ; i < N ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int candy = Integer.parseInt(st.nextToken());
+            int loc = Integer.parseInt(st.nextToken());
+            arr[loc] += candy;
+        }
+
+        prefix[0] = arr[0];
+        for(int i = 0 ; i <= MAX_LENGTH ; i++){
+            prefix[i + 1] = prefix[i] + arr[i + 1];
+        }
+
+        // for(int i = 0 ; i <= 15 ; i++){
+        //     System.out.print(prefix[i] + " ");
+        // }
+
+        int left = 0;
+        int ans = 0;
+        for(int i = 0 ; i <= MAX_LENGTH - 2 * K ; i++){
+            if(i == 0){
+                left = 0;
+            }
+            else{
+                left = prefix[i - 1];
+            }
+
+            ans = Math.max(ans, prefix[i + 2 * K] - left);
+        }
+        
+        System.out.print(ans);
+    }
+}
