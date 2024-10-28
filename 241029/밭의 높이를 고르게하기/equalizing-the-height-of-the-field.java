bc42ea17eff7b0d286310cc291fb17e8e546d3be
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-29 06:38:50 +0900

    Title: 밭의 높이를 고르게하기, Time: 78ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241029/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
+++ 241029/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
@@ -2,23 +2,38 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public static final int MAX_LENGTH = 100;
+    public static final int INVALID = (int)1e9;
+    public static int[] arr = new int[MAX_LENGTH];
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
-        int T = Integer.parseInt(st.nextToken());
         int H = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine(), " ");
+
         for(int i = 0 ; i < N ; i++){
-            pq.add(Math.abs(Integer.parseInt(st.nextToken()) - H));
+            arr[i] = Math.abs(Integer.parseInt(st.nextToken()) - H);
         }
 
-        int ans = 0;
-        while(T > 0){
-            ans += pq.poll();
-            T--;
+        int ans = INVALID;
+        int j = 0;
+        int val = 0;
+        for(int i = 0 ; i < N - T + 1 ; i++){
+            // System.out.println(i + " " + j);
+            val += arr[i];
+            if(i - j + 1 > T){
+                // System.out.println("s");
+                val -= arr[j++];
+            }
+            
+            // System.out.println(val);
+            if(i - j + 1 == T){
+                // System.out.println("c");
+                ans = Math.min(ans, val);
+            }
         }
         System.out.print(ans);
     }
