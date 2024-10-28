bf6ab3defd02fbb06bf54e7b7aacd6fb79617da9
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-28 18:51:29 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241028/G or H 3/G-or-H-3.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAL_LENGTH = 10000;
+    public static int[] arr = new int[MAL_LENGTH + 1];
+    public static int[] prefix = new int[MAL_LENGTH + 1];
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        for(int i = 0 ; i < N ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int idx = Integer.parseInt(st.nextToken());
+            pq.add(idx);
+            char c = String.valueOf(st.nextToken()).charAt(0);
+            if(c == 'G'){
+                arr[idx] = 1;
+            }
+            else{
+                arr[idx] = 2;
+            }
+        }
+
+        for(int i = 1 ; i < MAL_LENGTH ; i++){
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        int ans = 0;
+        while(!pq.isEmpty()){
+            int num = pq.poll();
+            ans = Math.max(ans, prefix[num + K] - prefix[num - 1]);
+        }
+        System.out.print(ans);
+    }
+}
