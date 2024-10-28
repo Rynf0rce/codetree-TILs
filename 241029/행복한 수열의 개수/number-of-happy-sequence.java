0a3925a1c7262f1302573e54e43f303745d1b09d
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-29 07:20:32 +0900

    Title: 행복한 수열의 개수, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241029/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100;
+    public static int[][] table = new int[MAX_LENGTH][MAX_LENGTH];
+    public static Stack<Integer> s = new Stack<>();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0 ; j < n ; j++){
+                table[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(s.isEmpty() || s.peek() == table[i][j]){
+                    s.add(table[i][j]);
+                }
+                else{
+                    s.clear();
+                }
+                
+                if(s.size() >= m){
+                    ans++;
+                    s.clear();
+                    break;
+                }
+            }
+        }
+
+        for(int j = 0 ; j < n ; j++){
+            for(int i = 0 ; i < n ; i++){
+                if(s.isEmpty() || s.peek() == table[i][j]){
+                    s.add(table[i][j]);
+                }
+                else{
+                    s.clear();
+                }
+                
+                if(s.size() >= m){
+                    ans++;
+                    s.clear();
+                    break;
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
