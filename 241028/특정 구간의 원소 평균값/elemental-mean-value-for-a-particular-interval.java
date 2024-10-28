3627bbe77661b4bccea15f90f083460138306ef1
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-28 19:13:59 +0900

    Title: 특정 구간의 원소 평균값, Time: 116ms, Memory: 9MB, Status: Passed - CodeTree

--- 241028/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
+++ 241028/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -3,27 +3,29 @@ import java.io.*;
 
 public class Main {
     public static final int MAX_LENGTH = 100;
-    public static int[] arr = new int[MAX_LENGTH];
+    public static double[] arr = new double[MAX_LENGTH];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0 ; i < N ; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Double.parseDouble(st.nextToken());
         }
         
         int ans = 0;
         for(int i = 0 ; i < N ; i++){
             for(int j = i ; j < N ; j++){
-                int sum = 0;
+                double sum = 0;
                 for(int k = i ; k <= j ; k++){
                     sum += arr[k];
                 }
 
                 for(int k = i ; k <= j ; k++){
-                    if(sum / (j - i + 1) == arr[j]){
+                    if(sum / (j - i + 1) == arr[k]){
+                        // System.out.println(i + " " + j);
                         ans++;
+                        break;
                     }
                 }
                 
