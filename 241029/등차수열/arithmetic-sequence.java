9e0be42cb262028ccf917cc003ce409ff8a6e942
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-29 16:28:57 +0900

    Title: 등차수열, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 241029/등차수열/arithmetic-sequence.java
+++ 241029/등차수열/arithmetic-sequence.java
@@ -25,7 +25,7 @@ public class Main {
         }
 
         int ans = 0;
-        for(int i = 1 ; i <= 10 ; i++){
+        for(int i = 1 ; i <= MAX_LENGTH ; i++){
             ans = Math.max(ans, archeive[i]);
         }
         System.out.print(ans);
