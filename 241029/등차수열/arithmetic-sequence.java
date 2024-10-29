848a9415781ff9d6901c62d36f09f6dabe33b432
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-29 16:28:28 +0900

    Title: 등차수열, Time: 102ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241029/등차수열/arithmetic-sequence.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100;
+    public static int[] arr = new int[MAX_LENGTH];
+    public static int[] archeive = new int[MAX_LENGTH + 1];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i + 1; j < n ; j++){
+                int val = arr[i] + arr[j];
+                if(val % 2 == 1){
+                    continue;
+                }
+                archeive[val / 2]++;
+            }
+        }
+
+        int ans = 0;
+        for(int i = 1 ; i <= 10 ; i++){
+            ans = Math.max(ans, archeive[i]);
+        }
+        System.out.print(ans);
+    }
+}
