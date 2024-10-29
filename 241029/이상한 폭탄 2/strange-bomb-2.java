5e4765703239b0b5a5a17ce311c75cfdbbd198c6
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-29 14:13:40 +0900

    Title: 이상한 폭탄 2, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241029/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static HashMap<Integer, Integer> map = new HashMap<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int ans = 0;
+        for(int i = 0 ; i < N ; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(!map.containsKey(num)){
+                map.put(num, i);
+            }
+            else{
+                if(i - map.get(num) <= K){
+                    ans = Math.max(ans, num);
+                }
+                map.put(num, i);
+            }
+        }
+        System.out.print(ans);
+    }
+}
