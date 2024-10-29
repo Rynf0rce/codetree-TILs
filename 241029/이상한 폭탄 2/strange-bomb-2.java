1b8a1682c5cbe6b7d872cdc6940e28ecc91a4816
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-29 14:13:53 +0900

    Title: 이상한 폭탄 2, Time: 110ms, Memory: 8MB, Status: Passed - CodeTree

--- 241029/이상한 폭탄 2/strange-bomb-2.java
+++ 241029/이상한 폭탄 2/strange-bomb-2.java
@@ -9,7 +9,7 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int ans = 0;
+        int ans = -1;
         for(int i = 0 ; i < N ; i++){
             int num = Integer.parseInt(br.readLine());
             if(!map.containsKey(num)){
