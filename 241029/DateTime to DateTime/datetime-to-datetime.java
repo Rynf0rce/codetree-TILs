207ea43c530547beee0bad0a5df08aee4a89d213
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-29 17:26:51 +0900

    Title: DateTime to DateTime, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241029/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static final int HOUR_TO_MINTE = 60;
+    public static final int DAY_TO_HOUR = 24;
+    public static final int DAY = 11;
+    public static final int HOUR = 11;
+    public static final int MINTE = 11;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        System.out.print(( a * DAY_TO_HOUR * HOUR_TO_MINTE + b * HOUR_TO_MINTE + c) - ( DAY * DAY_TO_HOUR * HOUR_TO_MINTE + HOUR * HOUR_TO_MINTE + MINTE ));
+    }
+}
