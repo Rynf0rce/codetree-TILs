4c35503157b4d18e819e111e62361e2598adcc59
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-29 17:13:42 +0900

    Title: Date to Date, Time: 110ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241029/Date to Date/date-to-date.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static int[] arr = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int left = b;
+        for(int i = 0 ; i <= a ; i++){
+            left += arr[i];
+        }
+
+        int right = d;
+        for(int i = 0 ; i <= c ; i++){
+            right += arr[i];
+        }
+
+        System.out.print(Math.abs(right - left));
+    }
+}
