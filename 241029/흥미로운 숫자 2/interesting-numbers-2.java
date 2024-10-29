831f624be385d3323f40eda69e3196e371a2ec16
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-29 15:32:56 +0900

    Title: 흥미로운 숫자 2, Time: 1029ms, Memory: 150MB, Status: Passed - CodeTree

--- 241029/흥미로운 숫자 2/interesting-numbers-2.java
+++ 241029/흥미로운 숫자 2/interesting-numbers-2.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static HashSet<Character> hs = new HashSet<>();
+    public static HashSet<Integer> hs = new HashSet<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -14,12 +14,29 @@ public class Main {
             int cnt2 = 0;
             String str = String.valueOf(i);
             hs.clear();
+            int[] arr = new int[10];
             for(int j = 0 ; j < str.length() ; j++){
-                hs.add(str.charAt(j));
+                hs.add(Integer.parseInt(str.charAt(j) + ""));
+                arr[Integer.parseInt(str.charAt(j) + "")]++;
             }
 
+            
+
             if( hs.size() == 2 ){
-                ans++;
+                boolean minor = false;
+                boolean mul = false;
+                for(Integer num : hs){
+                    if(arr[num] == 1){
+                        minor = true;
+                    }
+                    else if(arr[num] == str.length() - 1){
+                        mul = true;
+                    }
+                }
+
+                if(minor && mul){
+                    ans++;
+                }
             }
         }
         System.out.print(ans);
