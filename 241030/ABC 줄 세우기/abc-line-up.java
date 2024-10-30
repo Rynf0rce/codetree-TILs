0422aa3670348dfbd10ef4680e703540a872b038
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-30 14:13:58 +0900

    Title: ABC 줄 세우기, Time: 186ms, Memory: 8MB, Status: Passed - CodeTree

--- 241030/ABC 줄 세우기/abc-line-up.java
+++ 241030/ABC 줄 세우기/abc-line-up.java
@@ -2,26 +2,26 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int ALPHABET = 26;
-    public static char[] arr = new char[ALPHABET];
+    public static ArrayList<Character> arr = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0 ; i < n ; i++){
-            arr[i] = st.nextToken().charAt(0);
+            arr.add(st.nextToken().charAt(0));
         }
 
         char target = 'A';
         int cnt = 0;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i ; j < n ; j++){
-                if(arr[j] == target){
-                    cnt += (j - i);
+        while(!arr.isEmpty()){
+            for(int i = 0 ; i < arr.size() ; i++){
+                if(arr.get(i) == target){
+                    cnt += i;
+                    target++;
+                    arr.remove(i);
                 }
             }
-            target++;
         }
         System.out.print(cnt);
     }
