8ff5f13c2e26bfe0d773aad90a96c946f7f9131d
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-30 12:32:59 +0900

    Title: 독서실의 거리두기 3, Time: 117ms, Memory: 8MB, Status: Passed - CodeTree

--- 241030/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
+++ 241030/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
@@ -8,24 +8,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        int cnt = 0;
-        for(int i = 0 ; i < n ; i++){
+        int cnt = 1;
+        for(int i = 1 ; i < n ; i++){
             if(str.charAt(i) == '0'){
                 cnt++;
             }
             else{
-                al.add(cnt + 1);
-                cnt = 0;
+                al.add(cnt);
+                cnt = 1;
             }
         }
 
         Collections.sort(al);
 
-        int ans = 0;
-        for(int num : al){
-            ans = Math.max(ans, num / 2);
+        int ans = 0; // 최소의 최대
+        for(int i = 0 ; i < al.size(); i++){
+            int val = MAX_LENGTH;
+            for(int j = 0 ; j < al.size() ; j++){
+                int num = i == j ? al.get(j) / 2 : al.get(j);
+                val = Math.min(val, num);
+            }
+            ans = Math.max(ans, val);
         }
-
         System.out.print(ans);
     }
 }
