8d358ce62ed4104dde8ab1e48402ab65ea38d2bd
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-30 13:08:28 +0900

    Title: 개발자의 가위바위보, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241030/개발자의 가위바위보/developers-rock-paper-scissors.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100;
+    public static int[] left = new int[MAX_LENGTH];
+    public static int[] right = new int[MAX_LENGTH];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            left[i] = Integer.parseInt(st.nextToken());
+            right[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int val1 = 0;
+        int val2 = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(left[i] > right[i]){
+                val1++;
+            }
+
+            if(left[i] < right[i]){
+                val2++;
+            }
+        }
+        System.out.print(Math.max(val1, val2));
+    }
+}
