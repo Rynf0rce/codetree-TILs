dd13e6ac75567fd606a0de69a52fa41823e06ef5
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-31 08:49:35 +0900

    Title: 대응되는 수와 문자, Time: 723ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241031/대응되는 수와 문자/corresponding-numbers-and-characters.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static ArrayList<String> arr = new ArrayList<>();
+    public static HashMap<String, Integer> hm = new HashMap<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for(int i = 1 ; i <= n ; i++){
+            String str = br.readLine();
+            arr.add(str);
+            hm.put(str, i);
+        }
+
+        for(int i = 0 ; i < m ; i++){
+            String str = br.readLine();
+            if('0' <= str.charAt(0) && str.charAt(0) < '9'){
+                System.out.println(arr.get(Integer.parseInt(str) - 1));
+            }
+            else{
+                System.out.println(hm.get(str));
+            }
+        }
+    }
+}
