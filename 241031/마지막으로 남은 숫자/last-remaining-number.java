47851e1a53b301c4afbc9615914a5351d8ca30a1
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-31 16:42:24 +0900

    Title: 마지막으로 남은 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241031/마지막으로 남은 숫자/last-remaining-number.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < n ; i++){
+            pq.add(-1 * Integer.parseInt(st.nextToken()));
+        }
+
+        while(pq.size() > 1){
+            int first = pq.poll();
+            int second = pq.poll();
+            
+            if(first == second){
+                continue;
+            }
+
+            pq.add((first - second));
+        }
+
+        System.out.print((pq.peek() * -1));
+    }
+}
