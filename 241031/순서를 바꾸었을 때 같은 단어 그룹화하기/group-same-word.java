ad17350113cf7be7f33a78e30afa4a59db79a98a
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-31 13:04:44 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 421ms, Memory: 19MB, Status: Passed - CodeTree

--- 241031/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.java
+++ 241031/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.java
@@ -2,24 +2,18 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static PriorityQueue<Character> pq = new PriorityQueue<>();
     public static HashMap<String, Integer> hm = new HashMap<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int ans = 0;
         for(int i = 0 ; i < n ; i++){
-            String str = br.readLine();
-            for(int j = 0 ; j < str.length(); j++){
-                pq.add(str.charAt(j));
-            }
+            char[] arr = br.readLine().toCharArray();
+            Arrays.sort(arr);
+            String str = new String(arr);
 
-            String ppap = "";
-            while(!pq.isEmpty()){
-                ppap += pq.poll() + "";
-            }
-            hm.put(ppap, hm.getOrDefault(ppap, 0) + 1);
-            ans = Math.max(ans, hm.get(ppap));
+            hm.put(str, hm.getOrDefault(str, 0) + 1);
+            ans = Math.max(ans, hm.get(str));
         }
         System.out.print(ans);
     }
