7d98f2751dcfa3694315690961121b8ec4d44e03
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-31 16:25:06 +0900

    Title: 최소 정수 출력, Time: 511ms, Memory: 30MB, Status: Passed - CodeTree

--- 241031/최소 정수 출력/min-integer-output.java
+++ 241031/최소 정수 출력/min-integer-output.java
@@ -5,20 +5,27 @@ public class Main {
     public static PriorityQueue<Long> pq = new PriorityQueue<>();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         for(int i = 0 ; i < n ; i++){
             long num = Long.parseLong(br.readLine());
             if(num == 0){
                 if(pq.isEmpty()){
-                    System.out.println(0);
+                    sb.append(0 + "\n");
                 }
                 else{
-                    System.out.println(pq.poll());
+                    sb.append(pq.poll() + "\n");
                 }
             }
             else{
                 pq.add(num);
             }
         }
+
+        bw.write(sb.toString());
+
+        bw.close();
+        br.close();
     }
 }
