bf548d21570ec779f2511389fd253c2f31e7e4db
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-10-31 10:08:32 +0900

    Title: 두 수의 합, Time: 524ms, Memory: 28MB, Status: Passed - CodeTree

--- 241031/두 수의 합/sum-of-two-num.java
+++ 241031/두 수의 합/sum-of-two-num.java
@@ -2,40 +2,30 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int MAX_INT = Integer.MAX_VALUE;
-    public static final int MIN_INT = Integer.MIN_VALUE;
-    public static HashMap<Integer, Integer> hm = new HashMap<>();
+    public static final int MAX_LENGTH = 100000;
+    public static long[] arr = new long[MAX_LENGTH + 1];
+    public static HashMap<Long, Integer> hm = new HashMap<>();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        long k = Long.parseLong(st.nextToken());
 
         st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0 ; i < n ; i++){
-            Integer num = Integer.parseInt(st.nextToken());
-            
-            hm.put(num, hm.getOrDefault(num, 0) + 1);
+            arr[i] = Long.parseLong(st.nextToken());
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
         }
 
         int ans = 0;
-        int same = 0;
-        for(Map.Entry<Integer, Integer> entry : hm.entrySet()){
-            double d = k - entry.getKey();
-            if(MIN_INT > d && d > MAX_INT){
+        for(int i = 0 ; i < n ; i++){
+            if(hm.get(arr[i]) <= 0 || hm.getOrDefault(k - arr[i], 0 ) <= 0){
                 continue;
             }
 
-
-            if(hm.containsKey((int)d)){
-                if((int)d * 2 == k){
-                    same += (entry.getValue() * (entry.getValue() - 1) / 2);
-                }
-                else{
-                    ans += entry.getValue();
-                }
-            }
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0) - 1);
+            ans += hm.get(k - arr[i]);
         }
-        System.out.print( (ans / 2) + same);
+        System.out.print(ans);
     }
 }
