acd37729448aa8bfcae6fc365b5e9b54f932c3e4
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-11-01 19:56:48 +0900

    Title: 크기가 n인 순열, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241101/크기가 n인 순열/n-permutation.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_NUM = 8;
+    public static int n = 0;
+    public static boolean[] visited = new boolean[MAX_NUM + 1];
+    public static ArrayList<Integer> arr = new ArrayList<>();
+
+    public static void printArray(int idx){
+        if(idx >= n){
+            for(int num : arr){
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 1 ; i <= n ; i++){
+            if(visited[i]){
+                continue;
+            }
+
+            visited[i] = true;
+            arr.add(i);
+            printArray(idx + 1);
+            arr.remove(arr.size() - 1);
+            visited[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        printArray(0);
+    }
+}
