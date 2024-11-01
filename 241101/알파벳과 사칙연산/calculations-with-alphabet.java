d609557d4be3b00b609da452f5448753bf8d9743
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-11-01 15:12:56 +0900

    Title: 알파벳과 사칙연산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241101/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 241101/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -4,21 +4,20 @@ public class Main {
     public static final int MAX_LENGTH = 200;
     public static char[] arr;
     public static HashMap<Character, Integer> hm = new HashMap<>();
-    public static ArrayList<Character> operator = new ArrayList<>();
     public static long ans = 0;
 
     public static long check(){
         long ans = hm.get(arr[0]);
-        for(int i = 2 ; i < arr.length ; i += 2){
-            switch(operator.get(i/2 - 1)){
+        for(int i = 1 ; i < arr.length ; i += 2){
+            switch(arr[i]){
                 case '+' :
-                    ans += hm.get(arr[i]);
+                    ans += hm.get(arr[i + 1]);
                 break;
                 case '-' :
-                    ans -= hm.get(arr[i]);
+                    ans -= hm.get(arr[i + 1]);
                 break;
                 case '*' :
-                    ans *= hm.get(arr[i]);
+                    ans *= hm.get(arr[i + 1]);
                 break;
             }
         }
@@ -40,9 +39,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         arr = sc.next().toCharArray();
-        for(int i = 1 ; i < arr.length ; i += 2){
-            operator.add(arr[i]);
-        }
 
         simulation(0);
 
