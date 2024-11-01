75db89e9b9d6ff0afb7b581642e764e76f6ab390
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-11-01 21:29:38 +0900

    Title: 수들의 합 최대화하기, Time: 287ms, Memory: 17MB, Status: Passed - CodeTree

--- 241101/수들의 합 최대화하기/max-sum-of-numbers.java
+++ 241101/수들의 합 최대화하기/max-sum-of-numbers.java
@@ -4,15 +4,14 @@ import java.io.*;
 public class Main {
     public static final int MAX_LENGTH = 10;
     public static int[][] table = new int[MAX_LENGTH][MAX_LENGTH];
-    public static HashSet<Integer> rowVisited = new HashSet<>();
     public static HashSet<Integer> colVisited = new HashSet<>();
     public static ArrayList<Integer> selected = new ArrayList<>();
     public static int n = 0;
     public static int ans = 0;
 
 
-    public static void findVal(int r, int c, int cnt){    
-        if(cnt == n){
+    public static void findVal(int idx){    
+        if(idx >= n){
             int sum = 0;
             for(int num : selected){
                 sum += num;
@@ -21,36 +20,15 @@ public class Main {
             return;
         }
 
-        if(r >= n){
-            return;
-        }
-
-        for(int i = r ; i < n ; i++){
-            if(rowVisited.contains(i)){
+        for(int i = 0 ; i < n ; i++){
+            if(colVisited.contains(i)){
                 continue;
             }
-            for(int j = c ; j < n ; j++){
-                if(colVisited.contains(j)){
-                    continue;
-                }
-
-                int goRow = r;
-                int goCol = c + 1;
-                if(c + 1 >= n){
-                    goRow++;
-                    goCol = 0;
-                }
-                
-                selected.add(table[i][j]);
-                rowVisited.add(i);
-                colVisited.add(j);
-                findVal(goRow, goCol, cnt + 1);
-                colVisited.remove(j);
-                rowVisited.remove(i);
-                selected.remove(selected.size() - 1);
-
-                findVal(goRow, goCol, cnt);
-            }
+            colVisited.add(i);
+            selected.add(table[idx][i]);
+            findVal(idx + 1);
+            selected.remove(selected.size() - 1);
+            colVisited.remove(i);
         }
 
     }
@@ -65,7 +43,7 @@ public class Main {
             }
         }
 
-        findVal(0, 0, 0);
+        findVal(0);
 
         System.out.print(ans);        
     }
