dedf37039c2b38d453d1c0ae474d7eda8d6b5313
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-11-01 10:13:21 +0900

    Title: 최솟값 3개, Time: 817ms, Memory: 22MB, Status: Passed - CodeTree

--- 241101/최솟값 3개/top-3-smallest-number.java
+++ 241101/최솟값 3개/top-3-smallest-number.java
@@ -8,7 +8,7 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int ans = 0;
+        long ans = 0;
         for(int i = 0 ; i < n ; i++){
             int num = Integer.parseInt(st.nextToken());
             if(i < 3){
