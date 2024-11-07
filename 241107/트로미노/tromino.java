a2f455ae2a3d630da942a062c994089960ca2572
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-11-07 18:54:02 +0900

    Title: 트로미노, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241107/트로미노/tromino.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 200;
+    public static final int MAX_INT = 1000;
+    public static int[][] table = new int[MAX_LENGTH][MAX_LENGTH];
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0 ; j < m ; j++){
+                table[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0 ; i < n; i++){
+            for(int j = 0 ; j < m ; j++){
+                int sum = 0;
+                int minVal = MAX_INT;
+                for(int k = i ; k < Math.min(i + 2, n) ; k++){
+                    for(int h = j ; h < Math.min(j + 2, m) ; h++){
+                        sum += table[k][h];
+                        minVal = Math.min(minVal, table[k][h]);
+                    }
+                }
+
+                ans = Math.max(ans, sum - minVal);
+
+                sum = 0;
+                for(int k = i ; k < Math.min(i + 3, n) ; k++){
+                    sum += table[k][j];
+                }
+
+                ans = Math.max(ans, sum);
+
+                sum = 0;
+                for(int k = j ; k < Math.min(j + 3, m) ; k++){
+                    sum += table[i][k];
+                }
+                ans = Math.max(ans, sum);   
+            }
+        }
+        System.out.print(ans);
+    }
+}
