7274b142b15ebd04e350cc227e452090485c1508
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-11-11 22:32:04 +0900

    Title: 밭의 높이를 고르게하기, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241111/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LENGTH = 100;
+    public static final int INVALID = (int)1e9;
+    public static int[] arr = new int[MAX_LENGTH];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = Math.abs(Integer.parseInt(st.nextToken()) - H);
+        }
+
+        int ans = INVALID;
+        int j = 0;
+        int val = 0;
+        for(int i = 0 ; i < N ; i++){
+            // System.out.println(i + " " + j);
+            val += arr[i];
+            if(i - j + 1 > T){
+                // System.out.println("s");
+                val -= arr[j++];
+            }
+            
+            // System.out.println(val);
+            if(i - j + 1 == T){
+                // System.out.println("c");
+                ans = Math.min(ans, val);
+            }
+        }
+        System.out.print(ans);
+    }
+}
