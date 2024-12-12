57b419c772e19871d72c14a7dec13f2acd5e922e
RYNF0RCE <59043668+Rynf0rce@users.noreply.github.com>
2024-12-12 19:07:40 +0900

    Title: 막대 자르기의 최소 비용, Time: 459ms, Memory: 22MB, Status: Passed - CodeTree

--- 241212/막대 자르기의 최소 비용/minimum-cost-of-cutting-a-bar.java
+++ 241212/막대 자르기의 최소 비용/minimum-cost-of-cutting-a-bar.java
@@ -2,24 +2,24 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-    public static int total = 0;
+    public static PriorityQueue<Double> pq = new PriorityQueue<>(Collections.reverseOrder());
+    public static double total = 0;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         while(st.hasMoreTokens()){
-            int num = Integer.parseInt(st.nextToken());
+            double num = Double.parseDouble(st.nextToken());
             total += num;
             pq.add(num);
         }
 
-        int ans = 0;
+        double ans = 0;
         while(pq.size() > 1){
-            int num = pq.poll();
+            double num = pq.poll();
             ans += (total - num) * num;
             total -= num;
         }
-        System.out.print(ans);
+        System.out.printf("%.0f", ans);
     }
 }
